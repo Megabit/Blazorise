85b5a4fadf3ec9430cf7c0552e87c596712e75f0
Mladen Macanović <mladen.macanovic@gmail.com>
2019-01-07 12:43:24 +0100

    nuget 0.4.0

--- NuGet/pack.bat
+++ NuGet/pack.bat
@@ -8,7 +8,7 @@ nuget pack ..\Source\Blazorise\Blazorise.csproj
 nuget pack ..\Source\Blazorise.Bootstrap\Blazorise.Bootstrap.csproj
 nuget pack ..\Source\Blazorise.Bulma\Blazorise.Bulma.csproj
 nuget pack ..\Source\Blazorise.Material\Blazorise.Material.csproj
-nuget pack ..\Source\Blazorise.Charts\Blazorise.Charts.csproj
-nuget pack ..\Source\Blazorise.Sidebar\Blazorise.Sidebar.csproj
 
+nuget pack ..\Source\Extensions\Blazorise.Charts\Blazorise.Charts.csproj
+nuget pack ..\Source\Extensions\Blazorise.Sidebar\Blazorise.Sidebar.csproj
 nuget pack ..\Source\Extensions\Blazorise.Snackbar\Blazorise.Snackbar.csproj
