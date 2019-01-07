75e21f091201f06b6f5ef6ead488837c5406646f
Mladen Macanović <mladen.macanovic@gmail.com>
2019-01-07 14:53:08 +0100

    fix: build path

--- build.cmd
+++ build.cmd
@@ -1,3 +1,3 @@
 cd /d "%~dp0"
-"%ProgramFiles(x86)%\\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" Blazorise.sln /p:Configuration=Release  /t:Restore;Rebuild /v:m
-"%ProgramFiles(x86)%\\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" Blazorise.sln /p:Configuration=Debug  /t:Restore;Rebuild /v:m
+"%ProgramFiles(x86)%\\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" Blazorise.sln /p:Configuration=Release  /t:Restore;Rebuild /v:m
+"%ProgramFiles(x86)%\\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" Blazorise.sln /p:Configuration=Debug  /t:Restore;Rebuild /v:m
