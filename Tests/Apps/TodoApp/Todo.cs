7017bb30db0af66b9f7f06b496817c8926ae1bc1
Mladen Macanović <mladen.macanovic@gmail.com>
2019-01-11 14:15:36 +0100

    new: todo app

---
+++ Tests/Apps/TodoApp/Todo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApp
+{
+    public class Todo
+    {
+        public bool Completed { get; set; }
+
+        public string Description { get; set; }
+    }
+}
