c553e06f3e922811a1489d739fbab164b1f14dd2
Mladen Macanović <mladen.macanovic@gmail.com>
2019-02-07 14:59:48 +0100

    new: DataContract and DataMember added to chart options

--- Source/Extensions/Blazorise.Charts/ChartData.cs
+++ Source/Extensions/Blazorise.Charts/ChartData.cs
@@ -17,50 +17,60 @@ namespace Blazorise.Charts
     /// Base data object for all charts.
     /// </summary>
     /// <typeparam name="TItem">Type of value in the dataset.</typeparam>
+    [DataContract]
     public class ChartData<TItem>
     {
         /// <summary>
         /// List of labels for the chart coordinates.
         /// </summary>
+        [DataMember( EmitDefaultValue = false )]
         public List<string> Labels { get; set; }
 
         /// <summary>
         /// List of datasets to be displayed in the chart.
         /// </summary>
+        [DataMember( EmitDefaultValue = false )]
         public List<ChartDataset<TItem>> Datasets { get; set; }
     }
 
     /// <summary>
     /// Base class for the chart dataset.
     /// </summary>
+    [DataContract]
     public class ChartDataset<T>
     {
         /// <summary>
         /// Defines the dataset display name.
         /// </summary>
+        [DataMember]
         public string Label { get; set; }
 
         /// <summary>
         /// List of data items.
         /// </summary>
+        [DataMember( EmitDefaultValue = false )]
         public List<T> Data { get; set; }
 
         /// <summary>
         ///List of background colors for each of the data items.
         /// </summary>
+        [DataMember( EmitDefaultValue = false )]
         public List<string> BackgroundColor { get; set; }
 
         /// <summary>
         /// List of border colors for each of the data items.
         /// </summary>
+        [DataMember( EmitDefaultValue = false )]
         public List<string> BorderColor { get; set; }
 
         /// <summary>
         /// Defines the border width.
         /// </summary>
+        [DataMember]
         public int BorderWidth { get; set; } = 1;
     }
 
+    [DataContract]
     public class LineChartDataset<T> : ChartDataset<T>
     {
         /// <summary>
@@ -72,126 +82,153 @@ namespace Blazorise.Charts
         /// <summary>
         /// Offset for line dashes.
         /// </summary>
+        [DataMember]
         public int BorderDashOffset { get; set; }
 
         /// <summary>
         /// Fill the area under the line.
         /// </summary>
+        [DataMember]
         public bool Fill { get; set; } = true;
 
         /// <summary>
         /// Bezier curve tension of the line. Set to 0 to draw straightlines. This option is ignored if monotone cubic interpolation is used.
         /// </summary>
+        [DataMember]
         public int LineTension { get; set; }
 
         /// <summary>
         /// The fill color for points.
         /// </summary>
+        [DataMember( EmitDefaultValue = false )]
         public List<string> PointBackgroundColor { get; set; }
 
         /// <summary>
         /// The border color for points.
         /// </summary>
+        [DataMember( EmitDefaultValue = false )]
         public List<string> PointBorderColor { get; set; }
 
         /// <summary>
         /// The width of the point border in pixels.
         /// </summary>
+        [DataMember]
         public int PointBorderWidth { get; set; }
 
         /// <summary>
         /// The radius of the point shape. If set to 0, the point is not rendered.
         /// </summary>
+        [DataMember]
         public float PointRadius { get; set; }
 
         /// <summary>
         /// If false, the line is not drawn for this dataset.
         /// </summary>
+        [DataMember]
         public bool ShowLine { get; set; } = true;
 
         /// <summary>
         /// If true, lines will be drawn between points with no or null data. If false, points with NaN data will create a break in the line.
         /// </summary>
+        [DataMember]
         public bool SpanGaps { get; set; }
 
         /// <summary>
         /// If the line is shown as a stepped line.
         /// </summary>
+        [DataMember]
         public bool SteppedLine { get; set; }
     }
 
+    [DataContract]
     public class BarChartDataset<T> : ChartDataset<T>
     {
         /// <summary>
         /// The fill colour of the bars when hovered.
         /// </summary>
+        [DataMember( EmitDefaultValue = false )]
         public List<string> HoverBackgroundColor { get; set; }
 
         /// <summary>
         /// The stroke colour of the bars when hovered.
         /// </summary>
+        [DataMember( EmitDefaultValue = false )]
         public List<string> HoverBorderColor { get; set; }
 
         /// <summary>
         /// The stroke width of the bars when hovered.
         /// </summary>
+        [DataMember]
         public int HoverBorderWidth { get; set; }
     }
 
+    [DataContract]
     public class PieChartDataset<T> : ChartDataset<T>
     {
         /// <summary>
         /// The fill colour of the arcs when hovered.
         /// </summary>
+        [DataMember( EmitDefaultValue = false )]
         public List<string> HoverBackgroundColor { get; set; }
 
         /// <summary>
         /// The stroke colour of the arcs when hovered.
         /// </summary>
+        [DataMember( EmitDefaultValue = false )]
         public List<string> HoverBorderColor { get; set; }
 
         /// <summary>
         /// The stroke width of the arcs when hovered.
         /// </summary>
+        [DataMember]
         public int HoverBorderWidth { get; set; }
     }
 
+    [DataContract]
     public class DoughnutChartDataset<T> : PieChartDataset<T>
     {
         // same as pie chart
     }
 
+    [DataContract]
     public class PolarAreaChartDataset<T> : ChartDataset<T>
     {
         /// <summary>
         /// The fill colour of the arcs when hovered.
         /// </summary>
+        [DataMember( EmitDefaultValue = false )]
         public List<string> HoverBackgroundColor { get; set; }
 
         /// <summary>
         /// The stroke colour of the arcs when hovered.
         /// </summary>
+        [DataMember( EmitDefaultValue = false )]
         public List<string> HoverBorderColor { get; set; }
 
         /// <summary>
         /// The stroke width of the arcs when hovered.
         /// </summary>
+        [DataMember]
         public int HoverBorderWidth { get; set; }
     }
 
+    [DataContract]
     public class RadarChartDataset<T> : ChartDataset<T>
     {
         /// <summary>
         /// How to fill the area under the line.
         /// </summary>
+        [DataMember]
         public bool Fill { get; set; }
 
         /// <summary>
         /// Bezier curve tension of the line. Set to 0 to draw straightlines.
         /// </summary>
+        [DataMember]
         public float LineTension { get; set; }
     }
 
+    [DataContract]
     public struct ChartColor
     {
         #region Constructors
@@ -255,21 +292,25 @@ namespace Blazorise.Charts
         /// <summary>
         /// Gets or sets the red component value of color structure.
         /// </summary>
+        [DataMember]
         public byte R { get; set; }
 
         /// <summary>
         /// Gets or sets the green component value of color structure.
         /// </summary>
+        [DataMember]
         public byte G { get; set; }
 
         /// <summary>
         /// Gets or sets the blue component value of color structure.
         /// </summary>
+        [DataMember]
         public byte B { get; set; }
 
         /// <summary>
         /// Gets or sets the alpha component value of color structure.
         /// </summary>
+        [DataMember]
         public float A { get; set; }
 
         #endregion
--- Source/Extensions/Blazorise.Charts/ChartOptions.cs
+++ Source/Extensions/Blazorise.Charts/ChartOptions.cs
@@ -2,92 +2,114 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Threading.Tasks;
 #endregion
 
 namespace Blazorise.Charts
 {
+    [DataContract]
     public class ChartOptions
     {
+        [DataMember( EmitDefaultValue = false )]
         public Scales Scales { get; set; }
 
+        [DataMember( EmitDefaultValue = false )]
         public Legend Legend { get; set; }
 
+        [DataMember( EmitDefaultValue = false )]
         public Tooltips Tooltips { get; set; }
     }
 
     #region Specifics
 
+    [DataContract]
     public class LineChartOptions : ChartOptions
     {
         /// <summary>
         /// If false, the lines between points are not drawn.
         /// </summary>
+        [DataMember]
         public bool ShowLines { get; set; } = true;
 
         /// <summary>
         /// If false, NaN data causes a break in the line.
         /// </summary>
+        [DataMember]
         public bool SpanGaps { get; set; }
     }
 
+    [DataContract]
     public class BarChartOptions : ChartOptions
     {
         /// <summary>
         /// Percent (0-1) of the available width each bar should be within the category width. 1.0 will take the whole category width and put the bars right next to each other.
         /// </summary>
+        [DataMember]
         public float BarPercentage { get; set; } = 0.9f;
 
         /// <summary>
         /// Percent (0-1) of the available width each category should be within the sample width. 
         /// </summary>
+        [DataMember]
         public float CategoryPercentage { get; set; } = 0.8f;
 
         /// <summary>
         /// Manually set width of each bar in pixels. If not set, the base sample widths are calculated automatically so that they take the full available widths without overlap. Then, the bars are sized using barPercentage and categoryPercentage.
         /// </summary>
+        [DataMember]
         public int BarThickness { get; set; }
 
         /// <summary>
         /// Set this to ensure that bars are not sized thicker than this.
         /// </summary>
+        [DataMember]
         public int MaxBarThickness { get; set; }
     }
 
+    [DataContract]
     public class PieChartOptions : ChartOptions
     {
         /// <summary>
         /// The percentage of the chart that is cut out of the middle.
         /// </summary>
+        [DataMember]
         public int CutoutPercentage { get; set; } = 0;
 
         /// <summary>
         /// Starting angle to draw arcs from.
         /// </summary>
+        [DataMember]
         public double Rotation { get; set; } = -0.5 * Math.PI;
 
         /// <summary>
         /// Sweep to allow arcs to cover.
         /// </summary>
+        [DataMember]
         public double Circumference { get; set; } = 2 * Math.PI;
     }
 
+    [DataContract]
     public class DoughnutChartOptions : PieChartOptions
     {
         /// <summary>
         /// The percentage of the chart that is cut out of the middle.
         /// </summary>
+        [DataMember]
         public new int CutoutPercentage { get; set; } = 50;
     }
 
+    [DataContract]
     public class PolarAreaChartOptions : ChartOptions
     {
         /// <summary>
         /// Starting angle to draw arcs for the first item in a dataset.
         /// </summary>
+        [DataMember]
         public double StartAngle { get; set; } = -0.5 * Math.PI;
     }
 
+    [DataContract]
     public class RadarChartOptions : ChartOptions
     {
         // Unlike other charts, the radar chart has no chart specific options.
@@ -95,48 +117,62 @@ namespace Blazorise.Charts
 
     #endregion
 
+    [DataContract]
     public class Scales
     {
+        [DataMember( EmitDefaultValue = false )]
         public List<Axe> XAxes { get; set; }
 
+        [DataMember( EmitDefaultValue = false )]
         public List<Axe> YAxes { get; set; }
     }
 
+    [DataContract]
     public class Legend
     {
         /// <summary>
         /// Is the legend shown.
         /// </summary>
+        [DataMember]
         public bool Display { get; set; } = true;
 
         /// <summary>
         /// Marks that this box should take the full width of the canvas (pushing down other boxes). This is unlikely to need to be changed in day-to-day use.
         /// </summary>
+        [DataMember]
         public bool FullWidth { get; set; } = true;
 
         /// <summary>
         /// Legend will show datasets in reverse order.
         /// </summary>
+        [DataMember]
         public bool Reverse { get; set; } = false;
     }
 
+    [DataContract]
     public class Tooltips
     {
         /// <summary>
         /// Are on-canvas tooltips enabled.
         /// </summary>
+        [DataMember]
         public bool Enabled { get; set; } = true;
     }
 
+    [DataContract]
     public class Axe
     {
+        [DataMember]
         public bool Display { get; set; } = true;
 
+        [DataMember( EmitDefaultValue = false )]
         public AxeTicks Ticks { get; set; }
     }
 
+    [DataContract]
     public class AxeTicks
     {
+        [DataMember]
         public bool BeginAtZero { get; set; }
     }
 }
