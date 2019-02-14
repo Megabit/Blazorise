d5b7ad405273af88f3f915462de9024749f88e2f
Mladen Macanović <mladen.macanovic@gmail.com>
2019-02-14 15:06:05 +0100

    ref: validation rules

--- Source/Blazorise/Utils/ValidationRule.cs
+++ Source/Blazorise/Utils/ValidationRule.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿#region Using directives
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
+#endregion
 
 namespace Blazorise
 {
@@ -11,76 +13,120 @@ namespace Blazorise
     /// </summary>
     public static class ValidationRule
     {
+        #region Basic methods
+
+        public static bool IsEqual( string value, string compare ) => value == compare;
+
+        public static bool IsLength( string value, int min, int max ) => value != null && value.Length >= min && value.Length <= max;
+
+        /// <summary>
+        /// Check if the string is null or empty.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static bool IsEmpty( string value ) => string.IsNullOrEmpty( value );
+
+        /// <summary>
+        /// Check if the string is not null or empty.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static bool IsNotEmpty( string value ) => !string.IsNullOrEmpty( value );
+
+        /// <summary>
+        /// Check if the string is an email.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static bool IsEmail( string value ) => Regex.IsMatch( value, @"^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,6}$", RegexOptions.IgnoreCase );
+
+        /// <summary>
+        /// Check if the string contains only letters (a-zA-Z).
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static bool IsAlpha( string value ) => Regex.IsMatch( value, @"^[a-zA-Z]+$" );
+
+        /// <summary>
+        /// Check if the string contains only letters and numbers.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static bool IsAlphanumeric( string value ) => Regex.IsMatch( value, @"^[a-zA-Z0-9]+$" );
+
+        /// <summary>
+        /// Check if the string contains only letters, numbers and underscore.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static bool IsAlphanumericWithUnderscore( string value ) => Regex.IsMatch( value, "^[a-zA-Z0-9_]+$" );
+
+        /// <summary>
+        /// Check if the string is uppercase.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static bool IsUppercase( string value ) => value?.All( c => char.IsUpper( c ) ) == true;
+
+        /// <summary>
+        /// Check if the string is lowercase.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static bool IsLowercase( string value ) => value?.All( c => char.IsLower( c ) ) == true;
+
+        #endregion
+
+        #region Handler methods
+
         /// <summary>
         /// Check if the string is null or empty.
         /// </summary>
         /// <param name="e"></param>
-        public static void IsEmpty( ValidatorEventArgs e )
-        {
-            e.Status = string.IsNullOrEmpty( Convert.ToString( e.Value ) ) ? ValidationStatus.Success : ValidationStatus.Error;
-        }
+        public static void IsEmpty( ValidatorEventArgs e ) => e.Status = IsEmpty( e.Value as string ) ? ValidationStatus.Success : ValidationStatus.Error;
 
         /// <summary>
         /// Check if the string is not null or empty.
         /// </summary>
         /// <param name="e"></param>
-        public static void IsNotEmpty( ValidatorEventArgs e )
-        {
-            e.Status = !string.IsNullOrEmpty( Convert.ToString( e.Value ) ) ? ValidationStatus.Success : ValidationStatus.Error;
-        }
+        public static void IsNotEmpty( ValidatorEventArgs e ) => e.Status = IsNotEmpty( e.Value as string ) ? ValidationStatus.Success : ValidationStatus.Error;
 
         /// <summary>
         /// Check if the string is an email.
         /// </summary>
         /// <param name="e"></param>
-        public static void IsEmail( ValidatorEventArgs e )
-        {
-            e.Status = Regex.IsMatch( Convert.ToString( e.Value ), @"^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,6}$", RegexOptions.IgnoreCase ) ? ValidationStatus.Success : ValidationStatus.Error;
-        }
+        public static void IsEmail( ValidatorEventArgs e ) => e.Status = IsEmail( e.Value as string ) ? ValidationStatus.Success : ValidationStatus.Error;
 
         /// <summary>
         /// Check if the string contains only letters (a-zA-Z).
         /// </summary>
         /// <param name="e"></param>
-        public static void IsAlpha( ValidatorEventArgs e )
-        {
-            e.Status = Regex.IsMatch( Convert.ToString( e.Value ), @"^[a-zA-Z]+$" ) ? ValidationStatus.Success : ValidationStatus.Error;
-        }
+        public static void IsAlpha( ValidatorEventArgs e ) => e.Status = IsAlpha( e.Value as string ) ? ValidationStatus.Success : ValidationStatus.Error;
 
         /// <summary>
         /// Check if the string contains only letters and numbers.
         /// </summary>
         /// <param name="e"></param>
-        public static void IsAlphanumeric( ValidatorEventArgs e )
-        {
-            e.Status = Regex.IsMatch( Convert.ToString( e.Value ), @"^[a-zA-Z0-9]+$" ) ? ValidationStatus.Success : ValidationStatus.Error;
-        }
+        public static void IsAlphanumeric( ValidatorEventArgs e ) => e.Status = IsAlphanumeric( e.Value as string ) ? ValidationStatus.Success : ValidationStatus.Error;
 
         /// <summary>
         /// Check if the string contains only letters, numbers and underscore.
         /// </summary>
         /// <param name="e"></param>
-        public static void IsAlphanumericWithUnderscore( ValidatorEventArgs e )
-        {
-            e.Status = Regex.IsMatch( Convert.ToString( e.Value ), "^[a-zA-Z0-9_]+$" ) ? ValidationStatus.Success : ValidationStatus.Error;
-        }
+        public static void IsAlphanumericWithUnderscore( ValidatorEventArgs e ) => e.Status = IsAlphanumericWithUnderscore( e.Value as string ) ? ValidationStatus.Success : ValidationStatus.Error;
 
         /// <summary>
         /// Check if the string is uppercase.
         /// </summary>
         /// <param name="e"></param>
-        public static void IsUppercase( ValidatorEventArgs e )
-        {
-            e.Status = Convert.ToString( e.Value )?.All( c => char.IsUpper( c ) ) == true ? ValidationStatus.Success : ValidationStatus.Error;
-        }
+        public static void IsUppercase( ValidatorEventArgs e ) => e.Status = IsUppercase( e.Value as string ) ? ValidationStatus.Success : ValidationStatus.Error;
 
         /// <summary>
         /// Check if the string is lowercase.
         /// </summary>
         /// <param name="e"></param>
-        public static void IsLowercase( ValidatorEventArgs e )
-        {
-            e.Status = Convert.ToString( e.Value )?.All( c => char.IsLower( c ) ) == true ? ValidationStatus.Success : ValidationStatus.Error;
-        }
+        public static void IsLowercase( ValidatorEventArgs e ) => e.Status = IsLowercase( e.Value as string ) ? ValidationStatus.Success : ValidationStatus.Error;
+
+        #endregion
     }
 }
