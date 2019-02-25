f3ff32a4189bebdd6ec5b23fa32662ede10f84ac
Mladen Macanović <mladen.macanovic@gmail.com>
2019-02-25 15:41:17 +0100

    new: TryChangeType method

--- Source/Blazorise/Utils/Convertes.cs
+++ Source/Blazorise/Utils/Convertes.cs
@@ -20,6 +20,26 @@ namespace Blazorise.Utils
             return (TValue)Convert.ChangeType( o, conversionType );
         }
 
+        public static bool TryChangeType<TValue>( object o, out TValue value )
+        {
+            try
+            {
+                Type conversionType = Nullable.GetUnderlyingType( typeof( TValue ) ) ?? typeof( TValue );
+
+                if ( conversionType.IsEnum && EnumTryParse( o?.ToString(), conversionType, out TValue theEnum ) )
+                    value = theEnum;
+                else
+                    value = (TValue)Convert.ChangeType( o, conversionType );
+
+                return true;
+            }
+            catch
+            {
+                value = default;
+                return false;
+            }
+        }
+
         // modified version of https://stackoverflow.com/a/11521834/833106
         public static bool EnumTryParse<TValue>( string input, Type conversionType, out TValue theEnum )
         {
