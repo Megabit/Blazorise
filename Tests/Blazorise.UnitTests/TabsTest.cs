556c92b847a22e0da9cd7e7c4d5a1ca5438e448b
Mladen Macanović <mladen.macanovic@gmail.com>
2019-03-12 16:24:28 +0100

    fix: added panes to Tabs test

--- Tests/Blazorise.UnitTests/TabsTest.cs
+++ Tests/Blazorise.UnitTests/TabsTest.cs
@@ -27,22 +27,34 @@ namespace Blazorise.UnitTests
         {
             var paragraph = Browser.FindElement( By.Id( "basic-tabs" ) );
             var links = paragraph.FindElements( By.TagName( "a" ) );
+            var content = paragraph.FindElement( By.ClassName( "tab-content" ) );
+            var panels = content.FindElements( By.TagName( "div" ) );
 
             Assert.NotEmpty( links );
+            Assert.NotEmpty( panels );
 
             WaitAssert.False( () => links[0].GetAttribute( "class" ).Contains( "show" ) );
             WaitAssert.True( () => links[1].GetAttribute( "class" ).Contains( "show" ) );
             WaitAssert.False( () => links[2].GetAttribute( "class" ).Contains( "show" ) );
+            WaitAssert.False( () => panels[0].GetAttribute( "class" ).Contains( "show" ) );
+            WaitAssert.True( () => panels[1].GetAttribute( "class" ).Contains( "show" ) );
+            WaitAssert.False( () => panels[2].GetAttribute( "class" ).Contains( "show" ) );
 
             links[0].Click();
             WaitAssert.True( () => links[0].GetAttribute( "class" ).Contains( "show" ) );
             WaitAssert.False( () => links[1].GetAttribute( "class" ).Contains( "show" ) );
             WaitAssert.False( () => links[2].GetAttribute( "class" ).Contains( "show" ) );
+            WaitAssert.True( () => panels[0].GetAttribute( "class" ).Contains( "show" ) );
+            WaitAssert.False( () => panels[1].GetAttribute( "class" ).Contains( "show" ) );
+            WaitAssert.False( () => panels[2].GetAttribute( "class" ).Contains( "show" ) );
 
             links[2].Click();
             WaitAssert.False( () => links[0].GetAttribute( "class" ).Contains( "show" ) );
             WaitAssert.False( () => links[1].GetAttribute( "class" ).Contains( "show" ) );
             WaitAssert.True( () => links[2].GetAttribute( "class" ).Contains( "show" ) );
+            WaitAssert.False( () => panels[0].GetAttribute( "class" ).Contains( "show" ) );
+            WaitAssert.False( () => panels[1].GetAttribute( "class" ).Contains( "show" ) );
+            WaitAssert.True( () => panels[2].GetAttribute( "class" ).Contains( "show" ) );
         }
     }
 }
