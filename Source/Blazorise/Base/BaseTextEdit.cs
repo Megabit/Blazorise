3c94929ee049ee1455755fb47431e031570fc37b
Mladen Macanović <mladen.macanovic@gmail.com>
2019-03-19 14:31:57 +0100

    clean: TextEdit unused code

--- Source/Blazorise/Base/BaseTextEdit.cs
+++ Source/Blazorise/Base/BaseTextEdit.cs
@@ -15,8 +15,6 @@ namespace Blazorise.Base
 
         private Color color;
 
-        //private string text;
-
         #endregion
 
         #region Methods
@@ -54,70 +52,6 @@ namespace Blazorise.Base
             TextChanged?.Invoke( Text );
         }
 
-        //protected void HandleOnInput( UIChangeEventArgs e )
-        //{
-        //    Console.WriteLine( $"oninput: {e.Value}" );
-
-        //    var newText = e?.Value?.ToString();
-
-        //    if ( TryChangeText( newText ) && TryEditMask( newText ) )
-        //    {
-        //        text = newText;
-        //        TextChanged?.Invoke( text );
-        //    }
-        //    else
-        //    {
-        //        // currently there is no other way to prevent onchange value without js :(
-        //        JSRunner.SetTextValue( ElementRef, text );
-        //    }
-
-        //    //Console.WriteLine( $"text: {Text}; new: {newValue}" );
-        //}
-
-        //private bool TryEditMask( string newValue )
-        //{
-        //    if ( MaskType == MaskType.None || string.IsNullOrEmpty( newValue ) )
-        //        return true;
-
-        //    if ( MaskType == MaskType.Numeric )
-        //    {
-        //        return decimal.TryParse( newValue, out var n );
-        //    }
-        //    else if ( MaskType == MaskType.DateTime )
-        //    {
-        //        return DateTime.TryParse( newValue, out var dt );
-        //    }
-
-        //    return false;
-        //}
-
-        ///// <summary>
-        ///// Finds if any of the subscribed events has requested cancel.
-        ///// </summary>
-        ///// <param name="newValue">New value entered in the field.</param>
-        ///// <returns></returns>
-        //private bool TryChangeText( string newValue )
-        //{
-        //    var handler = TextChanging;
-
-        //    if ( handler != null )
-        //    {
-        //        var args = new ChangingEventArgs( text, newValue, false );
-
-        //        foreach ( Action<ChangingEventArgs> subHandler in handler?.GetInvocationList() )
-        //        {
-        //            subHandler( args );
-
-        //            if ( args.Cancel )
-        //            {
-        //                return false;
-        //            }
-        //        }
-        //    }
-
-        //    return true;
-        //}
-
         #endregion
 
         #region Properties
@@ -129,28 +63,11 @@ namespace Blazorise.Base
         /// </summary>
         [Parameter]
         protected string Text { get => Value; set => Value = value; }
-        //protected string Text
-        //{
-        //    get { return text; }
-        //    set
-        //    {
-        //        if ( text != value && TryChangeText( value ) )
-        //        {
-        //            text = value;
-        //        }
-        //    }
-        //}
 
         /// <summary>
         /// Occurs after text has changed.
         /// </summary>
         [Parameter] protected Action<string> TextChanged { get; set; }
-
-        //[Parameter] protected Action<ChangingEventArgs> TextChanging { get; set; }
-
-        //[Parameter] protected MaskType MaskType { get; set; } = MaskType.None;
-
-        //[Parameter] protected string MaskFormat { get; set; }
 
         protected string Type => Role.ToTextRoleString();
 
