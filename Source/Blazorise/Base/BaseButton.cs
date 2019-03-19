3faffeb504777d4b9afc8131cb7fd75cf86f2747
Mladen Macanović <mladen.macanovic@gmail.com>
2019-03-19 15:02:45 +0100

    feat: button Clicked event converted to EventCallback

--- Source/Blazorise/Base/BaseButton.cs
+++ Source/Blazorise/Base/BaseButton.cs
@@ -64,7 +64,7 @@ namespace Blazorise.Base
         protected void ClickHandler()
         {
             if ( !IsDisabled )
-                Clicked?.Invoke();
+                Clicked.InvokeAsync( null );
         }
 
         protected override void OnInit()
@@ -86,7 +86,7 @@ namespace Blazorise.Base
         /// <summary>
         /// Occurs when the button is clicked.
         /// </summary>
-        [Parameter] protected Action Clicked { get; set; }
+        [Parameter] protected EventCallback Clicked { get; set; }
 
         /// <summary>
         /// Defines the button type.
