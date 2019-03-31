8adfef28f1d9fd787758ec2df66fb56cfd0b2f11
Mladen Macanović <mladen.macanovic@gmail.com>
2019-03-31 15:01:56 +0200

    fix: build.cmd paths

--- build.cmd
+++ build.cmd
@@ -1,3 +1,3 @@
 cd /d "%~dp0"
-"%ProgramFiles(x86)%\\Microsoft Visual Studio\2019\Preview\MSBuild\Current\Bin\MSBuild.exe" Blazorise.sln /p:Configuration=Release  /t:Restore;Rebuild /v:m
-"%ProgramFiles(x86)%\\Microsoft Visual Studio\2019\Preview\MSBuild\Current\Bin\MSBuild.exe" Blazorise.sln /p:Configuration=Debug  /t:Restore;Rebuild /v:m
+"%ProgramFiles(x86)%\\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" Blazorise.sln /p:Configuration=Release  /t:Restore;Rebuild /v:m
+"%ProgramFiles(x86)%\\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" Blazorise.sln /p:Configuration=Debug  /t:Restore;Rebuild /v:m
