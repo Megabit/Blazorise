eab416a776779dd79eaa0fa5f4741a35154e65ac
Mladen Macanović <mladen.macanovic@gmail.com>
2019-04-19 23:13:19 +0200

    fix: BootstrapStyleProvider methods made virtual

--- Source/Blazorise.Bootstrap/BootstrapStyleProvider.cs
+++ Source/Blazorise.Bootstrap/BootstrapStyleProvider.cs
@@ -11,25 +11,25 @@ namespace Blazorise.Bootstrap
     {
         #region Modal
 
-        public string ModalShow() => "display: block; padding-right: 17px;";
+        public virtual string ModalShow() => "display: block; padding-right: 17px;";
 
         #endregion
 
         #region ModalBody
 
-        public string ModalBodyMaxHeight( int maxHeight ) => $"max-height: {maxHeight}vh; overflow-y: auto;";
+        public virtual string ModalBodyMaxHeight( int maxHeight ) => $"max-height: {maxHeight}vh; overflow-y: auto;";
 
         #endregion
 
         #region ProgressBar
 
-        public string ProgressBarValue( int value ) => $"width: {value}%";
+        public virtual string ProgressBarValue( int value ) => $"width: {value}%";
 
         #endregion
 
         #region Layout
 
-        public string Visibility( Visibility visibility ) => visibility == Blazorise.Visibility.Never ? "display: none;" : null;
+        public virtual string Visibility( Visibility visibility ) => visibility == Blazorise.Visibility.Never ? "display: none;" : null;
 
         #endregion
     }
