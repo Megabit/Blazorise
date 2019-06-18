b94f83e0e47dabbe2a3cacab61ebe9db1e657215
Mladen Macanovic <mladen.macanovic@gmail.com>
2019-06-18 23:09:25 +0200

    wip: added theme provider

---
+++ Source/Blazorise/Base/BaseThemeProvider.cs
@@ -0,0 +1,89 @@
+﻿#region Using directives
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+#endregion
+
+namespace Blazorise.Base
+{
+    public abstract class BaseThemeProvider : ComponentBase
+    {
+        #region Members
+
+        private Theme theme;
+
+        #endregion
+
+        #region Methods
+
+        public void Dispose()
+        {
+            if ( theme != null )
+            {
+                theme.Changed -= OnOptionsChanged;
+            }
+        }
+
+        protected virtual void GenerateStyle( StringBuilder sb )
+        {
+            if ( !string.IsNullOrEmpty( Theme?.Variants?.Primary ) )
+                sb.AppendLine( $"--b-theme-variant-primary: {Theme.Variants.Primary};" );
+
+            if ( !string.IsNullOrEmpty( Theme?.Variants?.Secondary ) )
+                sb.AppendLine( $"--b-theme-variant-secondary: {Theme.Variants.Secondary};" );
+        }
+
+        public string GetStyleTag()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine( "<style>" );
+            sb.AppendLine( ":root" );
+            sb.AppendLine( "{" );
+            GenerateStyle( sb );
+            sb.AppendLine( "}" );
+            sb.AppendLine( "</style>" );
+            return sb.ToString();
+        }
+
+        private void OnOptionsChanged( object sender, EventArgs e )
+        {
+            StateHasChanged();
+        }
+
+        #endregion
+
+        #region Properties
+
+        [Parameter]
+        public Theme Theme
+        {
+            get => theme;
+            set
+            {
+                if ( theme == value )
+                {
+                    return;
+                }
+
+                if ( theme != null )
+                {
+                    theme.Changed -= OnOptionsChanged;
+                }
+
+                theme = value;
+
+                if ( theme != null )
+                {
+                    theme.Changed += OnOptionsChanged;
+                }
+            }
+        }
+
+        [Parameter] protected RenderFragment ChildContent { get; set; }
+
+        #endregion
+    }
+}
---
+++ Source/Blazorise/Theme.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blazorise
+{
+    public class Theme
+    {
+        public event EventHandler<EventArgs> Changed;
+
+        public void ThemeHasChanged()
+        {
+            Changed?.Invoke( this, EventArgs.Empty );
+        }
+
+        public ThemeVariants Variants { get; set; }
+    }
+
+    public class ThemeVariants
+    {
+        public string Primary { get; set; }
+
+        public string Secondary { get; set; }
+    }
+}
