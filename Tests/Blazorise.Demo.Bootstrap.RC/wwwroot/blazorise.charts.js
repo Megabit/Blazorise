656c7b1d2552e67be08baeca0aa422d2c9f6dbd9
Mladen Macanovic <mladen.macanovic@gmail.com>
2019-09-19 16:02:35 +0200

    fix: js and css files for server-side demo

--- Tests/Blazorise.Demo.Bootstrap.RC/wwwroot/blazorise.charts.js
+++ Tests/Blazorise.Demo.Bootstrap.RC/wwwroot/blazorise.charts.js
@@ -1,34 +1,96 @@
 window.blazoriseCharts = {
-    setChartData: (canvasId, type, data, options, dataJsonString, optionsJsonString) => {
+    _instances: [],
+
+    initialize: (dotnetAdapter, hasClickEvent, hasHoverEvent, canvasId, type, data, options, dataJsonString, optionsJsonString) => {
         if (dataJsonString)
             data = JSON.parse(dataJsonString);
 
         if (optionsJsonString)
             options = JSON.parse(optionsJsonString);
 
-        //console.log(type);
-        //console.log(JSON.stringify(data));
-        //console.log(JSON.stringify(options));
+        // search for canvas element
+        const canvas = document.getElementById(canvasId);
 
-        const chart = blazoriseCharts.getChart(canvasId);
+        if (canvas) {
+            let chart = new Chart(canvas, {
+                type: type,
+                data: data,
+                options: options
+            });
+
+            // save references to all elements
+            window.blazoriseCharts._instances[canvasId] = {
+                dotNetRef: dotnetAdapter,
+                canvas: canvas,
+                chart: chart
+            };
+
+            window.blazoriseCharts.wireEvents(dotnetAdapter, hasClickEvent, hasHoverEvent, canvas, chart);
+        }
+
+        return true;
+    },
+
+    destroy: (canvasId) => {
+        var instances = window.blazoriseCharts._instances || {};
+
+        const chart = instances[canvasId].chart;
+
+        if (chart) {
+            chart.destroy();
+        }
+
+        delete instances[canvasId];
+
+        return true;
+    },
+
+    update: (canvasId, data, options, dataJsonString, optionsJsonString) => {
+        if (dataJsonString)
+            data = JSON.parse(dataJsonString);
+
+        if (optionsJsonString)
+            options = JSON.parse(optionsJsonString);
+
+        const chart = window.blazoriseCharts.getChart(canvasId);
 
         if (chart) {
             chart.config.data = data;
             chart.config.options = options;
             chart.update();
-        } else {
-            const canvas = document.getElementById(canvasId);
-
-            if (canvas) {
-                new Chart(canvas, {
-                    type: type,
-                    data: data,
-                    options: options
-                });
-            }
         }
 
         return true;
+    },
+
+    wireEvents: (dotnetAdapter, hasClickEvent, hasHoverEvent, canvas, chart) => {
+        if (hasClickEvent) {
+            canvas.onclick = function (evt) {
+                var elemetn = chart.getElementsAtEvent(evt);
+
+                for (var i = 0; i < elemetn.length; i++) {
+                    const datasetIndex = elemetn[i]["_datasetIndex"];
+                    const index = elemetn[i]["_index"];
+                    const model = elemetn[i]["_model"];
+
+                    dotnetAdapter.invokeMethodAsync("Event", "click", datasetIndex, index, JSON.stringify(model));
+                }
+            };
+        }
+
+        if (hasHoverEvent) {
+            chart.config.options.onHover = function (evt) {
+                var element = chart.getElementsAtEvent(evt);
+
+                for (var i = 0; i < element.length; i++) {
+                    const datasetIndex = element[i]["_datasetIndex"];
+                    const index = element[i]["_index"];
+                    const model = element[i]["_model"];
+
+                    dotnetAdapter.invokeMethodAsync("Event", "hover", datasetIndex, index, JSON.stringify(model));
+                }
+            };
+        }
     },
 
     getChart: (canvasId) => {
