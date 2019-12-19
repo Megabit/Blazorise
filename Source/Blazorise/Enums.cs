afcaee206ca2c581a342a61af2c747a3167d27e2
Mladen Macanović <macanovic.mladen@gmail.com>
2019-12-19 14:42:48 +0100

    feat: character casing

--- Source/Blazorise/Enums.cs
+++ Source/Blazorise/Enums.cs
@@ -1249,4 +1249,30 @@ namespace Blazorise
         /// </summary>
         Is512x512 = 9,
     }
+
+    /// <summary>
+    /// Specifies the case of characters in an element.
+    /// </summary>
+    public enum CharacterCasing
+    {
+        /// <summary>
+        /// The case of characters is left unchanged.
+        /// </summary>
+        Normal = 0,
+
+        /// <summary>
+        /// Converts all characters to uppercase.
+        /// </summary>
+        Upper = 1,
+
+        /// <summary>
+        /// Converts all characters to lowercase.
+        /// </summary>
+        Lower = 2,
+
+        /// <summary>
+        /// Convert first character to uppercase and all other to lowercase.
+        /// </summary>
+        Title = 3,
+    }
 }
