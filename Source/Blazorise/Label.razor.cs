ce5193e889b9f733c996c3bfb6f5b1edef742fb8
Mladen Macanović <macanovic.mladen@gmail.com>
2020-01-29 13:43:15 +0100

    refactor: Label Type attribute

--- Source/Blazorise/Label.razor.cs
+++ Source/Blazorise/Label.razor.cs
@@ -12,9 +12,7 @@ namespace Blazorise
     {
         #region Members
 
-        private bool isCheck;
-
-        private bool isFile;
+        private LabelType type = LabelType.None;
 
         private Cursor cursor;
 
@@ -24,9 +22,9 @@ namespace Blazorise
 
         protected override void BuildClasses( ClassBuilder builder )
         {
-            builder.Append( ClassProvider.Label(), !IsFile && !IsCheck );
-            builder.Append( ClassProvider.LabelFile(), IsFile );
-            builder.Append( ClassProvider.LabelCheck(), IsCheck );
+            builder.Append( ClassProvider.Label(), Type == LabelType.None );
+            builder.Append( ClassProvider.LabelFile(), Type == LabelType.File );
+            builder.Append( ClassProvider.LabelCheck(), Type == LabelType.Check );
             builder.Append( ClassProvider.LabelCursor( Cursor ), Cursor != Cursor.Default );
 
             base.BuildClasses( builder );
@@ -39,24 +37,12 @@ namespace Blazorise
         [Parameter] public string For { get; set; }
 
         [Parameter]
-        public bool IsCheck
+        public LabelType Type
         {
-            get => isCheck;
+            get => type;
             set
             {
-                isCheck = value;
-
-                DirtyClasses();
-            }
-        }
-
-        [Parameter]
-        public bool IsFile
-        {
-            get => isFile;
-            set
-            {
-                isFile = value;
+                type = value;
 
                 DirtyClasses();
             }
