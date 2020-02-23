329fa3e543a59e5416b4f94ac581f619945953f2
Mladen Macanovic <mladen.macanovic@gmail.com>
2020-02-23 19:46:20 +0100

    feat: chart colors converted to object type to allow single colors instead of array

--- Source/Extensions/Blazorise.Charts/ChartData.cs
+++ Source/Extensions/Blazorise.Charts/ChartData.cs
@@ -69,13 +69,13 @@ namespace Blazorise.Charts
         ///List of background colors for each of the data items.
         /// </summary>
         [DataMember( EmitDefaultValue = false )]
-        public List<string> BackgroundColor { get; set; }
+        public object BackgroundColor { get; set; }
 
         /// <summary>
         /// List of border colors for each of the data items.
         /// </summary>
         [DataMember( EmitDefaultValue = false )]
-        public List<string> BorderColor { get; set; }
+        public object BorderColor { get; set; }
 
         /// <summary>
         /// Defines the border width.
