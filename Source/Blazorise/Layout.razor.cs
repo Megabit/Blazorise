fb25f69438a277ae119c0137a687b06b36c5c08c
Mladen Macanovic <mladen.macanovic@gmail.com>
2020-04-08 12:39:34 +0200

    wip: Layout components

---
+++ Source/Blazorise/Layout.razor.cs
@@ -0,0 +1,49 @@
+﻿#region Using directives
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+#endregion
+
+namespace Blazorise
+{
+    public partial class Layout : BaseComponent
+    {
+        #region Members
+
+        private bool sider;
+
+        #endregion
+
+        #region Methods
+
+        protected override void BuildClasses( ClassBuilder builder )
+        {
+            builder.Append( "b-layout" );
+            builder.Append( "b-layout-has-sider", Sider );
+
+            base.BuildClasses( builder );
+        }
+
+        #endregion
+
+        #region Properties
+
+        [Parameter]
+        public bool Sider
+        {
+            get => sider;
+            set
+            {
+                sider = value;
+
+                DirtyClasses();
+            }
+        }
+
+        [Parameter] public RenderFragment ChildContent { get; set; }
+
+        #endregion
+    }
+}
