c296ef15969c1abd710d5b767170212a20e837bb
Pedro Silva <DarthPedro@gmail.com>
2020-04-13 10:14:27 -0700

    Added unit test for DateEdit control class.

---
+++ Tests/Blazorise.Tests/Mocks/MockDateEdit.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Moq;
+
+namespace Blazorise.Tests.Mocks
+{
+    internal class MockDateEdit<T> : DateEdit<T>
+    {
+        public MockDateEdit(Validation validation = null, Expression<Func<T>> dateExpression = null)
+        {
+            var mockRunner = new Mock<IJSRunner>();
+            mockRunner.Setup( r => r.ActivateDatePicker( It.IsAny<string>(), It.IsAny<string>() ) )
+                      .Callback( ( string id, string f ) => this.OnActivateDatePicker( id, f ) );
+            base.JSRunner = mockRunner.Object;
+
+            base.ParentValidation = validation;
+            base.DateExpression = dateExpression;
+
+            this.OnInitialized();
+        }
+
+        public string TextValue
+        {
+            get { return base.CurrentValueAsString; }
+        }
+
+        public string ClickedId { get; private set; }
+
+        public async Task<ParseValue<T>> ParseValueAsync( string value )
+        {
+            return await base.ParseValueFromStringAsync( value );
+        }
+
+        public void Click()
+        {
+            base.OnClickHandler( new MouseEventArgs() );
+        }
+
+        public void OnChange( ChangeEventArgs e)
+        {
+            base.OnChangeHandler( e );
+        }
+
+        private bool OnActivateDatePicker( string elementId, string formatString )
+        {
+            this.ClickedId = elementId;
+            return true;
+        }
+
+    }
+}
