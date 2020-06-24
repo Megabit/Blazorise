a531892274d05f1a95bfb5ecf2e37c314c133135
Mladen Macanovic <mladen.macanovic@pebble.tv>
2020-06-24 15:54:54 +0200

    feat: Snackbar close reason

--- Source/Extensions/Blazorise.Snackbar/Enums.cs
+++ Source/Extensions/Blazorise.Snackbar/Enums.cs
@@ -78,4 +78,20 @@ namespace Blazorise.Snackbar
         /// </summary>
         Dark,
     }
+
+    /// <summary>
+    /// Specifies the reason that a snackbar was closed.
+    /// </summary>
+    public enum SnackbarCloseReason
+    {
+        /// <summary>
+        /// Snackbar is closed automatically by internal timer or by other unknown reason.
+        /// </summary>
+        None,
+
+        /// <summary>
+        /// Snackbar is closed by the user.
+        /// </summary>
+        UserClosed,
+    }
 }
---
+++ Source/Extensions/Blazorise.Snackbar/EventArguments.cs
@@ -0,0 +1,24 @@
+﻿#region Using directives
+using System;
+using System.Collections.Generic;
+using System.Text;
+#endregion
+
+namespace Blazorise.Snackbar
+{
+    /// <summary>
+    /// Provides information about <see cref="Snackbar.Closed"/> event.
+    /// </summary>
+    public class SnackbarClosedEventArgs : EventArgs
+    {
+        public SnackbarClosedEventArgs( SnackbarCloseReason closeReason )
+        {
+            CloseReason = closeReason;
+        }
+
+        /// <summary>
+        /// Gets a value that indicates why the snackbar is being closed.
+        /// </summary>
+        public SnackbarCloseReason CloseReason { get; }
+    }
+}
