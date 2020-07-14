cd8c03f6950f83681cc0351d5564d1d7cab66c1f
Willian Gruber <willian.gruber@hotmail.com>
2020-07-14 17:06:55 -0300

    New Theme Page

---
+++ Demos/Apps/ThemeApp/ThemeCustomizer.razor.cs
@@ -0,0 +1,16 @@
+﻿using Blazorise;
+using Microsoft.AspNetCore.Components;
+
+namespace ThemeApp
+{
+    public partial class ThemeCustomizer : ComponentBase
+    {
+        private string SelectedTab { get; set; } = "General";
+        [CascadingParameter] public Theme Theme { get; set; }
+
+        private void OnSelectedTabChanged(string name)
+        {
+            SelectedTab = name;
+        }
+    }
+}
