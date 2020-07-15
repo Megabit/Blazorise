c2d3f3a6609048dedc0008617ef799927eb24045
Willian Gruber <willian.gruber@hotmail.com>
2020-07-15 13:06:45 -0300

    New Theme Page

---
+++ Demos/Apps/ThemeApp/ColorTransparencyEdit.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace ThemeApp
+{
+    public partial class ColorTransparencyEdit : ComponentBase
+    {
+        private string _color;
+        [Parameter] public string Color { get => _color; set { if (_color == value) return; _color = value; ColorChanged.InvokeAsync(value); } }
+        [Parameter] public EventCallback<string> ColorChanged { get; set; }
+        [Parameter] public string Label { get; set; }
+    }
+}
