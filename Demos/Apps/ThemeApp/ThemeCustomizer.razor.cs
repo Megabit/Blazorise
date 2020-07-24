b4ba9950d2ba8614321fb74c154c0f00c13402ff
Willian Gruber <willian.gruber@hotmail.com>
2020-07-24 14:56:35 -0300

    Implemented preview for Snackbar

--- Demos/Apps/ThemeApp/ThemeCustomizer.razor.cs
+++ Demos/Apps/ThemeApp/ThemeCustomizer.razor.cs
@@ -1,5 +1,6 @@
 ﻿#region Using directives
 using Blazorise;
+using Blazorise.Snackbar;
 using Microsoft.AspNetCore.Components;
 #endregion
 
@@ -32,6 +33,15 @@ namespace ThemeApp
         private bool ButtonDisabled { get; set; } = false;
         private bool ButtonLoading { get; set; } = false;
         private Modal ModalPreview { get; set; }
+        private Snackbar SnackbarPreview { get; set; }
+        private Snackbar SnackbarPreviewPrimary { get; set; }
+        private Snackbar SnackbarPreviewSecondary { get; set; }
+        private Snackbar SnackbarPreviewSuccess { get; set; }
+        private Snackbar SnackbarPreviewDanger { get; set; }
+        private Snackbar SnackbarPreviewWarning { get; set; }
+        private Snackbar SnackbarPreviewInfo { get; set; }
+        private Snackbar SnackbarPreviewLight { get; set; }
+        private Snackbar SnackbarPreviewDark { get; set; }
         #endregion
     }
 }
