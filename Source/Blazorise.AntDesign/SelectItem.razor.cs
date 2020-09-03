9f3607020610fc9aea6800e971105045040ac991
akusmin <akusmin@gmail.com>
2020-09-03 19:18:11 +0700

    Ant design select component improvements (#1235)
    * Update SUPPORTERS.md
    
    * Update SUPPORTERS.md
    
    * fix: Ant Design styles for Select component
    feat: Supported Loading, AllowClear properties for Select component
    fix: RenderFragment instead of Value on selection
    
    * Revert "fix: Ant Design styles for Select component"
    
    This reverts commit 91315abaded090700272722ba47d0079a79281fe.
    
    * feat: Styles and  Select selection item
    
    * fixed issues
    
    * removed test project
    
    * Fixed formating
    
    * Removed comments from razor
    
    * fix: Remover useless comparer parameter for Ant Select component
    
    * Autoformat
    
    * test page adjustment
    
    * fix: Nullable values support
    
    * fix: Nullable values support
    
    * Removed TestApp from solution
    
    * Formating
    
    * Fixed build errors, probably connected to changes in newest Visual Studio
    
    * Removed AllowClear property from Select
    
    Co-authored-by: Mladen Macanović <mladen.macanovic@gmail.com>
    Co-authored-by: Aleksey Kuzmin <ak@tripally.com>

--- Source/Blazorise.AntDesign/SelectItem.razor.cs
+++ Source/Blazorise.AntDesign/SelectItem.razor.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 #endregion
 
@@ -11,6 +12,8 @@ namespace Blazorise.AntDesign
     public partial class SelectItem<TValue> : Blazorise.SelectItem<TValue>
     {
         #region Members
+
+        private Select<TValue> parentSelect;
 
         #endregion
 
@@ -50,6 +53,32 @@ namespace Blazorise.AntDesign
             => $"{ClassNames} ant-select-item ant-select-item-option {( Selected ? "ant-select-item-option-selected" : "" )} {( Active ? "ant-select-item-option-active" : "" )}";
 
         bool Active { get; set; }
+
+        [CascadingParameter]
+        protected Select<TValue> AntParentSelect
+        {
+            get => parentSelect;
+            set
+            {
+                parentSelect = value;
+
+                // In case of usage object generic type there can be issue to get value by integer key.
+                if ( typeof( TValue ) == typeof( object ) && ( parentSelect?.Items.All( i => !i.Key.Equals( Value ) ) ?? false ) )
+                {
+                    if ( Value is int val )
+                    {
+                        parentSelect?.Items.Add( ((TValue)(object)val.ToString(), ChildContent) );
+
+                        return;
+                    }
+                }
+
+                if ( parentSelect?.Items.All( i => !i.Key.Equals( Value ) ) ?? false )
+                {
+                    parentSelect?.Items.Add( (Value, ChildContent) );
+                }
+            }
+        }
 
         #endregion
     }
