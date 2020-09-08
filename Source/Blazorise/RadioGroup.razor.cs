fb68f602d4159447c7ba713167c3c613946def6a
dfkeenan <dfkeenan@gmail.com>
2020-09-08 21:43:13 +1000

    Alternative RadioGroup fix.

--- Source/Blazorise/RadioGroup.razor.cs
+++ Source/Blazorise/RadioGroup.razor.cs
@@ -79,12 +79,12 @@ namespace Blazorise
         /// <inheritdoc/>
         public override async Task SetParametersAsync( ParameterView parameters )
         {
-            await base.SetParametersAsync( parameters );
-
             if ( parameters.TryGetValue<TValue>( nameof( CheckedValue ), out var result ) )
             {
-                await CurrentValueHandler( result?.ToString() );
+                RadioCheckedChanged?.Invoke( this, new RadioCheckedChangedEventArgs<TValue>( result ) );
             }
+
+            await base.SetParametersAsync( parameters );
         }
 
         #endregion
