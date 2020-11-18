39c9bf5b0d24a76c7922e9784b81f9918cb7f0d6
David-Moreira <david_nogueira_moreira@hotmail.com>
2020-11-18 21:53:41 +0000

    WIP: Added MultiSelect as Column

---
+++ Source/Extensions/Blazorise.DataGrid/DataGridMultiSelectColumn..cs
@@ -0,0 +1,30 @@
+﻿#region Using directives
+
+using Microsoft.AspNetCore.Components;
+
+#endregion Using directives
+
+namespace Blazorise.DataGrid
+{
+    public partial class DataGridMultiSelectColumn<TItem> : DataGridColumn<TItem>
+    {
+        #region Members
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+
+        #region Properties
+
+        public override DataGridColumnType ColumnType => DataGridColumnType.MultiSelect;
+
+        /// <summary>
+        /// Template to customize multi select checkbox.
+        /// </summary>
+        [Parameter] public RenderFragment<MultiSelectContext<TItem>> MultiSelectTemplate { get; set; }
+
+        #endregion
+    }
+}
