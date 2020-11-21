e7e60d2c567ca187f2eb2db58ec18df21b9992d6
Mladen Macanovic <mladen.macanovic@gmail.com>
2020-11-21 11:24:03 +0100

    Implemented ComponentActivator class

---
+++ Source/Blazorise/ComponentActivator.cs
@@ -0,0 +1,50 @@
+﻿#region Using directives
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+#endregion
+
+namespace Blazorise
+{
+    public class ComponentActivator : IComponentActivator
+    {
+        #region Constructors
+
+        public ComponentActivator( IServiceProvider serviceProvider )
+        {
+            ServiceProvider = serviceProvider;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public IComponent CreateInstance( Type componentType )
+        {
+            var instance = ServiceProvider.GetService( componentType );
+
+            if ( instance == null )
+            {
+                instance = Activator.CreateInstance( componentType );
+            }
+
+            if ( !( instance is IComponent component ) )
+            {
+                throw new ArgumentException( $"The type {componentType.FullName} does not implement {nameof( IComponent )}.", nameof( componentType ) );
+            }
+
+            return component;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public IServiceProvider ServiceProvider { get; }
+
+        #endregion
+    }
+}
