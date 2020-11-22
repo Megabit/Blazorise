c6e3ca04ca0f2269bca212d7ece529895850dc62
Mladen Macanovic <mladen.macanovic@gmail.com>
2020-11-22 13:31:14 +0100

    Added comments for ComponentActivator

--- Source/Blazorise/ComponentActivator.cs
+++ Source/Blazorise/ComponentActivator.cs
@@ -1,18 +1,22 @@
 ﻿#region Using directives
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
 #endregion
 
 namespace Blazorise
 {
+    /// <summary>
+    /// Blazorise implementation of <see cref="IComponentActivator"/> used to initialize components that
+    /// are registered through the dependency injection.
+    /// </summary>
     public class ComponentActivator : IComponentActivator
     {
         #region Constructors
 
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        /// <param name="serviceProvider">Service provider used to retrieve registered components.</param>
         public ComponentActivator( IServiceProvider serviceProvider )
         {
             ServiceProvider = serviceProvider;
@@ -22,6 +26,11 @@ namespace Blazorise
 
         #region Methods
 
+        /// <summary>
+        /// Created the component for the specified type.
+        /// </summary>
+        /// <param name="componentType">Type of compoment to create.</param>
+        /// <returns>Return the newly created component or raises an exception if the specified typo is invalid.</returns>
         public IComponent CreateInstance( Type componentType )
         {
             var instance = ServiceProvider.GetService( componentType );
@@ -43,6 +52,9 @@ namespace Blazorise
 
         #region Properties
 
+        /// <summary>
+        /// Gets the reference to the <see cref="IServiceProvider"/>.
+        /// </summary>
         public IServiceProvider ServiceProvider { get; }
 
         #endregion
