2811ad90aaea3fc0f9919b3938ede85fe04f756c
Niels Bouma <nielscil@users.noreply.github.com>
2020-12-05 14:33:19 +0100

    LoadingTemplate RenderFragment for buttons (#1525)
    * Update Button.cs
    
    Adding renderfragment for loading content
    
    * Update Button.cs
    
    Added LoadingContent to buildertree in Bootstrap Button
    
    * Added default loading RenderFragment
    
    * LoadingContent renamed to LoadingTemplate and other small optimizations
    
    * Rename ProvideDefaultLoadingTemplate
    
    * Added documentation for Loading(Template)
    
    Co-authored-by: nielscil <niels.bouma@live.nl>
    Co-authored-by: Mladen Macanović <mladen.macanovic@gmail.com>

--- Source/Blazorise.Bootstrap/Button.cs
+++ Source/Blazorise.Bootstrap/Button.cs
@@ -35,21 +35,31 @@ namespace Blazorise.Bootstrap
             builder.Attributes( Attributes );
             builder.ElementReferenceCapture( capturedRef => ElementRef = capturedRef );
 
-            if ( Loading )
+            if ( Loading && LoadingTemplate != null )
+            {
+                builder.Content( LoadingTemplate );
+            }
+            else
+            {
+                builder.Content( ChildContent );
+            }
+
+            builder.CloseElement();
+        }
+
+        /// <inheritdoc/>
+        protected override RenderFragment ProvideDefaultLoadingTemplate()
+        {
+            return builder =>
             {
                 builder.OpenElement( "span" );
-
                 builder
                     .Class( "spinner-border spinner-border-sm" )
                     .Role( "status" )
                     .AriaHidden( "true" );
-
                 builder.CloseElement();
-            }
-
-            builder.Content( ChildContent );
-
-            builder.CloseElement();
+                builder.Content( ChildContent );
+            };
         }
 
         #endregion
