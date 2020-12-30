b637ff84bf55cfda1f4708bc3ca94f63101f83e9
Ricardo Romão Soares <romaosoares@hotmail.com>
2020-12-29 23:04:15 -0300

    Validate the characters for '*' mask symbel Organize the code, put correlate methods close one another

--- Source/Blazorise/Components/MaskEdit/MaskEdit.razor.cs
+++ Source/Blazorise/Components/MaskEdit/MaskEdit.razor.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
@@ -22,7 +23,7 @@ namespace Blazorise
         #endregion
 
         #region Methods
-
+        // inherits
         protected override Task OnInitializedAsync()
         {
             SetPositions();
@@ -37,11 +38,13 @@ namespace Blazorise
             return base.OnInitializedAsync();
         }
 
+        // inherits
         protected override async Task OnFirstAfterRenderAsync()
         {
             await Task.CompletedTask;
         }
 
+        //inherits
         protected async Task OnKeyPressHandler( KeyboardEventArgs e )
         {
             caretPosition = await JSRunner.GetCaret( ElementRef );
@@ -87,6 +90,9 @@ namespace Blazorise
                 if ( EditMask[caretPosition] == '9' )
                     if ( !char.IsDigit( value[caretPosition] ) )
                         return;
+                if ( EditMask[caretPosition] == '*' )
+                    if ( !char.IsLetterOrDigit( value[caretPosition] ) )
+                        return;
             }
 
             await CurrentValueHandler( value );
@@ -97,87 +103,11 @@ namespace Blazorise
                 await JSRunner.SetCaret( ElementRef, caretPosition + 1 );
         }
 
-        /// <summary>
-        /// Clear the mask to be rebuild
-        /// </summary>
-        /// <param name="value">The value in construction</param>
-        /// <returns></returns>
-        private string ClearCurrencyMask( string value )
-        {
-            return new string( value.Where( c => char.IsDigit( c ) ).ToArray() );
-        }
-
-        /// <summary>
-        /// Clear mask 
-        /// </summary>
-        /// <param name="value">value in construction</param>
-        /// <returns></returns>
-        private string ClearMask( string value )
-        {
-            foreach ( var position in positions )
-                if ( value.Length >= position.Key + 1 )
-                {
-                    var maskValue = position.Value.ToString();
-                    if ( value.Substring( position.Key, 1 ) == maskValue )
-                        value = value.Remove( position.Key, 1 );
-                }
-            return value;
-        }
-
-        /// <summary>
-        /// Do a simple currency format
-        /// </summary>
-        /// <param name="value">value in construction</param>
-        /// <returns></returns>
-        private string DoCurrencyMask( string value )
-        {          
-            var cultureInfo = CultureInfo.GetCultures( CultureTypes.AllCultures )
-                .Where( c => c.NumberFormat.CurrencySymbol == Currency )
-                .First();
-
-            var decimalDigits = cultureInfo.NumberFormat.CurrencyDecimalDigits;
-            var decimalSeparator = cultureInfo.NumberFormat.CurrencyDecimalSeparator;
-            var groupSeparator = cultureInfo.NumberFormat.CurrencyGroupSeparator;         
-          
-            
-            decimal theValue;
-            if ( !decimal.TryParse( value, out theValue ) )
-                return theValue.ToString( "0.00" );
-
-            ///var tempValue = new string( value.Reverse().ToArray() );
-            if ( value.Length > decimalDigits )
-            {
-                var decimalValue = decimal.Parse( value.Substring( value.Length - decimalDigits, decimalDigits ) );
-                var integerValue = long.Parse( value.Substring( 0, value.Length - decimalDigits ) );
-
-                return $"{integerValue.ToString( "#,##0" )}{decimalSeparator}{decimalValue.ToString().PadLeft( decimalDigits, '0' )}";
-            }
-            else
-                return $"0{decimalSeparator}{value.PadLeft( decimalDigits, '0' )}";
-        }
-        /// <summary>
-        /// Check if the number is valid to be a currency value
-        /// </summary>
-        /// <param name="value">value in the construction</param>
-        /// <returns></returns>
-        private bool IsValidCurrencyNumber (string value)
-        {
-            decimal validCurrencyNumber;
-            return decimal.TryParse( value, out validCurrencyNumber );
-        }
-
         //inherits
         protected override Task OnInputHandler( ChangeEventArgs e )
         {
-             e.Value = RemovePositionsFromValue(e.Value.ToString() );
+            e.Value = RemovePositionsFromValue( e.Value.ToString() );
             return base.OnInputHandler( e );
-        }
-
-        private object RemovePositionsFromValue( string value )
-        {
-            while ( positions.ContainsKey( value.Length-1 ) )
-                value = value.Remove( value.Length - 1 );
-            return value;
         }
 
         /// <summary>
@@ -199,6 +129,90 @@ namespace Blazorise
                 }
 
             return value;
+        }
+
+        /// <summary>
+        /// Clear mask 
+        /// </summary>
+        /// <param name="value">value in construction</param>
+        /// <returns></returns>
+        private string ClearMask( string value )
+        {
+            foreach ( var position in positions )
+                if ( value.Length >= position.Key + 1 )
+                {
+                    var maskValue = position.Value.ToString();
+                    if ( value.Substring( position.Key, 1 ) == maskValue )
+                        value = value.Remove( position.Key, 1 );
+                }
+            return value;
+        }
+
+        /// <summary>
+        /// When deleting remove positions (characters) that format the value
+        /// close to the character that was deleted
+        /// </summary>
+        /// <param name="value">Value changed</param>
+        /// <returns></returns>
+        private object RemovePositionsFromValue( string value )
+        {
+            while ( positions.ContainsKey( value.Length - 1 ) )
+                value = value.Remove( value.Length - 1 );
+            return value;
+        }
+
+
+        /// <summary>
+        /// Do a simple currency format
+        /// </summary>
+        /// <param name="value">value in construction</param>
+        /// <returns></returns>
+        private string DoCurrencyMask( string value )
+        {
+            var cultureInfo = CultureInfo.GetCultures( CultureTypes.AllCultures )
+                .Where( c => c.NumberFormat.CurrencySymbol == Currency )
+                .First();
+
+            var decimalDigits = cultureInfo.NumberFormat.CurrencyDecimalDigits;
+            var decimalSeparator = cultureInfo.NumberFormat.CurrencyDecimalSeparator;
+            var groupSeparator = cultureInfo.NumberFormat.CurrencyGroupSeparator;
+
+
+            decimal theValue;
+            if ( !decimal.TryParse( value, out theValue ) )
+                return theValue.ToString( "0.00" );
+
+            ///var tempValue = new string( value.Reverse().ToArray() );
+            if ( value.Length > decimalDigits )
+            {
+                var decimalValue = decimal.Parse( value.Substring( value.Length - decimalDigits, decimalDigits ) );
+                var integerValue = long.Parse( value.Substring( 0, value.Length - decimalDigits ) );
+
+                return $"{integerValue.ToString( "#,##0" )}{decimalSeparator}{decimalValue.ToString().PadLeft( decimalDigits, '0' )}";
+            }
+            else
+                return $"0{decimalSeparator}{value.PadLeft( decimalDigits, '0' )}";
+        }
+
+        /// <summary>
+        /// Clear the mask to be rebuild
+        /// </summary>
+        /// <param name="value">The value in construction</param>
+        /// <returns></returns>
+        private string ClearCurrencyMask( string value )
+        {
+            return new string( value.Where( c => char.IsDigit( c ) ).ToArray() );
+        }
+
+        /// <summary>
+        /// Check if the number is valid to be a currency value
+        /// </summary>
+        /// <param name="value">value in the construction</param>
+        /// <returns></returns>
+        private bool IsValidCurrencyNumber (string value)
+        {
+            decimal validCurrencyNumber;
+            return decimal.TryParse( value, out validCurrencyNumber );
         }
 
         /// <summary>
