bac3c9fc17975326d97beb1ad6d1e69958b70e09
Andrea Tosato <andrea.tosato@hotmail.it>
2021-03-11 21:16:35 +0100

    Rating draft

---
+++ Source/Blazorise/Components/Rating/Rating.razor.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+
+namespace Blazorise
+{
+    public partial class Rating : BaseComponent
+    {
+        [Inject] IIconProvider IconProvider { get; set; }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitializedAsync();
+            FullIcon = IconProvider.GetIconName( IconName.Star );
+            EmptyIcon = IconProvider.GetIconName( IconName.Star );
+        }
+
+        /// <summary>
+        /// User class names for RatingItems, separated by space
+        /// </summary>
+        [Parameter] public string RatingItemsClass { get; set; }
+
+        /// <summary>
+        /// User styles for RatingItems.
+        /// </summary>
+        [Parameter] public string RatingItemsStyle { get; set; }
+
+        /// <summary>
+        /// Input name. If not initialized, name will be random guid.
+        /// </summary>
+        [Parameter] public string Name { get; set; } = Guid.NewGuid().ToString();
+
+        /// <summary>
+        /// Max value and how many elements to click will be generated. Default: 5
+        /// </summary>
+        [Parameter] public int MaxValue { get; set; } = 5;
+
+        /// <summary>
+        /// Selected or hovered icon. Default @Icons.Material.Star
+        /// </summary>
+        [Parameter] public string FullIcon { get; set; }
+
+        /// <summary>
+        /// Non selected item icon. Default @Icons.Material.StarBorder
+        /// </summary>
+        [Parameter] public string EmptyIcon { get; set; }
+
+        /// <summary>
+        /// The color of the component. It supports the theme colors.
+        /// </summary>
+        [Parameter] public Color Color { get; set; } = Color.Primary;
+        /// <summary>
+        /// The Size of the icons.
+        /// </summary>
+        [Parameter] public Size Size { get; set; } = Size.Medium;
+        /// <summary>
+        /// If true, the controls will be disabled.
+        /// </summary>
+        [Parameter] public bool Disabled { get; set; }
+        /// <summary>
+        /// If true, the ratings will show without interactions.
+        /// </summary>
+        [Parameter] public bool ReadOnly { get; set; }
+
+        /// <summary>
+        /// Fires when SelectedValue changes.
+        /// </summary>
+        [Parameter] public EventCallback<int> SelectedValueChanged { get; set; }
+
+        /// <summary>
+        /// Selected value. This property is two-way bindable.
+        /// </summary>
+        [Parameter]
+        public int SelectedValue
+        {
+            get => _selectedValue;
+            set
+            {
+                if ( _selectedValue == value )
+                    return;
+
+                _selectedValue = value;
+
+                SelectedValueChanged.InvokeAsync( _selectedValue );
+            }
+        }
+
+        private int _selectedValue = 0;
+
+        /// <summary>
+        /// Fires when hovered value change. Value will be null if no rating item is hovered.
+        /// </summary>
+        [Parameter] public EventCallback<int?> HoveredValueChanged { get; set; }
+
+        internal int? HoveredValue
+        {
+            get => _hoveredValue;
+            set
+            {
+                if ( _hoveredValue == value )
+                    return;
+
+                _hoveredValue = value;
+                HoveredValueChanged.InvokeAsync( value );
+            }
+        }
+
+        private int? _hoveredValue = null;
+
+        internal bool IsRatingHover => HoveredValue.HasValue;
+
+        private void HandleItemClicked( int itemValue )
+        {
+            SelectedValue = itemValue;
+
+            if ( itemValue == 0 )
+            {
+                HoveredValue = null;
+            }
+        }
+
+        private void HandleItemHovered( int? itemValue ) => HoveredValue = itemValue;
+    }
+}
---
+++ Source/Blazorise/Components/Rating/RatingItem.razor.cs
@@ -0,0 +1,135 @@
+﻿using System.Threading.Tasks;
+using Blazorise.Utilities;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace Blazorise
+{
+    public partial class RatingItem : BaseComponent
+    {
+        [CascadingParameter]
+        private Rating Rating { get; set; }
+
+        [Inject] public IClassProvider classProvider { get; set; }
+
+        [Parameter] public int ItemValue { get; set; }
+
+        internal string Name { get; set; }
+
+        internal bool IsActive { get; set; }
+
+        private bool IsChecked => ItemValue == Rating?.SelectedValue;
+
+        /// <summary>
+        /// The Size of the icon.
+        /// </summary>
+        [Parameter] public Size Size { get; set; } = Size.Medium;
+
+        /// <summary>
+        /// The color of the component. It supports the theme colors.
+        /// </summary>
+        [Parameter] public Color Color { get; set; } = Color.Primary;
+
+        /// <summary>
+        /// If true, the controls will be disabled.
+        /// </summary>
+        [Parameter] public bool Disabled { get; set; }
+
+        /// <summary>
+        /// If true, the item will be readonly.
+        /// </summary>
+        [Parameter] public bool ReadOnly { get; set; }
+
+        [Parameter] public IconStyle IconStyle { get; set; }
+
+        /// <summary>
+        /// Fires when element clicked.
+        /// </summary>
+        [Parameter] public EventCallback<int> ItemClicked { get; set; }
+
+        /// <summary>
+        /// Fires when element hovered.
+        /// </summary>
+        [Parameter] public EventCallback<int?> ItemHovered { get; set; }
+
+        protected override void BuildClasses( ClassBuilder builder )
+        {
+            // I need to color icon, but not work fine
+            builder.Append( $"color: {classProvider.ToColor( Color )}" );
+            base.BuildClasses( builder );
+        }
+
+        protected override void OnParametersSet()
+        {
+            base.OnParametersSet();
+            var select = SelectIcon();
+            Name = select.Name;
+            IconStyle = select.IconStyle;
+        }
+
+        private (string Name, IconStyle IconStyle) SelectIcon()
+        {
+            if ( Rating == null )
+                return (null, IconStyle.Regular);
+            if ( Rating.HoveredValue.HasValue && Rating.HoveredValue.Value >= ItemValue )
+            {
+                // full icon when @RatingItem hovered
+                return (Rating.FullIcon, IconStyle.Solid);
+            }
+            else if ( Rating.SelectedValue >= ItemValue )
+            {
+                if ( Rating.HoveredValue.HasValue && Rating.HoveredValue.Value < ItemValue )
+                {
+                    // empty icon when equal or higher RatingItem value clicked, but less value hovered 
+                    return (Rating.EmptyIcon, IconStyle.Regular);
+                }
+                else
+                {
+                    // full icon when equal or higher RatingItem value clicked
+                    return (Rating.FullIcon, IconStyle.Solid);
+                }
+            }
+            else
+            {
+                // empty icon when this or higher RatingItem is not clicked and not hovered
+                return (Rating.EmptyIcon, IconStyle.Regular);
+            }
+        }
+
+        // rating item lose hover
+        private async Task HandleMouseOut( MouseEventArgs e )
+        {
+            if ( Disabled )
+                return;
+            if ( Rating == null )
+                return;
+
+            IsActive = false;
+            await ItemHovered.InvokeAsync( null );
+        }
+
+        private void HandleMouseOver( MouseEventArgs e )
+        {
+            if ( Disabled )
+                return;
+
+            IsActive = true;
+            ItemHovered.InvokeAsync( ItemValue );
+        }
+
+        private void HandleClick( MouseEventArgs e )
+        {
+            if ( Disabled )
+                return;
+            IsActive = false;
+            if ( Rating?.SelectedValue == ItemValue )
+            {
+                ItemClicked.InvokeAsync( 0 );
+            }
+            else
+            {
+                ItemClicked.InvokeAsync( ItemValue );
+            }
+        }
+    }
+}
