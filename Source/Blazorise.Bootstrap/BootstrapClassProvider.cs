23aa8b5b8140b5cb3a34f395d8389e4d4e050958
Mladen Macanović <mladen.macanovic@gmail.com>
2021-03-15 16:27:20 +0100

    MemoEdit Size parameter is not applied (#2037)

--- Source/Blazorise.Bootstrap/BootstrapClassProvider.cs
+++ Source/Blazorise.Bootstrap/BootstrapClassProvider.cs
@@ -26,6 +26,8 @@ namespace Blazorise.Bootstrap
 
         public override string MemoEdit() => "form-control";
 
+        public override string MemoEditSize( Size size ) => $"form-control-{ToSize( size )}";
+
         public override string MemoEditValidation( ValidationStatus validationStatus ) => ToValidationStatus( validationStatus );
 
         #endregion
