7d2e42494bec3134a6fe4a0cfff766a8cdfdb39d
Mladen Macanovic <mladen.macanovic@gmail.com>
2021-04-26 22:31:28 +0200

    Add SIze parameter on ModalTitle

--- Source/Blazorise/Components/Modal/ModalTitle.razor.cs
+++ Source/Blazorise/Components/Modal/ModalTitle.razor.cs
@@ -10,6 +10,12 @@ namespace Blazorise
     /// </summary>
     public partial class ModalTitle : BaseComponent
     {
+        #region Members
+
+        private HeadingSize size = HeadingSize.Is4;
+
+        #endregion
+
         #region Methods
 
         /// <inheritdoc/>
@@ -23,6 +29,40 @@ namespace Blazorise
         #endregion
 
         #region Properties
+
+        /// <summary>
+        /// Gets the title tag name.
+        /// </summary>
+        protected string TagName => $"h{SizeNumber}";
+
+        /// <summary>
+        /// Gets the heading size number.
+        /// </summary>
+        protected string SizeNumber => Size switch
+        {
+            HeadingSize.Is1 => "1",
+            HeadingSize.Is2 => "2",
+            HeadingSize.Is3 => "3",
+            HeadingSize.Is4 => "4",
+            HeadingSize.Is5 => "5",
+            HeadingSize.Is6 => "6",
+            _ => "3",
+        };
+
+        /// <summary>
+        /// Gets or sets the title size.
+        /// </summary>
+        [Parameter]
+        public HeadingSize Size
+        {
+            get => size;
+            set
+            {
+                size = value;
+
+                DirtyClasses();
+            }
+        }
 
         /// <summary>
         /// Specifies the content to be rendered inside this <see cref="ModalTitle"/>.
