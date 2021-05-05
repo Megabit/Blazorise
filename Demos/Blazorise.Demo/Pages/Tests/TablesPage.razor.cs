bd8fe17502f51ce37194af0aa33007be0d50eb55
Mladen Macanovic <mladen.macanovic@gmail.com>
2021-05-05 21:49:29 +0200

    Optimize table demo page

---
+++ Demos/Blazorise.Demo/Pages/Tests/TablesPage.razor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace Blazorise.Demo.Pages.Tests
+{
+    public partial class TablesPage
+    {
+        bool fixedHeader = true;
+        bool stripped;
+        bool bordered;
+        bool borderless;
+        bool hoverable;
+        bool small;
+
+        class TableUser
+        {
+            public string Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Social { get; set; }
+
+            public TableUser( string id, string firstName, string lastName, string social )
+            {
+                Id = id;
+                FirstName = firstName;
+                LastName = lastName;
+                Social = social;
+            }
+        }
+
+        List<TableUser> tableUsers = new List<TableUser>
+        {
+            new TableUser("1", "Mark", "Otto", "@mdo"),
+            new TableUser("2", "Jacob", "Thornton", "@fat"),
+            new TableUser("3", "Larry", "the Bird", "@twitter"),
+        };
+    }
+}
