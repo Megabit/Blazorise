d3947763823eefc5d1d0bc7f2b859eeb806dfa9a
David-Moreira <david_nogueira_moreira@hotmail.com>
2021-05-23 16:12:01 +0100

    Virtualize Options: FixedHeaderTableHeight

--- Source/Extensions/Blazorise.DataGrid/Configuration/VirtualizeOptions.cs
+++ Source/Extensions/Blazorise.DataGrid/Configuration/VirtualizeOptions.cs
@@ -2,5 +2,9 @@
 {
     public class VirtualizeOptions
     {
+        /// <summary>
+        /// Sets the table height when <see cref="DataGrid.DataGrid{TItem}.Virtualize"/> feature is enabled (defaults to 500px).
+        /// </summary>
+        public string TableHeight { get; set; } = "500px";
     }
 }
