e8439f64c4a3446fe764f48cb6675193e7bfce6e
Mladen Macanovic <mladen.macanovic@gmail.com>
2021-05-28 10:58:04 +0200

    Remove eFrolic from nugets

--- NuGet/dotnet-pack.bat
+++ NuGet/dotnet-pack.bat
@@ -6,7 +6,6 @@ dotnet pack ../Source/Blazorise.Bootstrap/Blazorise.Bootstrap.csproj --force -o 
 dotnet pack ../Source/Blazorise.Material/Blazorise.Material.csproj --force -o . -c Release -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
 dotnet pack ../Source/Blazorise.Bulma/Blazorise.Bulma.csproj --force -o . -c Release -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
 dotnet pack ../Source/Blazorise.AntDesign/Blazorise.AntDesign.csproj --force -o . -c Release -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
-dotnet pack ../Source/Blazorise.Frolic/Blazorise.Frolic.csproj --force -o . -c Release -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
 
 dotnet pack ../Source/Extensions/Blazorise.Animate/Blazorise.Animate.csproj --force -o . -c Release -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
 dotnet pack ../Source/Extensions/Blazorise.Charts/Blazorise.Charts.csproj --force -o . -c Release -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
