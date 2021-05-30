c64d17b58c014eda64690e95c5be85b3171ba5a7
David-Moreira <david_nogueira_moreira@hotmail.com>
2021-05-30 10:48:27 +0100

    Virtualize : Changing _DataGridRowDisplay component to use composition over inheritance for performance

--- Source/Extensions/Blazorise.DataGrid/_DataGridRowDisplay.razor.cs
+++ Source/Extensions/Blazorise.DataGrid/_DataGridRowDisplay.razor.cs
@@ -10,8 +10,10 @@ using Microsoft.AspNetCore.Components;
 
 namespace Blazorise.DataGrid
 {
-    public partial class _DataGridRowDisplay<TItem> : _DataGridRow<TItem>
+    public partial class _DataGridRowDisplay<TItem>
     {
+
+        #region Properties
 
         /// <summary>
         /// Gets or sets whether it should display an EditRow.
@@ -42,6 +44,60 @@ namespace Blazorise.DataGrid
         /// Gets or sets the DetailRowTemplate.
         /// </summary>
         [Parameter] public RenderFragment<TItem> DetailRowTemplate { get; set; }
+
+        /// <summary>
+        /// Item associated with the data set.
+        /// </summary>
+        [Parameter] public TItem Item { get; set; }
+
+        /// <summary>
+        /// List of columns used to build this row.
+        /// </summary>
+        [Parameter] public IEnumerable<DataGridColumn<TItem>> Columns { get; set; }
+
+        /// <summary>
+        /// List of display columns used to build the view row.
+        /// </summary>
+        [Parameter] public IEnumerable<DataGridColumn<TItem>> DisplayableColumns { get; set; }
+
+        /// <summary>
+        /// Occurs after the row is selected.
+        /// </summary>
+        [Parameter] public EventCallback<TItem> Selected { get; set; }
+
+        /// <summary>
+        /// Occurs after the row is clicked.
+        /// </summary>
+        [Parameter] public EventCallback<DataGridRowMouseEventArgs<TItem>> Clicked { get; set; }
+
+        /// <summary>
+        /// Occurs after the row is double clicked.
+        /// </summary>
+        [Parameter] public EventCallback<DataGridRowMouseEventArgs<TItem>> DoubleClicked { get; set; }
+
+        /// <summary>
+        /// Activates the multi select command.
+        /// </summary>
+        [Parameter] public EventCallback<MultiSelectEventArgs<TItem>> MultiSelect { get; set; }
+
+        /// <summary>
+        /// Gets or sets the applied cursor when the row is hovered over.
+        /// </summary>
+        [Parameter] public Cursor HoverCursor { get; set; }
+
+        [Parameter]
+        public EventCallback<TItem> Edit { get; set; }
+
+        [Parameter]
+        public EventCallback<TItem> Delete { get; set; }
+
+        [Parameter]
+        public EventCallback Save { get; set; }
+
+        [Parameter]
+        public EventCallback Cancel { get; set; }
+
+        #endregion
 
     }
 }
