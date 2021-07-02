21c4ef05a4db2e40150720f75e9cea87d25edad0
Mladen Macanović <mladen.macanovic@gmail.com>
2021-07-02 15:27:02 +0200

    Markdown component (#2544)
    * Markdown component
    
    * Add Markdown to Nugets build
    
    * Revert Markdown to Nugets build

--- NuGet/dotnet-pack.bat
+++ NuGet/dotnet-pack.bat
@@ -17,6 +17,7 @@ dotnet pack ../Source/Extensions/Blazorise.Components/Blazorise.Components.cspro
 dotnet pack ../Source/Extensions/Blazorise.TreeView/Blazorise.TreeView.csproj -o . -c Release -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
 dotnet pack ../Source/Extensions/Blazorise.RichTextEdit/Blazorise.RichTextEdit.csproj -o . -c Release -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
 dotnet pack ../Source/Extensions/Blazorise.SpinKit/Blazorise.SpinKit.csproj -o . -c Release -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
+dotnet pack ../Source/Extensions/Blazorise.Markdown/Blazorise.Markdown.csproj -o . -c Release -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
 
 dotnet pack ../Source/Extensions/Blazorise.Icons.FontAwesome/Blazorise.Icons.FontAwesome.csproj -o . -c Release -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
 dotnet pack ../Source/Extensions/Blazorise.Icons.Material/Blazorise.Icons.Material.csproj -o . -c Release -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
