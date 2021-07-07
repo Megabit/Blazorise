07a5ba60c0d91c03524d26743c6a1021a6626483
Simon Cropp <simon.cropp@gmail.com>
2021-07-07 18:43:34 +1000

    Remove some redundant usings (#2570)
    * remove some redundant usings
    
    * fix newlines
    
    * fix newlines
    
    Co-authored-by: Mladen Macanović <mladen.macanovic@gmail.com>

--- Demos/Apps/TodoApp/BaseTodoItems.cs
+++ Demos/Apps/TodoApp/BaseTodoItems.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using Blazorise;
 using Microsoft.AspNetCore.Components;
 
