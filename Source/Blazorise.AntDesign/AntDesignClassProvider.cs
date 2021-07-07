6a12e39852e5a1b3f808e5c9d5426e3218447f51
Simon Cropp <simon.cropp@gmail.com>
2021-07-07 18:51:21 +1000

    remove Interpolation (#2591)
    Co-authored-by: Mladen Macanović <mladen.macanovic@gmail.com>

--- Source/Blazorise.AntDesign/AntDesignClassProvider.cs
+++ Source/Blazorise.AntDesign/AntDesignClassProvider.cs
@@ -632,7 +632,7 @@ namespace Blazorise.AntDesign
 
         public override string BarTogglerIcon( BarMode mode ) => mode == Blazorise.BarMode.Horizontal ? "navbar-toggler-icon" : "navbar-toggler-icon";
 
-        public override string BarDropdownMenu( BarMode mode ) => mode == Blazorise.BarMode.Horizontal ? $"ant-menu ant-menu-sub ant-menu-vertical" : $"ant-menu ant-menu-sub ant-menu-{ToBarMode( mode )}";
+        public override string BarDropdownMenu( BarMode mode ) => mode == Blazorise.BarMode.Horizontal ? "ant-menu ant-menu-sub ant-menu-vertical" : $"ant-menu ant-menu-sub ant-menu-{ToBarMode( mode )}";
 
         public override string BarDropdownMenuVisible( BarMode mode, bool visible ) => visible ? null : "ant-menu-hidden";
 
@@ -691,7 +691,7 @@ namespace Blazorise.AntDesign
         public override string Column( ColumnWidth columnWidth, Breakpoint breakpoint, bool offset )
         {
             // AntDesign requires for base ant-col class to be always defined.
-            var sb = new StringBuilder( $"ant-col" );
+            var sb = new StringBuilder( "ant-col" );
 
             if ( breakpoint != Blazorise.Breakpoint.None )
                 sb.Append( $"-{ToBreakpoint( breakpoint )}" );
