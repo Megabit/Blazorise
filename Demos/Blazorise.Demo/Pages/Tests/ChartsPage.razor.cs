80649b8db1849d30a45ff8de619a07669f728ee9
Mladen Macanovic <mladen.macanovic@gmail.com>
2021-07-19 11:59:10 +0200

    Comment CallbackJavaScript example

--- Demos/Blazorise.Demo/Pages/Tests/ChartsPage.razor.cs
+++ Demos/Blazorise.Demo/Pages/Tests/ChartsPage.razor.cs
@@ -15,22 +15,22 @@ namespace Blazorise.Demo.Pages.Tests
         Chart<double> polarAreaChart;
         Chart<double> radarChart;
 
-        LineChartOptions lineChartOptions = new()
-        {
-            Scales = new()
-            {
-                YAxes = new()
-                {
-                    new()
-                    {
-                        Ticks = new AxisTicks
-                        {
-                            CallbackJavaScript = "`${value / 1000} ${index} K`"
-                        }
-                    }
-                }
-            }
-        };
+        //LineChartOptions lineChartOptions = new()
+        //{
+        //    Scales = new()
+        //    {
+        //        YAxes = new()
+        //        {
+        //            new()
+        //            {
+        //                Ticks = new AxisTicks
+        //                {
+        //                    CallbackJavaScript = "`${value / 1000} ${index} K`"
+        //                }
+        //            }
+        //        }
+        //    }
+        //};
 
         LineChart<double> lineChartWithData;
 
