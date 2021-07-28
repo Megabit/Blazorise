1af1e5ce8b8e1d62b756a215abfa49ca75f9fae3
David-Moreira <david_nogueira_moreira@hotmail.com>
2021-07-28 21:21:57 +0100

    Revert "Demo Bootstrap.RC serve Gzip example"
    This reverts commit d8f0b57605719af8a623c4f40b15bd227f3a351a.

--- Demos/Blazorise.Demo.Bootstrap.RC/Startup.cs
+++ Demos/Blazorise.Demo.Bootstrap.RC/Startup.cs
@@ -3,7 +3,6 @@ using Blazorise.Icons.FontAwesome;
 using Blazorise.RichTextEdit;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -53,30 +52,6 @@ namespace Blazorise.Demo.Bootstrap.RC
             }
 
             app.UseHttpsRedirection();
-            //Gzip configuration
-            app.UseStaticFiles( new StaticFileOptions
-            {
-                
-                OnPrepareResponse = context =>
-                {
-                    IHeaderDictionary headers = context.Context.Response.Headers;
-                    string contentType = headers["Content-Type"];
-                    if ( contentType == "application/x-gzip" )
-                    {
-                        if ( context.File.Name.EndsWith( "js.gz" ) )
-                        {
-                            contentType = "application/javascript";
-                        }
-                        else if ( context.File.Name.EndsWith( "css.gz" ) )
-                        {
-                            contentType = "text/css";
-                        }
-                        headers.Add( "Content-Encoding", "gzip" );
-                        headers["Content-Type"] = contentType;
-                    }
-                }
-            } );
-            //Serves blazor js file
             app.UseStaticFiles();
 
             app.UseRouting();
