0b0220557f9398c2e892b5229685547a94cf2ee1
David <david_nogueira_moreira@hotmail.com>
2021-08-01 11:50:51 +0100

    ValidationItem Initializer Fix (#2756)
    * First version
    
    * Applied Fix | Added benchmark | Applied BindingFlags for Public Instances only, less work
    
    * String is kinda of an offcase for ReferenceTypes, add it as an exception like a ValueType.
    
    * Added list as an offcase
    
    * Ran cleanup on test project and affected files
    
    * Revert "Ran cleanup on test project and affected files"
    
    This reverts commit 0d5b6efa108801c1cf79853bc44784a1a5ca9330.
    
    * Refactored
    
    * Removed Leftover space...
    
    * Move IsListOrCollection to existing extensions class
    
    * Comments
    
    Co-authored-by: Mladen Macanovic <mladen.macanovic@gmail.com>

---
+++ Source/Extensions/Blazorise.DataGrid/Utils/RecursiveObjectActivator.cs
@@ -0,0 +1,47 @@
+﻿#region Using directives
+using System;
+using System.Reflection;
+#endregion
+
+namespace Blazorise.DataGrid.Utils
+{
+    public static class RecursiveObjectActivator
+    {
+        #region Methods
+
+        /// <summary>
+        /// Creates an instance of a complex object and it's public instance properties.
+        /// Ignores lists, valuetypes and strings
+        /// </summary>
+        /// <typeparam name="TItem">The type to create.</typeparam>
+        /// <returns>A reference to the newly created object.</returns>
+        public static TItem CreateInstance<TItem>()
+        {
+            var obj = Activator.CreateInstance<TItem>();
+            var properties = typeof( TItem ).GetProperties( BindingFlags.Public | BindingFlags.Instance );
+            CreateInstanceRecursive( obj, properties );
+
+            return obj;
+        }
+
+        private static void CreateInstanceRecursive( object currObjInstance, PropertyInfo[] properties )
+        {
+            foreach ( var property in properties )
+            {
+                var currType = property.PropertyType;
+                if ( !currType.IsValueType && currType != typeof( string ) && !TypeChecker.IsListOrCollection( currType ) )
+                {
+                    var instanced = property.GetValue( currObjInstance );
+                    if ( instanced is null )
+                    {
+                        instanced = Activator.CreateInstance( currType );
+                        property.SetValue( currObjInstance, instanced );
+                    }
+                    CreateInstanceRecursive( instanced, currType.GetProperties( BindingFlags.Public | BindingFlags.Instance ) );
+                }
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Tests/Blazorise.Benchmark/DataGrid/ReflectionBenchmark.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using BenchmarkDotNet.Attributes;
+using Blazorise.DataGrid.Utils;
+
+namespace Blazorise.Benchmark.DataGrid
+{
+    [MemoryDiagnoser]
+    public class ReflectionBenchmark
+    {
+        [Benchmark]
+        public Test InitObject_WithComplexObject_Returns_FullyInstancedObject()
+            => RecursiveObjectActivator.CreateInstance<Test>();
+
+        [Benchmark]
+        public Test InitObject_Standard()
+            => Activator.CreateInstance<Test>();
+
+        public class Test
+        {
+            public NestedTest NestedTest { get; set; }
+            public NestedTest AnotherNestedTest { get; set; }
+
+            public string UnusualReferenceType { get; set; }
+
+            public int SomeValueType { get; set; }
+
+            public int? SomeNullableValueType { get; set; }
+
+            public List<NestedTest> List { get; set; }
+
+            private NestedTest PrivateNestedTest { get; set; }
+
+            protected NestedTest ProtectedNestedTest { get; set; }
+
+            internal NestedTest InternalNestedTest { get; set; }
+        }
+
+        public class NestedTest
+        {
+            public FurtherNestedTest FurtherNestedTest { get; set; }
+            public FurtherNestedTest AnotherFurtherNestedTest { get; set; }
+
+            public string UnusualReferenceType { get; set; }
+
+            public int SomeValueType { get; set; }
+
+            public int? SomeNullableValueType { get; set; }
+            public List<NestedTest> List { get; set; }
+        }
+
+        public class FurtherNestedTest
+        {
+            public string UnusualReferenceType { get; set; }
+
+            public int SomeValueType { get; set; }
+
+            public int? SomeNullableValueType { get; set; }
+
+            public List<NestedTest> List { get; set; }
+        }
+    }
+}
