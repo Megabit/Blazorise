e8e9fee2792196e95f0b1bcf69bf651a01ef0c81
Mladen Macanovic <mladen.macanovic@gmail.com>
2021-08-03 11:54:38 +0200

    ListGroup page

--- Documentation/Blazorise.Docs/Models/Snippets.generated.cs
+++ Documentation/Blazorise.Docs/Models/Snippets.generated.cs
@@ -887,6 +887,99 @@ namespace Blazorise.Docs.Models
     Link
 </Link>";
 
+        public const string BasicListGroupExample = @"<ListGroup>
+    <ListGroupItem>An item</ListGroupItem>
+    <ListGroupItem>A second item</ListGroupItem>
+    <ListGroupItem>A third item</ListGroupItem>
+    <ListGroupItem>A fourth item</ListGroupItem>
+    <ListGroupItem Disabled=""true"">A disabled item</ListGroupItem>
+</ListGroup>";
+
+        public const string ListGroupBadgesExamples = @"<ListGroup Flush>
+    <ListGroupItem Flex=""Flex.JustifyContent.Between.AlignItems.Center"">
+        A list item
+        <Badge Color=""Color.Primary"" Pill=""true"">14</Badge>
+    </ListGroupItem>
+    <ListGroupItem Flex=""Flex.JustifyContent.Between.AlignItems.Center"">
+        A second list item
+        <Badge Color=""Color.Primary"" Pill=""true"">2</Badge>
+    </ListGroupItem>
+    <ListGroupItem Flex=""Flex.JustifyContent.Between.AlignItems.Center"">
+        A third list item
+        <Badge Color=""Color.Primary"" Pill=""true"">1</Badge>
+    </ListGroupItem>
+</ListGroup>";
+
+        public const string ListGroupContextualExample = @"<ListGroup>
+    <ListGroupItem Color=""Color.None"">None</ListGroupItem>
+    <ListGroupItem Color=""Color.Primary"">Primary</ListGroupItem>
+    <ListGroupItem Color=""Color.Secondary"">Secondary</ListGroupItem>
+    <ListGroupItem Color=""Color.Success"">Success</ListGroupItem>
+    <ListGroupItem Color=""Color.Danger"">Danger</ListGroupItem>
+    <ListGroupItem Color=""Color.Warning"">Warning</ListGroupItem>
+    <ListGroupItem Color=""Color.Info"">Info</ListGroupItem>
+    <ListGroupItem Color=""Color.Light"">Light</ListGroupItem>
+    <ListGroupItem Color=""Color.Dark"">Dark</ListGroupItem>
+</ListGroup>";
+
+        public const string ListGroupContextualSelectableExample = @"<ListGroup Mode=""ListGroupMode.Selectable"">
+    <ListGroupItem Name=""none"" Color=""Color.None"">None</ListGroupItem>
+    <ListGroupItem Name=""primary"" Color=""Color.Primary"">Primary</ListGroupItem>
+    <ListGroupItem Name=""secondary"" Color=""Color.Secondary"">Secondary</ListGroupItem>
+    <ListGroupItem Name=""success"" Color=""Color.Success"">Success</ListGroupItem>
+    <ListGroupItem Name=""danger"" Color=""Color.Danger"">Danger</ListGroupItem>
+    <ListGroupItem Name=""warning"" Color=""Color.Warning"">Warning</ListGroupItem>
+    <ListGroupItem Name=""info"" Color=""Color.Info"">Info</ListGroupItem>
+    <ListGroupItem Name=""light"" Color=""Color.Light"">Light</ListGroupItem>
+    <ListGroupItem Name=""dark"" Color=""Color.Dark"">Dark</ListGroupItem>
+</ListGroup>";
+
+        public const string ListGroupCustomContentExample = @"<ListGroup Flush=""true"">
+    <ListGroupItem>
+        <Div Flex=""Flex.JustifyContent.Between"" Width=""Width.Is100"">
+            <Heading Size=""HeadingSize.Is5"" Margin=""Margin.Is1.FromBottom"">List group item heading</Heading>
+            <Small>3 days ago</Small>
+        </Div>
+        <Paragraph Margin=""Margin.Is1.FromBottom"">Some placeholder content in a paragraph.</Paragraph>
+        <Small>And some small print.</Small>
+    </ListGroupItem>
+    <ListGroupItem>
+        <Div Flex=""Flex.JustifyContent.Between"" Width=""Width.Is100"">
+            <Heading Size=""HeadingSize.Is5"" Margin=""Margin.Is1.FromBottom"">List group item heading</Heading>
+            <Small TextColor=""TextColor.Muted"">3 days ago</Small>
+        </Div>
+        <Paragraph Margin=""Margin.Is1.FromBottom"">Some placeholder content in a paragraph.</Paragraph>
+        <Small TextColor=""TextColor.Muted"">And some muted small print.</Small>
+    </ListGroupItem>
+    <ListGroupItem>
+        <Div Flex=""Flex.JustifyContent.Between"" Width=""Width.Is100"">
+            <Heading Size=""HeadingSize.Is5"" Margin=""Margin.Is1.FromBottom"">List group item heading</Heading>
+            <Small TextColor=""TextColor.Muted"">3 days ago</Small>
+        </Div>
+        <Paragraph Margin=""Margin.Is1.FromBottom"">Some placeholder content in a paragraph.</Paragraph>
+        <Small TextColor=""TextColor.Muted"">And some muted small print.</Small>
+    </ListGroupItem>
+</ListGroup>";
+
+        public const string ListGroupFlushExample = @"<ListGroup Flush=""true"">
+    <ListGroupItem>An item</ListGroupItem>
+    <ListGroupItem>A second item</ListGroupItem>
+    <ListGroupItem>A third item</ListGroupItem>
+    <ListGroupItem>A fourth item</ListGroupItem>
+    <ListGroupItem Disabled=""true"">A disabled item</ListGroupItem>
+</ListGroup>";
+
+        public const string ListGroupSelectableExample = @"<ListGroup Mode=""ListGroupMode.Selectable"" @bind-SelectedItem=""selectedItem"">
+    <ListGroupItem Name=""first"">An item</ListGroupItem>
+    <ListGroupItem Name=""second"">A second item</ListGroupItem>
+    <ListGroupItem Name=""third"">A third item</ListGroupItem>
+    <ListGroupItem Name=""fourth"">A fourth item</ListGroupItem>
+    <ListGroupItem Name=""fifth"" Disabled=""true"">A disabled item</ListGroupItem>
+</ListGroup>
+@code {
+    private string selectedItem = ""first"";
+}";
+
         public const string BasicMemoEditExample = @"<MemoEdit Rows=""5"" />";
 
         public const string MemoEditTextChangedModeExample = @"public void ConfigureServices( IServiceCollection services )
