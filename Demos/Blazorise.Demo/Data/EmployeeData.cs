a3a4a855534796b215333af415807f0fc5c5e84d
David-Moreira <david_nogueira_moreira@hotmail.com>
2021-08-26 00:05:50 +0100

    Filter Standard Page | Switching load data to memory cache | Trying to avoid deadlocking...

--- Demos/Blazorise.Demo/Data/EmployeeData.cs
+++ Demos/Blazorise.Demo/Data/EmployeeData.cs
@@ -6,40 +6,37 @@ using System.Threading;
 using System.Threading.Tasks;
 using Blazorise.Demo.Models;
 using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace Blazorise.Demo.Data
 {
     public class EmployeeData
     {
         private readonly IHttpClientFactory httpClientfactory;
-        private readonly HttpClient httpClient;
         private readonly NavigationManager navigationManager;
+        private IMemoryCache cache;
+        private string employeesCacheKey = "cache_employees";
 
         /// <summary>
         /// Simplified code to get & cache data in memory...
         /// </summary>
         /// <param name="httpClientfactory"></param>
         /// <param name="navigationManager"></param>
-        public EmployeeData( IHttpClientFactory httpClientfactory, NavigationManager navigationManager )
+        public EmployeeData( IHttpClientFactory httpClientfactory, NavigationManager navigationManager, IMemoryCache memoryCache )
         {
             this.httpClientfactory = httpClientfactory;
             this.navigationManager = navigationManager;
-            httpClient = httpClientfactory.CreateClient();
+            this.cache = memoryCache;
+        }
+
+        public Task<List<Employee>> GetDataAsync()
+            => cache.GetOrCreateAsync<List<Employee>>( employeesCacheKey, LoadData );
+
+        private  Task<List<Employee>> LoadData(ICacheEntry cacheEntry )
+        {
+            HttpClient httpClient = httpClientfactory.CreateClient();
             httpClient.BaseAddress = new( navigationManager.BaseUri );
-        }
-
-        private List<Employee> data;
-
-        public async Task<List<Employee>> GetDataAsync()
-        {
-            if ( data is null )
-                await LoadData().ConfigureAwait( false );
-            return data;
-        }
-
-        private async Task LoadData()
-        {
-            data = await httpClient.GetFromJsonAsync<List<Employee>>( "_content/Blazorise.Demo/demoData.json" ).ConfigureAwait( false );
+            return httpClient.GetFromJsonAsync<List<Employee>>( "_content/Blazorise.Demo/demoData.json" );
         }
     }
 
