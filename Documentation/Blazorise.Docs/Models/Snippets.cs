cdca371bbd0ba0b9c61374699b32775a3559c4cf
David-Moreira <david_nogueira_moreira@hotmail.com>
2021-09-02 00:00:41 +0100

    Cache Snippets Fields

--- Documentation/Blazorise.Docs/Models/Snippets.cs
+++ Documentation/Blazorise.Docs/Models/Snippets.cs
@@ -7,9 +7,10 @@ namespace Blazorise.Docs.Models
 {
     public static partial class Snippets
     {
+        private static FieldInfo[] snippetsFields = typeof( Snippets ).GetFields( BindingFlags.Public | BindingFlags.Static | BindingFlags.GetField );
         public static string GetCode( string component )
         {
-            var field = typeof( Snippets ).GetFields( BindingFlags.Public | BindingFlags.Static | BindingFlags.GetField ).FirstOrDefault( f => f.Name == component );
+            var field = snippetsFields.FirstOrDefault( f => f.Name == component );
 
             if ( field == null )
                 return $"Snippet for component '{component}' not found!";
