351be6a65ceb0a2990854d4206425ced3b950e41
Mladen Macanovic <mladen.macanovic@gmail.com>
2021-09-01 14:46:31 +0200

    Hide page progress bar when page is disposed

---
+++ Documentation/Blazorise.Docs/Pages/Docs/Services/PageProgresses/PageProgressPage.razor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace Blazorise.Docs.Pages.Docs.Services.PageProgresses
+{
+    public partial class PageProgressPage : IDisposable
+    {
+        #region Methods
+
+        public void Dispose()
+        {
+            if ( PageProgressService != null )
+            {
+                // setting it to -1 will hide the progress bar
+                PageProgressService.Go( -1 );
+            }
+        }
+
+        #endregion
+
+        #region Properties
+
+        [Inject] private IPageProgressService PageProgressService { get; set; }
+
+        #endregion
+    }
+}
