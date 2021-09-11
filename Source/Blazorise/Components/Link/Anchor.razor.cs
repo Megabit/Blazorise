53e6ffe0ba8478b4bddeef282a6389910b190597
Mladen Macanovic <mladen.macanovic@gmail.com>
2021-09-11 14:35:55 +0200

    Anchor component that inherits from Link

---
+++ Source/Blazorise/Components/Link/Anchor.razor.cs
@@ -0,0 +1,10 @@
+﻿namespace Blazorise
+{
+    /// <summary>
+    /// A component that renders an anchor tag, automatically toggling its 'active'
+    /// class based on whether its 'href' matches the current URI.
+    /// </summary>
+    public partial class Anchor : Link
+    {
+    }
+}
