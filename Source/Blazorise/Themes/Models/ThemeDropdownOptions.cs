88144bb6d8de6ef99c64046b64ad59b266ceba30
Mladen Macanovic <mladen.macanovic@gmail.com>
2021-10-25 16:53:28 +0200

    Nullable theme options

--- Source/Blazorise/Themes/Models/ThemeDropdownOptions.cs
+++ Source/Blazorise/Themes/Models/ThemeDropdownOptions.cs
@@ -8,12 +8,12 @@
         /// <summary>
         /// Defines the percentage of how much the colors will blend together.
         /// </summary>
-        public float GradientBlendPercentage { get; set; } = 15f;
+        public float? GradientBlendPercentage { get; set; } = 15f;
 
         /// <summary>
         /// Defines the visibility of dropdown toggle icon.
         /// </summary>
-        public bool ToggleIconVisible { get; set; } = true;
+        public bool? ToggleIconVisible { get; set; } = true;
 
         /// <summary>
         /// Gets or sets the global size for dropdown components.
