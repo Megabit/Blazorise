0480ca2416be815956640f1a30bbfef552eab02f
Mladen Macanović <mladen.macanovic@gmail.com>
2021-10-26 15:51:51 +0200

    Theme improvements (#3055)
    * Add rgb theme variables
    
    * Better color naming
    
    * Body theme options
    
    * Nullable theme options
    
    * Change default breakpoint options
    
    * Spacing options generator
    
    * Revert test values
    
    * Spacing test
    
    * Spacing for other providers
    
    * Comment spacing options in demo
    
    * Release notes

--- Source/Blazorise/Themes/Models/ThemeListGroupItemOptions.cs
+++ Source/Blazorise/Themes/Models/ThemeListGroupItemOptions.cs
@@ -9,17 +9,17 @@
         /// Defines a level of background color.
         /// </summary>
         /// <remarks>Negative level values will lighten the color, while higher levels will darken.</remarks>
-        public int BackgroundLevel { get; set; } = -9;
+        public int? BackgroundLevel { get; set; } = -9;
 
         /// <summary>
         /// Defines a level of text color.
         /// </summary>
         /// <remarks>Negative level values will lighten the color, while higher levels will darken.</remarks>
-        public int ColorLevel { get; set; } = 6;
+        public int? ColorLevel { get; set; } = 6;
 
         /// <summary>
         /// Defines the percentage of how much the colors will blend together.
         /// </summary>
-        public float GradientBlendPercentage { get; set; } = 15f;
+        public float? GradientBlendPercentage { get; set; } = 15f;
     }
 }
