eee4b0a5fec1429079a4e28232673cedd75ea3d8
David-Moreira <david_nogueira_moreira@hotmail.com>
2021-11-04 23:46:09 +0000

    Fix MemoEdit Not Releasing BaseInputComponent Resources....

--- Source/Blazorise/Base/BaseInputComponent.razor.cs
+++ Source/Blazorise/Base/BaseInputComponent.razor.cs
@@ -89,21 +89,37 @@ namespace Blazorise
         {
             if ( disposing )
             {
-                if ( ParentValidation != null )
-                {
-                    // To avoid leaking memory, it's important to detach any event handlers in Dispose()
-                    ParentValidation.ValidationStatusChanged -= OnValidationStatusChanged;
-                }
-
-                ParentModal?.NotifyFocusableComponentRemoved( this );
-
-                if ( Theme != null )
-                {
-                    Theme.Changed -= OnThemeChanged;
-                }
+                ReleaseResources();
             }
 
             base.Dispose( disposing );
+        }
+
+        /// <inheritdoc/>
+        protected override ValueTask DisposeAsync( bool disposing )
+        {
+            if ( disposing )
+            {
+                ReleaseResources();
+            }
+
+            return base.DisposeAsync( disposing );
+        }
+
+        protected void ReleaseResources()
+        {
+            if ( ParentValidation != null )
+            {
+                // To avoid leaking memory, it's important to detach any event handlers in Dispose()
+                ParentValidation.ValidationStatusChanged -= OnValidationStatusChanged;
+            }
+
+            ParentModal?.NotifyFocusableComponentRemoved( this );
+
+            if ( Theme != null )
+            {
+                Theme.Changed -= OnThemeChanged;
+            }
         }
 
         /// <summary>
