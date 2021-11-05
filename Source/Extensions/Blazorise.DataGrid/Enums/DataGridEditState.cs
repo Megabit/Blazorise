cab42d86d4263030dd52c92550b26f299e20ffb9
Mladen Macanovic <mladen.macanovic@gmail.com>
2021-11-05 12:21:54 +0100

    Move enums to separate files

---
+++ Source/Extensions/Blazorise.DataGrid/Enums/DataGridEditState.cs
@@ -0,0 +1,23 @@
+﻿namespace Blazorise.DataGrid
+{
+    /// <summary>
+    /// States of the row items.
+    /// </summary>
+    public enum DataGridEditState
+    {
+        /// <summary>
+        /// Default state which means the row value will just be showed.
+        /// </summary>
+        None,
+
+        /// <summary>
+        /// Row values will be visible in the new item edit mode.
+        /// </summary>
+        New,
+
+        /// <summary>
+        /// Row values will be visible in the row editing mode.
+        /// </summary>
+        Edit,
+    }
+}
---
+++ Source/Extensions/Blazorise.DataGrid/Enums/DataGridFilterMethod.cs
@@ -0,0 +1,14 @@
+﻿namespace Blazorise.DataGrid
+{
+    /// <summary>
+    /// Defines the type of filter operations.
+    /// </summary>
+    public enum DataGridFilterMethod
+    {
+        Contains,
+        StartsWith,
+        EndsWith,
+        Equals,
+        NotEquals,
+    }
+}
