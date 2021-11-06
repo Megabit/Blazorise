c71d186e0fa01c914f736c9fb0070281fabd3e8c
David-Moreira <david_nogueira_moreira@hotmail.com>
2021-11-06 22:22:28 +0000

    Docs

--- Source/Extensions/Blazorise.DataGrid/Enums/DataGridSelectReason.cs
+++ Source/Extensions/Blazorise.DataGrid/Enums/DataGridSelectReason.cs
@@ -6,10 +6,22 @@ using System.Threading.Tasks;
 
 namespace Blazorise.DataGrid.Enums
 {
+    /// <summary>
+    /// Defines the Select Reason of the DataGrid Selection.
+    /// </summary>
     public enum DataGridSelectReason
     {
+        /// <summary>
+        /// Row has been clicked.
+        /// </summary>
         RowClick,
+        /// <summary>
+        /// Multi select has been triggered.
+        /// </summary>
         MultiSelectClick,
+        /// <summary>
+        /// Multi select all has been triggered.
+        /// </summary>
         MultiSelectAll
     }
 }
