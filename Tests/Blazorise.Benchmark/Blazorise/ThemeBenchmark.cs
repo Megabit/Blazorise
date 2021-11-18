53707390e108e93bd03c7af4be89ffa6d82a7c02
David-Moreira <david_nogueira_moreira@hotmail.com>
2021-11-18 22:29:50 +0000

    Start With Benchmark

---
+++ Tests/Blazorise.Benchmark/Blazorise/ThemeBenchmark.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using BenchmarkDotNet.Attributes;
+using Blazorise.DataGrid.Utils;
+
+namespace Blazorise.Benchmark.Blazorise
+{
+    [MemoryDiagnoser]
+    public class ThemeBenchmark
+    {
+        [Benchmark]
+        public void ExtractHexDigitsBenchmark()
+            => ExtractHexDigits( "#ffffff" );
+
+        [Benchmark]
+        public void CachedExtractHexDigitsBenchmark()
+            => CachedExtractHexDigits( "#ffffff" );
+
+        /// <summary>
+        /// Extract only the hex digits from a string.
+        /// </summary>
+        /// <param name="input">A string to extract.</param>
+        /// <returns>A new hex string.</returns>
+        protected static string ExtractHexDigits( string input )
+        {
+            // remove any characters that are not digits (like #)
+            Regex isHexDigit = new( "[abcdefABCDEF\\d]+", RegexOptions.Compiled );
+            string newnum = "";
+            foreach ( char c in input )
+            {
+                if ( isHexDigit.IsMatch( c.ToString() ) )
+                    newnum += c.ToString();
+            }
+            return newnum;
+        }
+
+        /// <summary>
+        /// Extract only the hex digits from a string.
+        /// </summary>
+        /// <param name="input">A string to extract.</param>
+        /// <returns>A new hex string.</returns>
+        protected static string CachedExtractHexDigits( string input )
+        {
+            string newnum = "";
+            foreach ( char c in input )
+            {
+                if ( isHexDigit.IsMatch( c.ToString() ) )
+                    newnum += c.ToString();
+            }
+            return newnum;
+        }
+
+
+        private static Regex isHexDigit = new( "[abcdefABCDEF\\d]+", RegexOptions.Compiled );
+
+    }
+
+}
