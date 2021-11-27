4a26977b83387ac65db988518918748671c7cfe1
David-Moreira <david_nogueira_moreira@hotmail.com>
2021-11-27 18:38:56 +0000

    DataGrid cts dispose

--- Source/Extensions/Blazorise.DataGrid/DataGrid.razor.cs
+++ Source/Extensions/Blazorise.DataGrid/DataGrid.razor.cs
@@ -254,6 +254,8 @@ namespace Blazorise.DataGrid
                 {
                     paginationContext.UnsubscribeOnPageSizeChanged( OnPageSizeChanged );
                     paginationContext.UnsubscribeOnPageChanged( OnPageChanged );
+                    paginationContext.CancellationTokenSource?.Dispose();
+                    paginationContext.CancellationTokenSource = null;
                 }
             }
 
