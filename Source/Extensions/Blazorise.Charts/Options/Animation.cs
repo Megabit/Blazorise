472c6591c11714406e66e36c9e03c72c4bac7856
Mladen Macanovic <mladen.macanovic@gmail.com>
2021-12-02 18:20:08 +0100

    Animation options

--- Source/Extensions/Blazorise.Charts/Options/Animation.cs
+++ Source/Extensions/Blazorise.Charts/Options/Animation.cs
@@ -16,9 +16,21 @@ namespace Blazorise.Charts
         public int? Duration { get; set; } = 1000;
 
         /// <summary>
-        /// Easing function to use (https://www.chartjs.org/docs/latest/configuration/animations.html#easing).
+        /// Easing function to use. <see href="https://www.chartjs.org/docs/latest/configuration/animations.html#easing">more...</see>
         /// </summary>
         [JsonIgnore( Condition = JsonIgnoreCondition.WhenWritingNull )]
         public string Easing { get; set; } = "easeOutQuart";
+
+        /// <summary>
+        /// Delay before starting the animations.
+        /// </summary>
+        [JsonIgnore( Condition = JsonIgnoreCondition.WhenWritingNull )]
+        public int? Delay { get; set; }
+
+        /// <summary>
+        /// If set to true, the animations loop endlessly.
+        /// </summary>
+        [JsonIgnore( Condition = JsonIgnoreCondition.WhenWritingNull )]
+        public bool? Loop { get; set; }
     }
 }
