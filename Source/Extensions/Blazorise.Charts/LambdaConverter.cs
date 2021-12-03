ea612097aefc5a2bbba1120ec0c83bd4959547d9
Mladen Macanovic <mladen.macanovic@gmail.com>
2021-12-03 15:09:03 +0100

    Share one StringBuilder

--- Source/Extensions/Blazorise.Charts/LambdaConverter.cs
+++ Source/Extensions/Blazorise.Charts/LambdaConverter.cs
@@ -25,12 +25,12 @@ namespace Blazorise.Charts
             writer.WriteStringValue( result );
         }
 
-        static string BuildFunction( Expression<T> expression, string jsBody )
+        private static string BuildFunction( Expression<T> expression, string jsBody )
         {
             var sb = new StringBuilder();
 
             sb.Append( "function(" );
-            sb.Append( BuildFunctionParameters( expression ) );
+            BuildFunctionParameters( sb, expression );
             sb.Append( ") {" );
 
             sb.Append( "return " );
@@ -40,100 +40,18 @@ namespace Blazorise.Charts
             return sb.ToString();
         }
 
-        static string BuildFunctionParameters( Expression<T> value )
+        private static void BuildFunctionParameters( StringBuilder sb, Expression<T> value )
         {
             if ( value.Parameters == null || value.Parameters.Count == 0 )
-                return null;
-
-            var sb = new StringBuilder();
+                return;
 
             for ( int i = 0; i < value.Parameters.Count; i++ )
             {
                 if ( i > 0 )
                     sb.Append( ", " );
+
                 sb.Append( value.Parameters[i].Name );
             }
-
-            return sb.ToString();
         }
     }
-
-    //class JsExpressionVisitor : ExpressionVisitor
-    //{
-    //    private readonly StringBuilder _builder;
-
-    //    public JsExpressionVisitor()
-    //    {
-    //        _builder = new StringBuilder();
-    //    }
-
-    //    public string JavaScriptCode
-    //    {
-    //        get { return _builder.ToString(); }
-    //    }
-
-    //    public override Expression Visit( Expression node )
-    //    {
-    //        _builder.Clear();
-    //        return base.Visit( node );
-    //    }
-
-    //    protected override Expression VisitParameter( ParameterExpression node )
-    //    {
-    //        _builder.Append( node.Name );
-    //        base.VisitParameter( node );
-    //        return node;
-    //    }
-
-    //    protected override Expression VisitBinary( BinaryExpression node )
-    //    {
-    //        base.Visit( node.Left );
-    //        _builder.Append( GetOperator( node.NodeType ) );
-    //        base.Visit( node.Right );
-    //        return node;
-    //    }
-
-    //    protected override Expression VisitLambda<T>( Expression<T> node )
-    //    {
-    //        _builder.Append( "function(" );
-    //        for ( int i = 0; i < node.Parameters.Count; i++ )
-    //        {
-    //            if ( i > 0 )
-    //                _builder.Append( ", " );
-    //            _builder.Append( node.Parameters[i].Name );
-    //        }
-    //        _builder.Append( ") {" );
-    //        if ( node.Body.Type != typeof( void ) )
-    //        {
-    //            _builder.Append( "return " );
-    //        }
-    //        base.Visit( node.Body );
-    //        _builder.Append( "; }" );
-    //        return node;
-    //    }
-
-    //    private static string GetOperator( ExpressionType nodeType )
-    //    {
-    //        switch ( nodeType )
-    //        {
-    //            case ExpressionType.Add:
-    //                return " + ";
-    //            case ExpressionType.Multiply:
-    //                return " * ";
-    //            case ExpressionType.Subtract:
-    //                return " - ";
-    //            case ExpressionType.Divide:
-    //                return " / ";
-    //            case ExpressionType.Assign:
-    //                return " = ";
-    //            case ExpressionType.Equal:
-    //                return " == ";
-    //            case ExpressionType.NotEqual:
-    //                return " != ";
-
-    //                // TODO: Add other operators...
-    //        }
-    //        throw new NotImplementedException( "Operator not implemented" );
-    //    }
-    //}
 }
