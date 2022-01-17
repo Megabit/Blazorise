425282fd1f79e7f372111ba01bbc2f8860df044f
David-Moreira <david_nogueira_moreira@hotmail.com>
2022-01-17 01:07:41 +0000

    Initial new API definition

---
+++ Source/Extensions/Blazorise.DataGrid/Contexts/DetailRowTriggerContext.cs
@@ -0,0 +1,26 @@
+﻿namespace Blazorise.DataGrid
+{
+    public class DetailRowTriggerContext<TItem>
+    {
+        public DetailRowTriggerContext()
+        {
+
+        }
+
+        /// <summary>
+        /// Gets the model.
+        /// </summary>
+        public TItem Item { get; set; }
+
+        /// <summary>
+        /// If DetailRow is showing, and if the trigger is re-evaluated the DetailRow will be set to false.
+        /// Defaults to true.
+        /// </summary>
+        public bool Toggleable { get; set; } = true;
+
+        /// <summary>
+        /// Sets the DetailRowTrigger conditions to trigger its evaluation.
+        /// </summary>
+        public DetailRowTriggerType Type { get; set; }
+    }
+}
---
+++ Source/Extensions/Blazorise.DataGrid/Contexts/DetailRowTriggerType.cs
@@ -0,0 +1,15 @@
+﻿namespace Blazorise.DataGrid
+{
+    public enum DetailRowTriggerType
+    {
+        /// <summary>
+        /// Triggers on row click.
+        /// </summary>
+        TriggerOnRowClick,
+
+        /// <summary>
+        /// Triggers only when an item is actually selected.
+        /// </summary>
+        TriggerOnSelectedItem,
+    }
+}
