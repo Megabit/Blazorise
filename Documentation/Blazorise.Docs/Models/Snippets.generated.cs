001d17b90fad953b5084691e79691217d4147af4
David-Moreira <david_nogueira_moreira@hotmail.com>
2022-01-22 17:49:03 +0000

    docs - news

--- Documentation/Blazorise.Docs/Models/Snippets.generated.cs
+++ Documentation/Blazorise.Docs/Models/Snippets.generated.cs
@@ -3384,7 +3384,7 @@ namespace Blazorise.Docs.Models
         public const string DataGridDetailRowTemplateExample = @"<DataGrid TItem=""Employee""
           Data=""@employeeList""
           @bind-SelectedRow=""@selectedEmployee""
-          DetailRowTrigger=""@((context)=> {context.Single = true; context.Toggleable = false; return context.Item.Salaries?.Count > 0 && context.Item.Id == selectedEmployee?.Id; })""
+          DetailRowTrigger=""@((context) => context.Item.Salaries?.Count > 0 && context.Item.Id == selectedEmployee?.Id)""
           Responsive>
     <DataGridColumns>
         <DataGridCommandColumn TItem=""Employee"" />
