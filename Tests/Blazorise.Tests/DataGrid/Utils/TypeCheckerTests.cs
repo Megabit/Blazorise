1a12b5828e5d21b6c93a49701b677759c15510c4
David-Moreira <david_nogueira_moreira@hotmail.com>
2022-02-01 01:47:09 +0000

    IsCollection fix implementation. Look for ICollection / IEnumerable generic derivates and Array

--- Tests/Blazorise.Tests/DataGrid/Utils/TypeCheckerTests.cs
+++ Tests/Blazorise.Tests/DataGrid/Utils/TypeCheckerTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using Blazorise.DataGrid;
 using Blazorise.DataGrid.Utils;
@@ -14,13 +15,17 @@ namespace Blazorise.Tests.DataGrid.Utils
             Assert.True( typeof( IEnumerable<string> ).IsCollection() );
             Assert.True( typeof( List<string> ).IsCollection() );
             Assert.True( typeof( string[] ).IsCollection() );
+            Assert.True( typeof( int[] ).IsCollection() );
             Assert.True( typeof( IList ).IsCollection() );
-            Assert.True( typeof( IEnumerable ).IsCollection() );
+            Assert.True( typeof( ICollection ).IsCollection() );
+            Assert.True( typeof( ArrayList ).IsCollection() );
+            Assert.True( typeof( Array ).IsCollection() );
         }
 
         [Fact]
         public void IsCollection_WrongType_ReturnsFalse()
         {
+            Assert.False( typeof( TypeCheckerTests ).IsCollection() );
             Assert.False( typeof( string ).IsCollection() );
             Assert.False( typeof( object ).IsCollection() );
             Assert.False( typeof( int ).IsCollection() );
