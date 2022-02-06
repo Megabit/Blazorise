d6b5b12421a3fe72b6e66223681e60bc074b138b
David <david_nogueira_moreira@hotmail.com>
2022-02-06 14:06:47 +0000

    Is list or collection fix  (#3387)
    * Rename to IsCollection. Fix tests to straight up use the wanted types.
    
    * IsCollection fix implementation. Look for ICollection / IEnumerable generic derivates and Array

--- Tests/Blazorise.Tests/DataGrid/Utils/TypeCheckerTests.cs
+++ Tests/Blazorise.Tests/DataGrid/Utils/TypeCheckerTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using Blazorise.DataGrid;
 using Blazorise.DataGrid.Utils;
@@ -9,31 +10,26 @@ namespace Blazorise.Tests.DataGrid.Utils
     public class TypeCheckerTests
     {
         [Fact]
-        public void IsListOrCollection_ListOrCollectionDerivedType_ReturnsTrue()
+        public void IsCollection_ListOrCollectionDerivedType_ReturnsTrue()
         {
-            IEnumerable<string> IEnumerableObj = new string[] { "A", "B", "C" };
-            IList IListObj = new ArrayList();
-            List<string> ListObj = new();
-            string[] strArray = { "A", "B", "C" };
-
-            Assert.True( IEnumerableObj.GetType().IsListOrCollection() );
-            Assert.True( IListObj.GetType().IsListOrCollection() );
-            Assert.True( ListObj.GetType().IsListOrCollection() );
-            Assert.True( strArray.GetType().IsListOrCollection() );
+            Assert.True( typeof( IEnumerable<string> ).IsCollection() );
+            Assert.True( typeof( List<string> ).IsCollection() );
+            Assert.True( typeof( string[] ).IsCollection() );
+            Assert.True( typeof( int[] ).IsCollection() );
+            Assert.True( typeof( IList ).IsCollection() );
+            Assert.True( typeof( ICollection ).IsCollection() );
+            Assert.True( typeof( ArrayList ).IsCollection() );
+            Assert.True( typeof( Array ).IsCollection() );
         }
 
         [Fact]
-        public void IsListOrCollection_WrongType_ReturnsFalse()
+        public void IsCollection_WrongType_ReturnsFalse()
         {
-            string str = "Hello";
-            object obj = new { a = 1 };
-            int valueType = 1;
-            int? nullableValueType = 1;
-
-            Assert.False( str.GetType().IsListOrCollection() );
-            Assert.False( obj.GetType().IsListOrCollection() );
-            Assert.False( valueType.GetType().IsListOrCollection() );
-            Assert.False( nullableValueType.GetType().IsListOrCollection() );
+            Assert.False( typeof( TypeCheckerTests ).IsCollection() );
+            Assert.False( typeof( string ).IsCollection() );
+            Assert.False( typeof( object ).IsCollection() );
+            Assert.False( typeof( int ).IsCollection() );
+            Assert.False( typeof( int? ).IsCollection() );
         }
     }
 }
