e9301e4a16a9dfed79e77b8898cabbfb575d9f16
Niek Jannink <niek.jannink@baselinez.com>
2022-02-07 19:22:01 -0500

    #3410 QRCode generation enhancements

--- Source/Extensions/Blazorise.QRCode/QRCode.cs
+++ Source/Extensions/Blazorise.QRCode/QRCode.cs
@@ -1,115 +1,163 @@
-﻿using System;
+﻿#region Using directives
+using System;
 using System.Threading.Tasks;
+using Blazorise.Extensions;
 using Blazorise.Utilities;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Rendering;
 using QRCoder;
+#endregion
 
-namespace Blazorise.QRCode;
-
-public class QRCode : BaseComponent
+namespace Blazorise.QRCode
 {
-    private QRCodeGenerator generator;
 
-    protected override ValueTask DisposeAsync( bool disposing )
+    public class QRCode : BaseComponent
     {
-        generator?.Dispose();
-        return base.DisposeAsync( disposing );
+        #region Members
+
+        private QRCodeGenerator generator;
+
+        #endregion
+
+        #region Methods
+
+        protected override ValueTask DisposeAsync( bool disposing )
+        {
+            generator?.Dispose();
+            return base.DisposeAsync( disposing );
+        }
+
+        protected override void BuildRenderTree( RenderTreeBuilder builder )
+        {
+            if ( AsAnchor )
+            {
+                builder.OpenElement( "a" )
+                    .Attribute( "href", BuildUrl() )
+                    .Attribute( "target", "_blank" );
+            }
+
+            builder.OpenElement( "img" )
+                .Id( ElementId )
+                .Attribute( "src", GetAsBase64Image() )
+                .Attribute( "alt", Alt )
+                .Class( ClassNames )
+                .Style( StyleNames )
+                .Attributes( Attributes )
+                .CloseElement();
+
+            if ( AsAnchor )
+            {
+                builder.CloseElement(); // </a>
+            }
+        }
+
+        private string BuildUrl()
+        {
+            if ( Uri.IsWellFormedUriString( Text, UriKind.Absolute ) )
+            {
+                return Text;
+            }
+
+            var builder = new UriBuilder( Text );
+            if ( ( builder.Scheme == Uri.UriSchemeHttp && builder.Port == 80 ) ||
+                 ( builder.Scheme == Uri.UriSchemeHttps && builder.Port == 443 ) )
+            {
+                builder.Port = -1;
+            }
+
+            return builder.ToString();
+        }
+
+        /// <summary>
+        /// Generate QR Code image as base64 string
+        /// </summary>
+        public string GetAsBase64Image()
+        {
+            generator ??= new QRCodeGenerator();
+
+            var eccLevel = EccLevel switch
+            {
+                EccLevel.L => QRCodeGenerator.ECCLevel.L,
+                EccLevel.M => QRCodeGenerator.ECCLevel.M,
+                EccLevel.Q => QRCodeGenerator.ECCLevel.Q,
+                EccLevel.H => QRCodeGenerator.ECCLevel.H,
+                _ => throw new ArgumentOutOfRangeException()
+            };
+
+            var darkColor = GetColorBytes( DarkColor );
+            var lightColor = GetColorBytes( LightColor );
+
+            using var data = Payload == null
+                ? generator.CreateQrCode( Text, eccLevel )
+                : generator.CreateQrCode( Payload, eccLevel );
+
+            var code = new PngByteQRCode( data );
+            var bytes = code.GetGraphic( PixelsPerModule, darkColor, lightColor, DrawQuietZones );
+
+            const string pngData = "data:image/png;base64, ";
+            return pngData + Convert.ToBase64String( bytes );
+        }
+
+        private static byte[] GetColorBytes( string color )
+        {
+            byte[] bytes = null;
+            if ( HtmlColorCodeParser.TryParse( color, out var red, out var green, out var blue ) )
+            {
+                bytes = new[] { red, green, blue, byte.MaxValue };
+            }
+
+            return bytes;
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Text input or URL for QR code generation
+        /// </summary>
+        [Parameter] public string Text { get; set; }
+
+        /// <summary>
+        /// Payload used of QR code generation
+        /// </summary>
+        [Parameter] public PayloadGenerator.Payload Payload { get; set; }
+
+        /// <summary>
+        /// Image alt text
+        /// </summary>
+        [Parameter] public string Alt { get; set; }
+
+        /// <summary>
+        /// Error correction level
+        /// </summary>
+        [Parameter] public EccLevel EccLevel { get; set; } = EccLevel.L;
+
+        /// <summary>
+        /// Color used as dark color
+        /// </summary>
+        [Parameter] public string DarkColor { get; set; } = "black";
+
+        /// <summary>
+        /// Color used as light color
+        /// </summary>
+        [Parameter] public string LightColor { get; set; } = "white";
+
+        /// <summary>
+        /// Generate QR code image as anchor
+        /// </summary>
+        [Parameter] public bool AsAnchor { get; set; }
+
+        /// <summary>
+        /// Pixels per module
+        /// </summary>
+        [Parameter] public int PixelsPerModule { get; set; } = 5;
+
+        /// <summary>
+        /// Draw quiet zones (blank margins around QR Code image)
+        /// </summary>
+        [Parameter] public bool DrawQuietZones { get; set; } = true;
+
+        #endregion
     }
-
-    protected override void BuildRenderTree( RenderTreeBuilder builder )
-    {
-        int sequence = -1;
-
-        if ( AsAnchor )
-        {
-            builder.OpenElement( sequence++, "a" );
-            builder.AddAttribute( sequence++, "href", GetUrl() );
-            builder.AddAttribute( sequence++, "target", "_blank" );
-        }
-
-        builder.OpenElement( sequence++, "img" );
-        builder.AddAttribute( sequence++, "src", BuildQrCode() );
-
-        if ( !string.IsNullOrWhiteSpace( Alt ) )
-        {
-            builder.AddAttribute( sequence++, "alt", Alt );
-        }
-
-        builder.CloseElement();
-
-        if ( AsAnchor )
-        {
-            builder.CloseElement();
-        }
-    }
-
-    private string GetUrl()
-    {
-        if ( Uri.IsWellFormedUriString( Text, UriKind.Absolute ) )
-        {
-            return Text;
-        }
-
-        var builder = new UriBuilder( Text );
-        if ( ( builder.Scheme == Uri.UriSchemeHttp && builder.Port == 80 ) ||
-             ( builder.Scheme == Uri.UriSchemeHttps && builder.Port == 443 ) )
-        {
-            builder.Port = -1;
-        }
-
-        return builder.ToString();
-    }
-
-    private string BuildQrCode()
-    {
-        generator ??= new QRCodeGenerator();
-
-        var eccLevel = EccLevel switch
-        {
-            EccLevel.L => QRCodeGenerator.ECCLevel.L,
-            EccLevel.M => QRCodeGenerator.ECCLevel.M,
-            EccLevel.Q => QRCodeGenerator.ECCLevel.Q,
-            EccLevel.H => QRCodeGenerator.ECCLevel.H,
-            _ => throw new ArgumentOutOfRangeException()
-        };
-        var darkColor = GetColorBytes( DarkColor );
-        var lightColor = GetColorBytes( LightColor );
-
-        using var data = generator.CreateQrCode( Text, eccLevel );
-        var code = new PngByteQRCode( data );
-        var bytes = code.GetGraphic( 5, darkColor, lightColor );
-
-        const string pngData = "data:image/png;base64, ";
-        return pngData + Convert.ToBase64String( bytes );
-    }
-
-    private static byte[] GetColorBytes( string color )
-    {
-        byte[] bytes = null;
-        if ( HtmlColorCodeParser.TryParse( color, out var red, out var green, out var blue ) )
-        {
-            bytes = new[] { red, green, blue, byte.MaxValue };
-        }
-
-        return bytes;
-    }
-
-    [Parameter]
-    public string Text { get; set; }
-
-    [Parameter]
-    public string Alt { get; set; }
-
-    [Parameter]
-    public EccLevel EccLevel { get; set; } = EccLevel.L;
-
-    [Parameter]
-    public string DarkColor { get; set; } = "black";
-
-    [Parameter]
-    public string LightColor { get; set; } = "white";
-
-    [Parameter]
-    public bool AsAnchor { get; set; }
 }
