d8fa820bd3abad79899515a985fe60a27c6f74c5
Mladen Macanović <mladen.macanovic@gmail.com>
2022-02-09 14:17:35 +0100

    QRCode component (#3417)
    * Recreate QRCode
    
    * Rename Text to Value
    
    * Documentation page
    
    * Remove Anchor parameter
    
    * Mention PR in release notes

---
+++ Source/Extensions/Blazorise.QRCode/Enums/EccLevel.cs
@@ -0,0 +1,28 @@
+﻿namespace Blazorise.QRCode
+{
+    /// <summary>
+    /// Error Correction Level
+    /// </summary>
+    public enum EccLevel
+    {
+        /// <summary>
+        /// Level L (Low): 7% of data bytes can be restored.
+        /// </summary>
+        L,
+
+        /// <summary>
+        /// Level M (Medium): 15% of data bytes can be restored.
+        /// </summary>
+        M,
+
+        /// <summary>
+        /// Level Q (Quartile): 25% of data bytes can be restored.
+        /// </summary>
+        Q,
+
+        /// <summary>
+        /// Level H (High): 30% of data bytes can be restored.
+        /// </summary>
+        H
+    }
+}
