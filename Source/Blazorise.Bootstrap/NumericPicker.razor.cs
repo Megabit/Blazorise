29be122c41d1d5dea9914c270e12e5916798b06f
Mladen Macanovic <mladen.macanovic@gmail.com>
2022-02-13 15:51:51 +0100

    Fix Width for NumericPicker wrapper

--- Source/Blazorise.Bootstrap/NumericPicker.razor.cs
+++ Source/Blazorise.Bootstrap/NumericPicker.razor.cs
@@ -1,21 +1,51 @@
 ﻿#region Using directives
+using System.Linq;
+using System.Threading.Tasks;
 using Blazorise.Utilities;
+using Microsoft.AspNetCore.Components;
 #endregion
 
 namespace Blazorise.Bootstrap
 {
     public partial class NumericPicker<TValue> : Blazorise.NumericPicker<TValue>
     {
+        #region Members
+
+        private IFluentSizing numericWrapperWidth;
+
+        #endregion
+
         #region Constructors
 
         public NumericPicker()
         {
-            NumericWrapperClassBuilder = new( BuildDialogClasses );
+            NumericWrapperClassBuilder = new( BuildNumericWrapperClasses );
         }
 
         #endregion
 
         #region Methods
+
+        public override Task SetParametersAsync( ParameterView parameters )
+        {
+            // Width should be set on the wrapper element so we need to extract it from the parameters
+            if ( parameters.TryGetValue<IFluentSizing>( nameof( Width ), out var paramWidth ) )
+            {
+                numericWrapperWidth = paramWidth;
+
+                var readOnlyDictionary = parameters.ToDictionary()
+                    .Where( x => x.Key != nameof( Width ) )
+                    .ToDictionary( x => x.Key, x => x.Value );
+
+                return base.SetParametersAsync( ParameterView.FromDictionary( readOnlyDictionary ) );
+            }
+            else
+            {
+                numericWrapperWidth = null;
+            }
+
+            return base.SetParametersAsync( parameters );
+        }
 
         protected internal override void DirtyClasses()
         {
@@ -24,10 +54,15 @@ namespace Blazorise.Bootstrap
             base.DirtyClasses();
         }
 
-        private void BuildDialogClasses( ClassBuilder builder )
+        private void BuildNumericWrapperClasses( ClassBuilder builder )
         {
             builder.Append( "b-numeric" );
             builder.Append( ClassProvider.NumericPickerValidation( ParentValidation?.Status ?? ValidationStatus.None ), ParentValidation?.Status != ValidationStatus.None );
+
+            if ( numericWrapperWidth != null )
+            {
+                builder.Append( numericWrapperWidth.Class( ClassProvider ) );
+            }
         }
 
         #endregion
