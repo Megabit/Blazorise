f4640752ceb00084347a8b19480603b4dab96158
David <david_nogueira_moreira@hotmail.com>
2022-02-17 10:23:54 +0000

    Modal Render Mode (#3469)
    * ModalRenderMode : LazyLoad
    
    * Setup Example and RenderMode
    
    * Show message in alert
    
    * Fix animations
    
    * Release notes
    
    * Docs
    
    Co-authored-by: Mladen Macanović <mladen.macanovic@gmail.com>

---
+++ Source/Blazorise/Enums/ModalRenderMode.cs
@@ -0,0 +1,23 @@
+﻿namespace Blazorise
+{
+    /// <summary>
+    /// Defines how the modal content will be rendered.
+    /// </summary>
+    public enum ModalRenderMode
+    {
+        /// <summary>
+        /// Always renders the modal html content to the DOM.
+        /// </summary>
+        Default,
+
+        /// <summary>
+        /// Lazy loads modal, meaning the modal html content will only be rendered/loaded the first time it is visited.
+        /// </summary>
+        LazyLoad,
+
+        /// <summary>
+        /// Lazy loads modal everytime, meaning the modal html content will have it's html re-rendered to the DOM everytime.
+        /// </summary>
+        LazyReload,
+    }
+}
