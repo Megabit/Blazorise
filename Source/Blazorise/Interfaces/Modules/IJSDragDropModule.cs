d4670f60cb1d948340a4bbb047548bb20bd13111
Mladen Macanovic <mladen.macanovic@gmail.com>
2022-02-23 22:26:13 +0100

    Drag & Drop components

---
+++ Source/Blazorise/Interfaces/Modules/IJSDragDropModule.cs
@@ -0,0 +1,22 @@
+﻿#region Using directives
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+#endregion
+
+namespace Blazorise.Modules
+{
+    /// <summary>
+    /// Contracts for the tooltip JS module.
+    /// </summary>
+    public interface IJSDragDropModule : IBaseJSModule,
+        IJSDestroyableModule
+    {
+        /// <summary>
+        /// Initializes the new dragdrop zone within the JS module.
+        /// </summary>
+        /// <param name="elementRef">Reference to the rendered element.</param>
+        /// <param name="elementId">ID of the rendered element.</param>
+        /// <returns>A task that represents the asynchronous operation.</returns>
+        ValueTask Initialize( ElementReference elementRef, string elementId );
+    }
+}
---
+++ Source/Blazorise/Modules/JSDragDropModule.cs
@@ -0,0 +1,58 @@
+﻿#region Using directives
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+#endregion
+
+namespace Blazorise.Modules
+{
+    /// <summary>
+    /// Default implementation of the tooltip JS module.
+    /// </summary>
+    public class JSDragDropModule : BaseJSModule, IJSDragDropModule
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Default module constructor.
+        /// </summary>
+        /// <param name="jsRuntime">JavaScript runtime instance.</param>
+        /// <param name="versionProvider">Version provider.</param>
+        public JSDragDropModule( IJSRuntime jsRuntime, IVersionProvider versionProvider )
+            : base( jsRuntime, versionProvider )
+        {
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <inheritdoc/>
+        public virtual async ValueTask Initialize( ElementReference elementRef, string elementId )
+        {
+            var moduleInstance = await Module;
+
+            await moduleInstance.InvokeVoidAsync( "initialize", elementRef, elementId );
+        }
+
+        /// <inheritdoc/>
+        public virtual async ValueTask Destroy( ElementReference elementRef, string elementId )
+        {
+            if ( IsUnsafe )
+                return;
+
+            var moduleInstance = await Module;
+
+            await moduleInstance.InvokeVoidAsync( "destroy", elementRef, elementId );
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <inheritdoc/>
+        public override string ModuleFileName => $"./_content/Blazorise/dragDrop.js?v={VersionProvider.Version}";
+
+        #endregion
+    }
+}
