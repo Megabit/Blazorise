106b9209bc594a56b9cf60354a3fa860a3706fe5
Mladen Macanović <mladen.macanovic@gmail.com>
2022-03-02 11:58:41 +0100

    NumericPicker MinMaxLimitsOverride parameter (#3520)

---
+++ Source/Blazorise/Enums/NumericMinMaxLimitsOverride.cs
@@ -0,0 +1,28 @@
+﻿namespace Blazorise
+{
+    /// <summary>
+    /// Override the minimum and maximum limits.
+    /// </summary>
+    public enum NumericMinMaxLimitsOverride
+    {
+        /// <summary>
+        /// Ignores both minimumValue &amp; maximumValue.
+        /// </summary>
+        Ignore,
+
+        /// <summary>
+        /// The minimumValue and maximumValue limits are respected.
+        /// </summary>
+        DoNotOverride,
+
+        /// <summary>
+        /// Adheres to maximumValue and ignores minimumValue settings.
+        /// </summary>
+        Ceiling,
+
+        /// <summary>
+        /// Adheres to minimumValue and ignores maximumValue settings.
+        /// </summary>
+        Floor,
+    }
+}
--- Source/Blazorise/wwwroot/numericPicker.js
+++ Source/Blazorise/wwwroot/numericPicker.js
@@ -24,7 +24,7 @@ export function initialize(dotnetAdapter, element, elementId, options) {
         wheelStep: options.step || 1,
         minimumValue: fromExponential(options.min || options.typeMin) || AutoNumeric.options.minimumValue.tenTrillions,
         maximumValue: fromExponential(options.max || options.typeMax) || AutoNumeric.options.maximumValue.tenTrillions,
-        overrideMinMaxLimits: "ignore",
+        overrideMinMaxLimits: options.minMaxLimitsOverride || AutoNumeric.options.overrideMinMaxLimits.doNotOverride,
         roundingMethod: options.roundingMethod || AutoNumeric.options.roundingMethod.halfUpSymmetric,
 
         currencySymbol: options.currencySymbol || AutoNumeric.options.currencySymbol.none,
@@ -96,6 +96,10 @@ export function updateOptions(element, elementId, options) {
 
         if (options.max.changed) {
             instance.options.maximumValue(options.max.value || AutoNumeric.options.maximumValue.tenTrillions);
+        }
+
+        if (options.minMaxLimitsOverride.changed) {
+            instance.options.overrideMinMaxLimits(options.minMaxLimitsOverride.value || AutoNumeric.options.overrideMinMaxLimits.doNotOverride);
         }
 
         if (options.selectAllOnFocus.changed) {
