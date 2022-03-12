4eb606b478bfbac3b3257c9246d241d970254559
David <david_nogueira_moreira@hotmail.com>
2022-03-12 14:02:44 +0000

    BS5 MultiSelectCheck fix select (#3559)
    * Rename BS5 provider and check for the BS5 provider on MultiSelectColumn preventDefault
    
    * Introduce ClassProvider.DataGridRowMultiSelectPreventClick and delete IsBS5Provider
    
    * Remove DataGridRowMultiSelectPreventClick from class providers
    
    * Add new IBehaviourProvider
    
    * Add missing BootstrapBehaviourProvider in tests
    
    Co-authored-by: Mladen Macanovic <mladen.macanovic@gmail.com>

---
+++ Source/Blazorise.AntDesign/AntDesignBehaviourProvider.cs
@@ -0,0 +1,11 @@
+﻿namespace Blazorise.AntDesign
+{
+    public class AntDesignBehaviourProvider : BehaviourProvider
+    {
+        #region DataGrid
+
+        public override bool DataGridRowMultiSelectPreventClick => true;
+
+        #endregion
+    }
+}
---
+++ Source/Blazorise.Bootstrap5/Bootstrap5BehaviourProvider.cs
@@ -0,0 +1,7 @@
+﻿namespace Blazorise.Bootstrap5
+{
+    public class BootstrapBehaviourProvider : BehaviourProvider
+    {
+        public override bool DataGridRowMultiSelectPreventClick => false;
+    }
+}
