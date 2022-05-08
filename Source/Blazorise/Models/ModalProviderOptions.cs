8d6ed205d9a1a2288a50e9e559e719da77b14fe8
Mladen Macanovic <mladen.macanovic@gmail.com>
2022-05-08 14:19:25 +0200

    Nullable instance options

--- Source/Blazorise/Models/ModalProviderOptions.cs
+++ Source/Blazorise/Models/ModalProviderOptions.cs
@@ -15,12 +15,12 @@ namespace Blazorise
         /// Uses the modal standard structure, by setting this to true you are only in charge of providing the custom content.
         /// Defaults to true.
         /// </summary>
-        public bool UseModalStructure { get; set; } = true;
+        public bool? UseModalStructure { get; set; }
 
         /// <summary>
         /// If true modal will scroll to top when opened.
         /// </summary>
-        public bool ScrollToTop { get; set; } = true;
+        public bool? ScrollToTop { get; set; }
 
         /// <summary>
         /// Occurs before the modal is opened.
@@ -35,32 +35,32 @@ namespace Blazorise
         /// <summary>
         /// Occurs after the modal has opened.
         /// </summary>
-        public EventCallback Opened { get; set; }
+        public EventCallback? Opened { get; set; }
 
         /// <summary>
         /// Occurs after the modal has closed.
         /// </summary>
-        public EventCallback Closed { get; set; }
+        public EventCallback? Closed { get; set; }
 
         /// <summary>
         /// Specifies the backdrop needs to be rendered for this <see cref="Modal"/>.
         /// </summary>
-        public bool ShowBackdrop { get; set; } = true;
+        public bool? ShowBackdrop { get; set; }
 
         /// <summary>
         /// Gets or sets the animation duration.
         /// </summary>
-        public bool Animated { get; set; } = true;
+        public bool? Animated { get; set; }
 
         /// <summary>
         /// Gets or sets whether the component has any animations.
         /// </summary>
-        public int AnimationDuration { get; set; } = 150;
+        public int? AnimationDuration { get; set; }
 
         /// <summary>
         /// Defines how the modal content will be rendered.
         /// </summary>
-        public ModalRenderMode RenderMode { get; set; }
+        public ModalRenderMode? RenderMode { get; set; }
 
         /// <summary>
         /// Defines if the modal should keep the input focus at all times.
