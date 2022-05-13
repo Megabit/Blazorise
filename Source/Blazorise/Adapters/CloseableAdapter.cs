c1fbc4d0b1715b38a0a5a65dff534d2c1b88af40
Bobbar <bobbar@gmail.com>
2022-05-13 13:20:16 -0400

    ClosableAdapter: Remove the questionable Show/Hide calls. They can cause an infinite loop within the Modal component if a Hide is called before a Show call finishes.

--- Source/Blazorise/Adapters/CloseableAdapter.cs
+++ Source/Blazorise/Adapters/CloseableAdapter.cs
@@ -33,12 +33,6 @@ namespace Blazorise
 
                 await Task.Delay( animatedComponent.AnimationDuration );
 
-                // NOT SURE IF WE NEED THIS?
-                if ( visible )
-                    await animatedComponent.Show();
-                else
-                    await animatedComponent.Hide();
-
                 await animatedComponent.EndAnimation( visible );
             }
             else
