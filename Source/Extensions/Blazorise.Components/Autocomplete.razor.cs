e3e94aec3ecb406dc1d64534c2c5b19ccc8376ca
David-Moreira <david_nogueira_moreira@hotmail.com>
2022-05-31 01:07:17 +0100

    Handle ReadData with Multiple Selection

--- Source/Extensions/Blazorise.Components/Autocomplete.razor.cs
+++ Source/Extensions/Blazorise.Components/Autocomplete.razor.cs
@@ -456,31 +456,34 @@ namespace Blazorise.Components
             if ( TextField == null )
                 return;
 
-            if ( Multiple )
-                query = query.Where( x => !SelectedValues.Contains( ValueField.Invoke( x ) ) );
+            if ( !ManualReadMode )
+            {
+                if ( Multiple )
+                    query = query.Where( x => !SelectedValues.Contains( ValueField.Invoke( x ) ) );
 
-            var currentSearch = CurrentSearch ?? string.Empty;
+                var currentSearch = CurrentSearch ?? string.Empty;
 
-            if ( CustomFilter != null )
-            {
-                query = from q in query
-                        where q != null
-                        where CustomFilter( q, currentSearch )
-                        select q;
-            }
-            else if ( Filter == AutocompleteFilter.Contains )
-            {
-                query = from q in query
-                        let text = GetItemText( q )
-                        where text.IndexOf( currentSearch, 0, StringComparison.CurrentCultureIgnoreCase ) >= 0
-                        select q;
-            }
-            else
-            {
-                query = from q in query
-                        let text = GetItemText( q )
-                        where text.StartsWith( currentSearch, StringComparison.OrdinalIgnoreCase )
-                        select q;
+                if ( CustomFilter != null )
+                {
+                    query = from q in query
+                            where q != null
+                            where CustomFilter( q, currentSearch )
+                            select q;
+                }
+                else if ( Filter == AutocompleteFilter.Contains )
+                {
+                    query = from q in query
+                            let text = GetItemText( q )
+                            where text.IndexOf( currentSearch, 0, StringComparison.CurrentCultureIgnoreCase ) >= 0
+                            select q;
+                }
+                else
+                {
+                    query = from q in query
+                            let text = GetItemText( q )
+                            where text.StartsWith( currentSearch, StringComparison.OrdinalIgnoreCase )
+                            select q;
+                }
             }
 
             filteredData = query.ToList();
