8cf94a2e29a9d70013a9d95e6d31d870ff824501
Mladen Macanović <mladen.macanovic@gmail.com>
2022-06-14 16:37:31 +0200

    Blog section on main web and other improvements (#3885)
    * Docs: Add popular components section on homepage
    
    * Docs: configure build paths for snippet compiler
    
    * Blog: how to create Blazorise project
    
    * Blog: how to use validation
    
    * Compiler for csharp examples
    
    * Fix code example
    
    * Fix example
    
    * Validation form blog

--- Documentation/Blazorise.Docs.Compiler/CodeSnippets.cs
+++ Documentation/Blazorise.Docs.Compiler/CodeSnippets.cs
@@ -18,24 +18,26 @@ namespace Blazorise.Docs.Compiler
             {
                 var currentCode = string.Empty;
 
-                if ( File.Exists( paths.SnippetsFilePath ) )
+                if ( File.Exists( paths.SnippetsFilePath() ) )
                 {
-                    currentCode = File.ReadAllText( paths.SnippetsFilePath );
+                    currentCode = File.ReadAllText( paths.SnippetsFilePath() );
                 }
 
                 var cb = new CodeBuilder();
                 cb.AddHeader();
-                cb.AddLine( "namespace Blazorise.Docs.Models" );
+                cb.AddLine( $"namespace Blazorise.Docs.Models" );
                 cb.AddLine( "{" );
                 cb.IndentLevel++;
-                cb.AddLine( "public static partial class Snippets" );
+                cb.AddLine( $"public static partial class Snippets" );
                 cb.AddLine( "{" );
                 cb.IndentLevel++;
 
-                var razorFiles = Directory.EnumerateFiles( paths.DocsDirPath, "*.razor", SearchOption.AllDirectories );
-                var snippetFiles = Directory.EnumerateFiles( paths.DocsDirPath, "*.snippet", SearchOption.AllDirectories );
+                var dirPath = paths.DirPath();
+                var razorFiles = Directory.EnumerateFiles( dirPath, "*.razor", SearchOption.AllDirectories );
+                var snippetFiles = Directory.EnumerateFiles( dirPath, "*.snippet", SearchOption.AllDirectories );
+                var csharpFiles = Directory.EnumerateFiles( dirPath, "*.csharp", SearchOption.AllDirectories );
 
-                foreach ( var entry in razorFiles.Concat( snippetFiles ).OrderBy( e => e.Replace( "\\", "/" ), StringComparer.Ordinal ) )
+                foreach ( var entry in razorFiles.Concat( snippetFiles ).Concat( csharpFiles ).OrderBy( e => e.Replace( "\\", "/" ), StringComparer.Ordinal ) )
                 {
                     var filename = Path.GetFileName( entry );
                     var componentName = Path.GetFileNameWithoutExtension( filename );
@@ -51,7 +53,7 @@ namespace Blazorise.Docs.Compiler
 
                 if ( currentCode != cb.ToString() )
                 {
-                    File.WriteAllText( paths.SnippetsFilePath, cb.ToString() );
+                    File.WriteAllText( paths.SnippetsFilePath(), cb.ToString() );
                 }
             }
             catch ( Exception e )
