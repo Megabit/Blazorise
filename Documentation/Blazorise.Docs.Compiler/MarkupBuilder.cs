fea48643a796134c8395b8536cc3e554d2c1a7fc
Mladen Macanović <mladen.macanovic@gmail.com>
2022-07-01 11:33:47 +0200

    Generate blogs from markdown files (#3922)
    * WIP: convert markdown to razor
    
    * Rename ExampleMarkup
    
    * Code blocks
    
    * Add more inlines
    
    * Add more page info
    
    * Use code block info for naming of example files
    
    * Delete test markdown
    
    * Persist code block handling and fix inlines
    
    * Convert BeginnersGuideToCreateBlazoriseApp blog
    
    * Improvements
    
    * Make Paths as static class
    
    * Remov unused method
    
    * Use all compilers
    
    * Generate Example files along with Code files
    
    * Contribution guide
    
    * Skip generating Code from markdown examples
    
    * Run all builders
    
    * Optimize image note

---
+++ Documentation/Blazorise.Docs.Compiler/MarkupBuilder.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using ColorCode;
+
+namespace Blazorise.Docs.Compiler
+{
+    public class MarkupBuilder
+    {
+        private readonly HtmlClassFormatter formatter;
+
+        public MarkupBuilder( HtmlClassFormatter formatter )
+        {
+            this.formatter = formatter;
+        }
+
+        public string Build( string source, bool isCSharp )
+        {
+            var cb = new CodeBuilder();
+
+            var strippedSource = StripComponentSource( source );
+
+            if ( isCSharp )
+            {
+                cb.AddLine( "<div class=\"blazorise-codeblock\">" );
+
+                cb.AddLine(
+                    formatter.GetHtmlString( strippedSource, Languages.CSharp )
+                        .Replace( "@", "<span class=\"atSign\">&#64;</span>" )
+                        .ToLfLineEndings() );
+
+                cb.AddLine( "</div>" );
+            }
+            else
+            {
+                var blocks = strippedSource.Split( "@code" );
+
+                var blocks0 = Regex.Replace( blocks[0], @"</?DocsFrame>", string.Empty )
+                    .Replace( "@", "PlaceholdeR" )
+                    .Trim();
+
+                // Note: the @ creates problems and thus we replace it with an unlikely placeholder and in the markup replace back.
+                var html = formatter.GetHtmlString( blocks0, Languages.Html ).Replace( "PlaceholdeR", "@" );
+                html = AttributePostprocessing( html ).Replace( "@", "<span class=\"atSign\">&#64;</span>" );
+
+                cb.AddLine( "<div class=\"blazorise-codeblock\">" );
+                cb.AddLine( html.ToLfLineEndings() );
+
+                if ( blocks.Length == 2 )
+                {
+                    cb.AddLine(
+                        formatter.GetHtmlString( "@code" + blocks[1], Languages.CSharp )
+                            .Replace( "@", "<span class=\"atSign\">&#64;</span>" )
+                            .ToLfLineEndings() );
+                }
+
+                cb.AddLine( "</div>" );
+            }
+
+            return cb.ToString();
+        }
+
+        private static string StripComponentSource( string source )
+        {
+            source = Regex.Replace( source, "@(namespace|layout|page) .+?\n", string.Empty );
+            return source.Trim();
+        }
+
+        public static string AttributePostprocessing( string html )
+        {
+            return Regex.Replace(
+                html,
+                @"<span class=""htmlAttributeValue"">&quot;(?'value'.*?)&quot;</span>",
+                new MatchEvaluator( m =>
+                {
+                    var value = m.Groups["value"].Value;
+                    return
+                        $@"<span class=""quot"">&quot;</span>{AttributeValuePostprocessing( value )}<span class=""quot"">&quot;</span>";
+                } ) );
+        }
+
+        private static string AttributeValuePostprocessing( string value )
+        {
+            if ( string.IsNullOrWhiteSpace( value ) )
+                return value;
+            if ( value == "true" || value == "false" )
+                return $"<span class=\"keyword\">{value}</span>";
+            if ( Regex.IsMatch( value, "^[A-Z][A-Za-z0-9]+[.][A-Za-z][A-Za-z0-9]+$" ) )
+            {
+                var tokens = value.Split( '.' );
+                return $"<span class=\"enum\">{tokens[0]}</span><span class=\"enumValue\">.{tokens[1]}</span>";
+            }
+
+            if ( Regex.IsMatch( value, "^@[A-Za-z0-9]+$" ) )
+            {
+                return $"<span class=\"sharpVariable\">{value}</span>";
+            }
+
+            return $"<span class=\"htmlAttributeValue\">{value}</span>";
+        }
+    }
+}
