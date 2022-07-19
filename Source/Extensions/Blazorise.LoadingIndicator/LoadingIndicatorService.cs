d9f452b7eaba0a4489abbf8111efbe0c756700a5
Eugene Pistrak <evpistra@microsoft.com>
2022-07-19 07:37:08 -0700

    Avoid locking when using service with a single indicator (eg. global App busy indicator)

--- Source/Extensions/Blazorise.LoadingIndicator/LoadingIndicatorService.cs
+++ Source/Extensions/Blazorise.LoadingIndicator/LoadingIndicatorService.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 
 namespace Blazorise.LoadingIndicator
@@ -13,39 +15,36 @@ namespace Blazorise.LoadingIndicator
         private object hashLock = new();
         private HashSet<LoadingIndicator> indicators = new();
 
+        // avoid locking in single indicator (app busy) scenario
+        Action<bool> SetBusyAction;
+        Action<bool> SetLoadedAction;
+        Func<bool?> GetBusyFunc;
+        Func<bool?> GetLoadedFunc;
+
         #endregion
 
         #region Methods
+        /// <inheritdoc>
+        public LoadingIndicatorService()
+        {
+            // defalut to foreach implementation
+            SetBusyAction = SetBusyMulti;
+            SetLoadedAction = SetLoadedMulti;
+            GetBusyFunc = GetBusyMulti;
+            GetLoadedFunc = GetLoadedMulti;
+        }
 
         /// <summary>
         /// Set Busy state
         /// </summary>
         /// <param name="value">true or false</param>
-        public void SetBusy( bool value )
-        {
-            lock ( hashLock )
-            {
-                foreach ( var indicator in indicators )
-                {
-                    indicator.SetBusy( value );
-                }
-            }
-        }
+        public void SetBusy( bool value ) => SetBusyAction( value );
 
         /// <summary>
         /// Set Loaded state
         /// </summary>
         /// <param name="value">true or false</param>
-        public void SetLoaded( bool value )
-        {
-            lock ( hashLock )
-            {
-                foreach ( var indicator in indicators )
-                {
-                    indicator.SetLoaded( value );
-                }
-            }
-        }
+        public void SetLoaded( bool value ) => SetLoadedAction( value );
 
         /// <summary>
         /// Subscribe indicator to change events and save reference
@@ -55,6 +54,21 @@ namespace Blazorise.LoadingIndicator
         {
             lock ( hashLock )
             {
+                if ( indicators.Count == 0 )
+                {
+                    SetBusyAction = indicator.SetBusy;
+                    SetLoadedAction = indicator.SetLoaded;
+                    GetBusyFunc = () => indicator.Busy;
+                    GetLoadedFunc = () => indicator.Loaded;
+                } 
+                else if ( indicators.Count == 1 )
+                {
+                    SetBusyAction = SetBusyMulti;
+                    SetLoadedAction = SetLoadedMulti;
+                    GetBusyFunc = GetBusyMulti;
+                    GetLoadedFunc = GetLoadedMulti;
+                }
+
                 indicators.Add( indicator );
             }
         }
@@ -68,7 +82,89 @@ namespace Blazorise.LoadingIndicator
             lock ( hashLock )
             {
                 indicators.Remove( indicator );
+
+                if ( indicators.Count == 0 )
+                {
+                    SetBusyAction = SetLoadedAction = null;
+                    GetBusyFunc = GetLoadedFunc = null;
+                }
+                else if ( indicators.Count == 1 )
+                {
+                    indicator = indicators.First();
+                    SetBusyAction = indicator.SetBusy;
+                    SetLoadedAction = indicator.SetLoaded;
+                    GetBusyFunc = () => indicator.Busy;
+                    GetLoadedFunc = () => indicator.Loaded;
+                }
             }
+        }
+
+        private void SetBusyMulti( bool value )
+        {
+            lock ( hashLock )
+            {
+                foreach ( var indicator in indicators )
+                {
+                    indicator.SetBusy( value );
+                }
+            }
+        }
+
+        private void SetLoadedMulti( bool value )
+        {
+            lock ( hashLock )
+            {
+                foreach ( var indicator in indicators )
+                {
+                    indicator.SetLoaded( value );
+                }
+            }
+        }
+
+        private bool? GetBusyMulti()
+        {
+            bool? val = null;
+            lock ( hashLock )
+            {
+                foreach ( var indicator in indicators )
+                {
+                    if ( val == null )
+                    {
+                        val = indicator.Busy;
+                    }
+                    else
+                    {
+                        if ( val != indicator.Busy )
+                        {
+                            return null;
+                        }
+                    }
+                }
+            }
+            return val;
+        }
+
+        private bool? GetLoadedMulti()
+        {
+            bool? val = null;
+            lock ( hashLock )
+            {
+                foreach ( var indicator in indicators )
+                {
+                    if ( val == null )
+                    {
+                        val = indicator.Loaded;
+                    }
+                    else
+                    {
+                        if ( val != indicator.Loaded )
+                        {
+                            return null;
+                        }
+                    }
+                }
+            }
+            return val;
         }
 
         #endregion
@@ -78,60 +174,12 @@ namespace Blazorise.LoadingIndicator
         /// <summary>
         /// Returns Busy state shared by all indicator instances or null if the state is not the same for all indicators.
         /// </summary>
-        public bool? Busy
-        {
-            get
-            {
-                bool? val = null;
-                lock ( hashLock )
-                {
-                    foreach ( var indicator in indicators )
-                    {
-                        if ( val == null )
-                        {
-                            val = indicator.Busy;
-                        }
-                        else
-                        {
-                            if ( val != indicator.Busy )
-                            {
-                                return null;
-                            }
-                        }
-                    }
-                }
-                return val;
-            }
-        }
+        public bool? Busy => GetBusyFunc();
 
         /// <summary>
         /// Returns Loaded state shared by all indicator instances or null if the state is not the same for all indicators.
         /// </summary>
-        public bool? Loaded
-        {
-            get
-            {
-                bool? val = null;
-                lock ( hashLock )
-                {
-                    foreach ( var indicator in indicators )
-                    {
-                        if ( val == null )
-                        {
-                            val = indicator.Loaded;
-                        }
-                        else
-                        {
-                            if ( val != indicator.Loaded )
-                            {
-                                return null;
-                            }
-                        }
-                    }
-                }
-                return val;
-            }
-        }
+        public bool? Loaded => GetLoadedFunc();
 
         #endregion
     }
