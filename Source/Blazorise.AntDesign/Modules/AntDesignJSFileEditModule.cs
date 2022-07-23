fb4c660d2166afc77a8d07be352a32559fcffbf0
David <david_nogueira_moreira@hotmail.com>
2022-07-23 19:36:19 +0100

    FileEdit : Add AntDesignJSModalModule | Add Missing Open JS needed dependency (#4006)

---
+++ Source/Blazorise.AntDesign/Modules/AntDesignJSFileEditModule.cs
@@ -0,0 +1,15 @@
+﻿using Blazorise.Modules;
+using Microsoft.JSInterop;
+
+namespace Blazorise.AntDesign.Modules
+{
+    internal class AntDesignJSFileEditModule : JSFileEditModule
+    {
+        public AntDesignJSFileEditModule( IJSRuntime jsRuntime, IVersionProvider versionProvider )
+            : base( jsRuntime, versionProvider )
+        {
+        }
+
+        public override string ModuleFileName => $"./_content/Blazorise.AntDesign/fileEdit.js?v={VersionProvider.Version}";
+    }
+}
---
+++ Source/Blazorise.AntDesign/wwwroot/fileEdit.js
@@ -0,0 +1,13 @@
+import { getRequiredElement } from "../Blazorise/utilities.js?v=1.0.4.0";
+import { initialize, destroy, reset } from "../Blazorise/fileEdit.js?v=1.0.4.0";
+
+export function open(element, elementId) {
+    element = getRequiredElement(element, elementId);
+
+    if (!element)
+        return;
+
+    element.click();
+}
+
+export { initialize, destroy, reset };
