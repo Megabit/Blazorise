eaedeadbb585ec3aa93a22176c19006a4b15a8aa
Mladen Macanovic <mladen.macanovic@gmail.com>
2022-07-25 12:00:23 +0200

    Update filedit.js module version

--- Source/Blazorise.AntDesign/wwwroot/fileEdit.js
+++ Source/Blazorise.AntDesign/wwwroot/fileEdit.js
@@ -1,5 +1,5 @@
-import { getRequiredElement } from "../Blazorise/utilities.js?v=1.0.4.0";
-import { initialize, destroy, reset } from "../Blazorise/fileEdit.js?v=1.0.4.0";
+import { getRequiredElement } from "../Blazorise/utilities.js?v=1.0.6.0";
+import { initialize, destroy, reset } from "../Blazorise/fileEdit.js?v=1.0.6.0";
 
 export function open(element, elementId) {
     element = getRequiredElement(element, elementId);
