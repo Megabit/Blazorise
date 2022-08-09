565b44decc424f89bb1e526269473b67cb6fae66
David-Moreira <david_nogueira_moreira@hotmail.com>
2022-08-09 23:44:29 +0100

    Autocomplete : UnitTests : Should_AutoPreSelectFirstItem

--- Tests/Blazorise.Tests/Components/AutoCompleteComponentTest.cs
+++ Tests/Blazorise.Tests/Components/AutoCompleteComponentTest.cs
@@ -13,7 +13,7 @@ namespace Blazorise.Tests.Components
         public AutoCompleteComponentTest()
         {
             BlazoriseConfig.AddBootstrapProviders( Services );
-            BlazoriseConfig.JSInterop.AddTextEdit(this.JSInterop);
+            BlazoriseConfig.JSInterop.AddTextEdit( this.JSInterop );
             BlazoriseConfig.JSInterop.AddUtilities( this.JSInterop );
             BlazoriseConfig.JSInterop.AddClosable( this.JSInterop );
             BlazoriseConfig.JSInterop.AddDropdown( this.JSInterop );
@@ -30,7 +30,7 @@ namespace Blazorise.Tests.Components
             // test
             var input = comp.Find( ".b-is-autocomplete input" );
             var inputText = input.GetAttribute( "value" );
-            
+
             // validate
             // validate Dropdown initialize / textfield initialize
             this.JSInterop.VerifyInvoke( "initialize", 2 );
@@ -39,19 +39,36 @@ namespace Blazorise.Tests.Components
         }
 
         [Fact]
+        public void Should_AutoPreSelectFirstItem()
+        {
+            // setup
+            var comp = RenderComponent<AutocompleteComponent>( parameters =>
+                    parameters.Add( x => x.MinLength, 0 ) );
+
+            // test
+            var autoComplete = comp.Find( ".b-is-autocomplete input" );
+            autoComplete.Input( "" );
+            autoComplete.Focus();
+
+            var preSelected = comp.Find( ".b-is-autocomplete-suggestion.focus" );
+
+            Assert.NotNull( preSelected );
+
+        }
+        [Fact]
         public void MinLength0_ShouldShowOptions_OnFocus()
         {
             // setup
             var comp = RenderComponent<AutocompleteComponent>( parameters =>
                     parameters.Add( x => x.MinLength, 0 ) );
-            
+
             // test
             var autoComplete = comp.Find( ".b-is-autocomplete input" );
             autoComplete.Input( "" );
             autoComplete.Focus();
 
             var options = comp.FindAll( ".b-is-autocomplete-suggestion" );
-            
+
             Assert.NotEmpty( options );
         }
 
