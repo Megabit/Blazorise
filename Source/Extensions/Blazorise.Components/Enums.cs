0b9594a34b18ae0ecc686aa5019db1ffd20e0bdd
Mladen Macanovic <mladen.macanovic@gmail.com>
2022-09-19 11:56:13 +0200

    Add comments

--- Source/Extensions/Blazorise.Components/Enums.cs
+++ Source/Extensions/Blazorise.Components/Enums.cs
@@ -5,7 +5,14 @@
     /// </summary>
     public enum AutocompleteFilter
     {
+        /// <summary>
+        /// The filtered items will appear only if the search value is found at the beginning of the item value.
+        /// </summary>
         StartsWith,
+
+        /// <summary>
+        /// The filtered items will appear if the search value is found anywhere in the item value.
+        /// </summary>
         Contains,
     }
 
@@ -18,10 +25,12 @@
         /// Default mode. Selection is single.
         /// </summary>
         Default,
+
         /// <summary>
         /// Multiple mode. Selection is multiple.
         /// </summary>
         Multiple,
+
         /// <summary>
         /// Checkbox mode. Selection is multiple with checkbox selection support.
         /// </summary>
