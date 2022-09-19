c89b896432cd246c92613dc17f7332fa0879f34e
Mladen Macanovic <mladen.macanovic@gmail.com>
2022-09-19 15:40:33 +0200

    Dynamic markdown modules import

--- Source/Extensions/Blazorise.Markdown/wwwroot/markdown.js
+++ Source/Extensions/Blazorise.Markdown/wwwroot/markdown.js
@@ -1,3 +1,8 @@
+import "./vendors/easymde.js?v=1.1.0.0";
+import "./vendors/highlight.js?v=1.1.0.0";
+
+document.getElementsByTagName("head")[0].insertAdjacentHTML("beforeend", "<link rel=\"stylesheet\" href=\"_content/Blazorise.Markdown/vendors/easymde.css?v=1.1.0.0\" />");
+
 const _instances = [];
 
 export function initialize(dotNetObjectRef, element, elementId, options) {
