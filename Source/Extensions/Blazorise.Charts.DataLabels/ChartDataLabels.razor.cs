8832e0af1fbdcb9999e3f2719604b36595692ff3
Mladen Macanovic <mladen.macanovic@gmail.com>
2022-10-27 16:19:02 +0200

    Add empty Blazorise.Charts.DataLabels project

---
+++ Source/Extensions/Blazorise.Charts.DataLabels/ChartDataLabels.razor.cs
@@ -0,0 +1,16 @@
+﻿#region Using directives
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Blazorise.Extensions;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+#endregion
+
+namespace Blazorise.Charts.DataLabels
+{
+    public partial class ChartDataLabels<TItem> : BaseComponent
+    {
+    }
+}
