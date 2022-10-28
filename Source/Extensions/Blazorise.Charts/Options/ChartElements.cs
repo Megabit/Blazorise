232e2f1a90acad20a6d8d537e48842cadae0bfba
Mladen Macanovic <mladen.macanovic@gmail.com>
2022-10-28 15:13:44 +0200

    Add Layout and Elements to chart options

---
+++ Source/Extensions/Blazorise.Charts/Options/ChartElements.cs
@@ -0,0 +1,84 @@
+﻿#region Using directives
+using System.Text.Json.Serialization;
+#endregion
+
+namespace Blazorise.Charts
+{
+    /// <summary>
+    /// Point elements are used to represent the points in a line, radar or bubble chart.
+    /// </summary>
+    public class ChartElements
+    {
+        /// <summary>
+        /// Point radius.
+        /// </summary>
+        [JsonIgnore( Condition = JsonIgnoreCondition.WhenWritingNull )]
+        public double? Radius { get; set; }
+
+        /// <summary>
+        /// Point style.
+        /// </summary>
+        [JsonIgnore( Condition = JsonIgnoreCondition.WhenWritingNull )]
+        public string PointStyle { get; set; }
+
+        /// <summary>
+        /// Point rotation (in degrees).
+        /// </summary>
+        [JsonIgnore( Condition = JsonIgnoreCondition.WhenWritingNull )]
+        public double? Rotation { get; set; }
+
+        /// <summary>
+        /// Point fill color.
+        /// </summary>
+        [JsonIgnore( Condition = JsonIgnoreCondition.WhenWritingNull )]
+        public string BackgroundColor { get; set; }
+
+        /// <summary>
+        /// Point stroke width.
+        /// </summary>
+        [JsonIgnore( Condition = JsonIgnoreCondition.WhenWritingNull )]
+        public double? BorderWidth { get; set; }
+
+        /// <summary>
+        /// Point stroke color.
+        /// </summary>
+        [JsonIgnore( Condition = JsonIgnoreCondition.WhenWritingNull )]
+        public string BorderColor { get; set; }
+
+        /// <summary>
+        /// Extra radius added to point radius for hit detection.
+        /// </summary>
+        [JsonIgnore( Condition = JsonIgnoreCondition.WhenWritingNull )]
+        public double? HitRadius { get; set; }
+
+        /// <summary>
+        /// Point radius when hovered.
+        /// </summary>
+        [JsonIgnore( Condition = JsonIgnoreCondition.WhenWritingNull )]
+        public double? HoverRadius { get; set; }
+
+        /// <summary>
+        /// Stroke width when hovered.
+        /// </summary>
+        [JsonIgnore( Condition = JsonIgnoreCondition.WhenWritingNull )]
+        public double? HoverBorderWidth { get; set; }
+
+        /// <summary>
+        /// Line elements are used to represent the line in a line chart.
+        /// </summary>
+        [JsonIgnore( Condition = JsonIgnoreCondition.WhenWritingNull )]
+        public ChartLineElements Line { get; set; }
+
+        /// <summary>
+        /// Bar elements are used to represent the bars in a bar chart.
+        /// </summary>
+        [JsonIgnore( Condition = JsonIgnoreCondition.WhenWritingNull )]
+        public ChartBarElements Bar { get; set; }
+
+        /// <summary>
+        /// Arcs are used in the polar area, doughnut and pie charts.
+        /// </summary>
+        [JsonIgnore( Condition = JsonIgnoreCondition.WhenWritingNull )]
+        public ChartArcElements Arc { get; set; }
+    }
+}
