0b933f7e87e2959b7f09de115a121f0c8572f19a
Mladen Macanovic <mladen.macanovic@gmail.com>
2022-10-28 15:33:40 +0200

    Bar chart example

--- Demos/Blazorise.Demo/Pages/Tests/ChartsDataLabelsPage.razor.cs
+++ Demos/Blazorise.Demo/Pages/Tests/ChartsDataLabelsPage.razor.cs
@@ -10,6 +10,9 @@ namespace Blazorise.Demo.Pages.Tests
     public partial class ChartsDataLabelsPage
     {
         private LineChart<int> lineChart;
+        private BarChart<int> barChart;
+
+        #region Line
 
         LineChartOptions lineChartOptions = new()
         {
@@ -48,7 +51,7 @@ namespace Blazorise.Demo.Pages.Tests
             }
         };
 
-        List<ChartDataLabelsDataset> dataLabelsDatasets = new()
+        List<ChartDataLabelsDataset> lineDataLabelsDatasets = new()
         {
             new()
             {
@@ -80,7 +83,7 @@ namespace Blazorise.Demo.Pages.Tests
             },
         };
 
-        ChartDataLabelsOptions dataLabelsOptions = new()
+        ChartDataLabelsOptions lineDataLabelsOptions = new()
         {
             BorderRadius = 4,
             Color = "#ffffff",
@@ -92,15 +95,114 @@ namespace Blazorise.Demo.Pages.Tests
             Padding = new( 6 )
         };
 
+        #endregion
+
+        #region Bar
+
+        BarChartOptions barChartOptions = new()
+        {
+            AspectRatio = 5d / 3d,
+            Layout = new()
+            {
+                Padding = new()
+                {
+                    Top = 24,
+                    Right = 16,
+                    Bottom = 0,
+                    Left = 8
+                }
+            },
+            Elements = new()
+            {
+                Line = new()
+                {
+                    Fill = false,
+                },
+                Point = new()
+                {
+                    HoverRadius = 7,
+                    Radius = 5
+                }
+            },
+            Scales = new()
+            {
+                X = new()
+                {
+                    Stacked = true,
+                },
+                Y = new()
+                {
+                    Stacked = true,
+                }
+            },
+            Plugins = new()
+            {
+                Legend = new()
+                {
+                    Display = false
+                }
+            }
+        };
+
+        List<ChartDataLabelsDataset> barDataLabelsDatasets = new()
+        {
+            new()
+            {
+                DatasetIndex = 0,
+                Options = new()
+                {
+                    BackgroundColor = BackgroundColors[0],
+                    Align = "end",
+                    Anchor = "start"
+                }
+            },
+            new()
+            {
+                DatasetIndex = 1,
+                Options = new ()
+                {
+                    BackgroundColor = BackgroundColors[1],
+                }
+            },
+            new()
+            {
+                DatasetIndex = 2,
+                Options = new ()
+                {
+                    BackgroundColor = BackgroundColors[2],
+                    Align = "center",
+                    Anchor = "center"
+                }
+            },
+        };
+
+        ChartDataLabelsOptions barDataLabelsOptions = new()
+        {
+            BorderRadius = 4,
+            Color = "#ffffff",
+            Font = new()
+            {
+                Weight = "bold"
+            },
+            Formatter = ChartMathFormatter.Round,
+            Padding = new( 6 )
+        };
+
+        #endregion
+
         private static string[] Labels = new string[] { "1", "2", "3", "4", "5", "6" };
         private static string[] BackgroundColors = new string[] { "#4bc0c0", "#36a2eb", "#ff3d88" };
-        private static string[] borderColors = new string[] { "#4bc0c0", "#36a2eb", "#ff3d88" };
+        private static string[] BorderColors = new string[] { "#4bc0c0", "#36a2eb", "#ff3d88" };
         private Random random = new( DateTime.Now.Millisecond );
 
         protected override async Task OnAfterRenderAsync( bool firstRender )
         {
             if ( firstRender )
             {
+                await Task.WhenAll(
+                    HandleRedraw( lineChart, GetLineChartDataset ),
+                    HandleRedraw( barChart, GetBarChartDataset ) );
+
                 await lineChart.Clear();
 
                 await lineChart.AddLabelsDatasetsAndUpdate( Labels,
@@ -110,15 +212,40 @@ namespace Blazorise.Demo.Pages.Tests
             }
         }
 
+        private async Task HandleRedraw<TDataSet, TItem, TOptions, TModel>( Blazorise.Charts.BaseChart<TDataSet, TItem, TOptions, TModel> chart, Func<int, TDataSet> getDataSet )
+            where TDataSet : ChartDataset<TItem>
+            where TOptions : ChartOptions
+            where TModel : ChartModel
+        {
+            await chart.Clear();
+
+            await chart.AddLabelsDatasetsAndUpdate( Labels,
+                getDataSet( 0 ),
+                getDataSet( 1 ),
+                getDataSet( 2 ) );
+        }
+
         private LineChartDataset<int> GetLineChartDataset( int colorIndex )
         {
             return new()
             {
                 Label = "# of randoms",
                 Data = RandomizeData( 2, 9 ),
-                BackgroundColor = BackgroundColors[colorIndex], // line chart can only have one color
-                BorderColor = borderColors[colorIndex],
-                PointBorderColor = Enumerable.Repeat( borderColors.First(), 6 ).ToList(),
+                BackgroundColor = BackgroundColors[colorIndex],
+                BorderColor = BorderColors[colorIndex],
+                PointBorderColor = Enumerable.Repeat( BorderColors.First(), 6 ).ToList(),
+            };
+        }
+
+        private BarChartDataset<int> GetBarChartDataset( int colorIndex )
+        {
+            return new()
+            {
+                Label = "# of randoms",
+                Data = RandomizeData( 2, 9 ),
+                BackgroundColor = BackgroundColors[colorIndex],
+                BorderColor = BorderColors[colorIndex],
+                BorderWidth = 1
             };
         }
 
