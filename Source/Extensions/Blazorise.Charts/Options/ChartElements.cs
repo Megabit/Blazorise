d34030d26c408d162d1d770fcf4352c453bf91e3
Mladen Macanovic <mladen.macanovic@gmail.com>
2022-10-28 15:33:27 +0200

    Fix Point element structure

--- Source/Extensions/Blazorise.Charts/Options/ChartElements.cs
+++ Source/Extensions/Blazorise.Charts/Options/ChartElements.cs
@@ -10,58 +10,10 @@ namespace Blazorise.Charts
     public class ChartElements
     {
         /// <summary>
-        /// Point radius.
+        /// Point elements are used to represent the points in a line, radar or bubble chart.
         /// </summary>
         [JsonIgnore( Condition = JsonIgnoreCondition.WhenWritingNull )]
-        public double? Radius { get; set; }
-
-        /// <summary>
-        /// Point style.
-        /// </summary>
-        [JsonIgnore( Condition = JsonIgnoreCondition.WhenWritingNull )]
-        public string PointStyle { get; set; }
-
-        /// <summary>
-        /// Point rotation (in degrees).
-        /// </summary>
-        [JsonIgnore( Condition = JsonIgnoreCondition.WhenWritingNull )]
-        public double? Rotation { get; set; }
-
-        /// <summary>
-        /// Point fill color.
-        /// </summary>
-        [JsonIgnore( Condition = JsonIgnoreCondition.WhenWritingNull )]
-        public string BackgroundColor { get; set; }
-
-        /// <summary>
-        /// Point stroke width.
-        /// </summary>
-        [JsonIgnore( Condition = JsonIgnoreCondition.WhenWritingNull )]
-        public double? BorderWidth { get; set; }
-
-        /// <summary>
-        /// Point stroke color.
-        /// </summary>
-        [JsonIgnore( Condition = JsonIgnoreCondition.WhenWritingNull )]
-        public string BorderColor { get; set; }
-
-        /// <summary>
-        /// Extra radius added to point radius for hit detection.
-        /// </summary>
-        [JsonIgnore( Condition = JsonIgnoreCondition.WhenWritingNull )]
-        public double? HitRadius { get; set; }
-
-        /// <summary>
-        /// Point radius when hovered.
-        /// </summary>
-        [JsonIgnore( Condition = JsonIgnoreCondition.WhenWritingNull )]
-        public double? HoverRadius { get; set; }
-
-        /// <summary>
-        /// Stroke width when hovered.
-        /// </summary>
-        [JsonIgnore( Condition = JsonIgnoreCondition.WhenWritingNull )]
-        public double? HoverBorderWidth { get; set; }
+        public ChartPointElements Point { get; set; }
 
         /// <summary>
         /// Line elements are used to represent the line in a line chart.
