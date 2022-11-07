241f6e5ab8ac9784d316723845de3256373fb650
Mladen Macanovic <mladen.macanovic@gmail.com>
2022-11-07 13:40:16 +0100

    Remove unused classes

--- Source/Extensions/Blazorise.ImageCropper/ImageCropperState.cs
+++
@@ -1,8 +0,0 @@
-﻿//namespace Blazorise.ImageCropper;
-
-//internal record ImageCropperState
-//{
-//    public JSCropperOptions Options { get; set; }
-//    public bool Enabled { get; set; } = true;
-//    public string Source { get; set; }
-//}
--- Source/Extensions/Blazorise.ImageCropper/JSCropperOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Blazorise.ImageCropper;
-
-internal sealed record JSCropperOptions
-{
-    public double? AspectRatio { get; set; } = 1.0;
-    public int ViewMode { get; set; } = 1;
-    public string Preview { get; set; }
-    public int? Radius { get; set; }
-}
