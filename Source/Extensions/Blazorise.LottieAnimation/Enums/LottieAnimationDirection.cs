010cb3f47e7d17f820e2c9fcb6a30775f9aea7f6
Sam Fields <smfields896@gmail.com>
2022-11-14 12:38:37 -0500

    LottieAnimation Extension (#4265)
    * Initial commit of new extension boilerplate
    
    * Adding new HTML code files
    
    * Added basic animation
    
    * Adding config typing and using new callback type
    
    * Small updates
    
    * Adding config updating
    
    * Adding event handling
    
    * Added better support for events with backpressure
    
    * Only send current frame updates if someone is listening
    
    * Updated animation demo
    
    * Add back direction and speed settings
    
    * Removing frame segment properties
    
    * Fleshing out docs page
    
    * Documentation and code cleanup
    
    * Added loading indicator for demo
    
    * Fixing merge issue
    
    * Replacing spaces with tabs
    
    * Removing unused animation file
    
    * Code style and formatting updates
    
    * Formating
    
    * Rename members
    
    * Rename enums, events, and configs
    
    * Swapping async/await for Promise APIs
    
    * Clean demo code
    
    * Fix docs snippets
    
    * Mention lottie-web in credits
    
    * Mention smfields in the release 1.2 notes
    
    * Execute multiple tasks with Task.WhenAll
    
    * Add Best Practices note
    
    Co-authored-by: Mladen Macanovic <mladen.macanovic@gmail.com>
    Co-authored-by: Mladen Macanović <mladen.macanovic@blazorise.com>

---
+++ Source/Extensions/Blazorise.LottieAnimation/Enums/LottieAnimationDirection.cs
@@ -0,0 +1,17 @@
+namespace Blazorise.LottieAnimation;
+
+/// <summary>
+/// Direction of the lottie animation.
+/// </summary>
+public enum LottieAnimationDirection
+{
+    /// <summary>
+    /// Animation will play forwards.
+    /// </summary>
+    Forward = 1,
+
+    /// <summary>
+    /// Animation will play backwards.
+    /// </summary>
+    Backward = -1,
+}
