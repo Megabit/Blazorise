25168646ef801c7313b61fa9ef9032a3ca7d0005
stather <russell.stather@mac.com>
2022-11-23 10:23:14 +0200

    Added enum file

---
+++ Source/Extensions/Blazorise.DataGrid/Enums/RowOverlayPosition.cs
@@ -0,0 +1,18 @@
+﻿namespace Blazorise.DataGrid.Enums
+{
+    /// <summary>
+    /// Defines the position of the row overlay
+    /// </summary>
+    public enum RowOverlayPosition
+    {        
+        /// <summary>
+        /// Overlay start at left of row.
+        /// </summary>
+
+        Left,
+        /// <summary>
+        /// Overlay start at right of row.
+        /// </summary>
+        Right
+    }
+}
