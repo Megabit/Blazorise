9c6b2fa0763693433349d814d6fb7562074febf9
Matthew Collera <Matthew.Collera@microsoft.com>
2022-11-28 13:43:33 -0500

    Added support for drag and drop

--- Source/Blazorise/wwwroot/filePicker.js
+++ Source/Blazorise/wwwroot/filePicker.js
@@ -35,12 +35,19 @@ function onDragLeave(e) {
     e.preventDefault();
 }
 
-function onDrop(e, element) {
+async function onDrop(e, element) {
     e.preventDefault();
     console.log(element);
     let fileInput = getFileInput(element);
 
-    fileInput.files = e.dataTransfer.files;
+    let _files = await getFilesAsync(e.dataTransfer);
+    var dt = new DataTransfer();
+
+    for (var i = 0; i < _files.length; i++) {
+        dt.items.add(_files[i]);
+    }
+    fileInput.files = dt.files;
+
     const event = new Event('change', { bubbles: true });
     fileInput.dispatchEvent(event);
 }
@@ -61,4 +68,66 @@ function setFileInput(element) {
 
 function getFileInput(element) {
     return _instances[element.id].fileInput;
+}
+
+async function getFilesAsync(dataTransfer) {
+    const files = [];
+    for (let i = 0; i < dataTransfer.items.length; i++) {
+        const item = dataTransfer.items[i];
+        if (item.kind === "file") {
+            const entry = item.webkitGetAsEntry();
+            const entryContent = await readEntryContentAsync(entry);
+            files.push(...entryContent);
+            continue;
+
+            const file = item.getAsFile();
+            if (file) {
+                files.push(file);
+            }
+        }
+    }
+
+    return files;
+}
+
+// Returns a promise with all the files of the directory hierarchy
+function readEntryContentAsync(entry) {
+    return new Promise ((resolve, reject) => {
+        let reading = 0;
+        const contents = [];
+
+        readEntry(entry, "");
+
+        function readEntry(entry, path) {
+            if (entry.isFile) {
+                reading++;
+                entry.file(file => {
+                    file.webkitRelativePath = path + file.name;
+                    reading--;
+                    contents.push(file);
+
+                    if (reading === 0) {
+                        resolve(contents);
+                    }
+                });
+            } else if (entry.isDirectory) {
+                readReaderContent(entry.createReader(), path);
+            }
+        }
+
+        function readReaderContent(reader, path) {
+            reading++;
+
+            reader.readEntries(function (entries) {
+                reading--;
+                for (const entry of entries) {
+                    readEntry(entry, path + entry.name);
+                }
+
+                if (reading === 0) {
+                    resolve(contents);
+                }
+            });
+        }
+    });
 }
