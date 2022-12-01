2771e0a2a24013f53142e1b95add126e722cb974
Mladen Macanovic <mladen.macanovic@gmail.com>
2022-12-01 10:09:35 +0100

    Check for array types

--- Source/Blazorise/Extensions/ParameterViewExtensions.cs
+++ Source/Blazorise/Extensions/ParameterViewExtensions.cs
@@ -1,17 +1,34 @@
 ﻿#region Using directives
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Components;
 #endregion
 
 namespace Blazorise.Extensions;
-#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+
+/// <summary>
+/// Helper methods for component parameters.
+/// </summary>
 public static class ParameterViewExtensions
 {
-    public static void TryGetParameter<T>( this ParameterView parameters, string parameterName, T currentValue, out ComponentParameterInfo<T> componentParameter )
+    /// <summary>
+    /// Gets the value of the parameter with the specified name.
+    /// </summary>
+    /// <typeparam name="T">The type of the value.</typeparam>
+    /// <param name="parameters">Dictionary of all component paremeters.</param>
+    /// <param name="parameterName">The name of the parameter.</param>
+    /// <param name="currentValue">Last known parameter value.</param>
+    /// <param name="result">Receives the result, if any.</param>
+    public static void TryGetParameter<T>( this ParameterView parameters, string parameterName, T currentValue, out ComponentParameterInfo<T> result )
     {
         if ( parameters.TryGetValue<T>( parameterName, out var paramNewValue ) )
-            componentParameter = new ComponentParameterInfo<T>( paramNewValue, true, !paramNewValue.IsEqual( currentValue ) );
+        {
+            var changed = currentValue is IEnumerable<T> array && paramNewValue is IEnumerable<T> array2
+                ? !array.AreEqual( array2 )
+                : !paramNewValue.IsEqual( currentValue );
+
+            result = new ComponentParameterInfo<T>( paramNewValue, true, changed );
+        }
         else
-            componentParameter = new ComponentParameterInfo<T>( default );
+            result = new ComponentParameterInfo<T>( default );
     }
 }
-#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
