e34f32d54c6ac21dc51787afc75385cf5f5a8277
Mladen Macanovic <mladen.macanovic@gmail.com>
2022-12-02 20:52:11 +0100

    Empty Tailwind project

---
+++ Source/Blazorise.Tailwind/Config.cs
@@ -0,0 +1,50 @@
+﻿#region Using directives
+using System;
+using System.Collections.Generic;
+using Blazorise.Modules;
+using Microsoft.Extensions.DependencyInjection;
+#endregion
+
+namespace Blazorise.Tailwind;
+
+public static class Config
+{
+    /// <summary>
+    /// Adds a bootstrap providers and component mappings.
+    /// </summary>
+    /// <param name="serviceCollection"></param>
+    /// <returns></returns>
+    public static IServiceCollection AddTailwindProviders( this IServiceCollection serviceCollection, Action<IClassProvider> configureClassProvider = null )
+    {
+        var classProvider = new TailwindClassProvider();
+
+        configureClassProvider?.Invoke( classProvider );
+
+        serviceCollection.AddSingleton<IClassProvider>( classProvider );
+        serviceCollection.AddSingleton<IStyleProvider, TailwindStyleProvider>();
+        serviceCollection.AddSingleton<IBehaviourProvider, TailwindBehaviourProvider>();
+        serviceCollection.AddScoped<IThemeGenerator, TailwindThemeGenerator>();
+
+        serviceCollection.AddTailwindComponents();
+
+        serviceCollection.AddScoped<IJSModalModule, Modules.TailwindJSModalModule>();
+        serviceCollection.AddScoped<IJSTooltipModule, Modules.TailwindJSTooltipModule>();
+
+        return serviceCollection;
+    }
+
+    public static IServiceCollection AddTailwindComponents( this IServiceCollection serviceCollection )
+    {
+        foreach ( var mapping in ComponentMap )
+        {
+            serviceCollection.AddTransient( mapping.Key, mapping.Value );
+        }
+
+        return serviceCollection;
+    }
+
+    public static IDictionary<Type, Type> ComponentMap => new Dictionary<Type, Type>
+    {
+        
+    };
+}
---
+++ Source/Blazorise.Tailwind/wwwroot/tooltip.js
@@ -0,0 +1,11 @@
+import { initialize as baseInitialize, destroy, updateContent } from "../Blazorise/tooltip.js?v=1.1.4.1";
+
+export function initialize(element, elementId, options) {
+    baseInitialize(element, elementId, options);
+
+    if (options.autodetectInline && element && element.querySelector(".custom-control-input,.btn")) {
+        element.classList.add("b-tooltip-inline");
+    }
+}
+
+export { destroy, updateContent };
