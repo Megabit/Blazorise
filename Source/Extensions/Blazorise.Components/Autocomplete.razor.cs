407a2f6f74b54cbe149dac0af83214837498fa1d
David Nogueira Moreira <david_nogueira_moreira@hotmail.com>
2022-12-06 23:43:48 +0000

    On Default Mode only Close if MinLength > 0

--- Source/Extensions/Blazorise.Components/Autocomplete.razor.cs
+++ Source/Extensions/Blazorise.Components/Autocomplete.razor.cs
@@ -363,7 +363,7 @@ namespace Blazorise.Components
 
                 if ( ActiveItemIndex >= 0 )
                 {
-                    if ( FilteredData?.Count > 0 )
+                    if ( FilteredData?.Count > 0 && DropdownVisible )
                     {
                         var item = FilteredData[ActiveItemIndex];
                         if ( item != null && ValueField != null )
@@ -436,7 +436,7 @@ namespace Blazorise.Components
 
         private async Task OnDropdownItemSelected( object value )
         {
-            if ( SelectionMode == AutocompleteSelectionMode.Default )
+            if ( SelectionMode == AutocompleteSelectionMode.Default && MinLength > 0 )
             {
                 await Close();
             }
