bb8487612734bca40ed14fa2bed95522e0b1f0e8
Mladen Macanovic <mladen.macanovic@gmail.com>
2022-12-07 16:34:15 +0100

    Badge

--- Source/Blazorise.Tailwind/TailwindClassProvider.cs
+++ Source/Blazorise.Tailwind/TailwindClassProvider.cs
@@ -1026,7 +1026,7 @@ public class TailwindClassProvider : ClassProvider
 
     #region Badge
 
-    public override string Badge() => "text-xs inline-block py-1 px-2.5 leading-none text-center whitespace-nowrap align-baseline font-bold bg-blue-600 text-white rounded";
+    public override string Badge() => "text-xs font-semibold mr-2 px-2.5 py-0.5 rounded";
 
     public override string BadgeColor( Color color )
     {
@@ -1034,15 +1034,15 @@ public class TailwindClassProvider : ClassProvider
 
         return name switch
         {
-            "primary" => "bg-blue-600 text-white",
-            "secondary" => "bg-purple-600 text-white",
-            "success" => "bg-green-500 text-white",
-            "danger" => "bg-red-600 text-white",
-            "warning" => "bg-yellow-500 text-white",
-            "info" => "bg-indigo-400 text-white",
-            "light" => "bg-gray-200 text-white",
-            "dark" => "bg-gray-800 text-white",
-            "link" => "bg-indigo-400 text-white",
+            "primary" => "bg-blue-100 text-blue-800 dark:bg-blue-200 dark:text-blue-800",
+            "secondary" => "bg-gray-100 text-gray-800 dark:bg-gray-200 dark:text-gray-800",
+            "success" => "bg-green-100 text-green-800 dark:bg-green-200 dark:text-green-900",
+            "danger" => "bg-red-100 text-red-800 dark:bg-red-200 dark:text-red-900",
+            "warning" => "bg-yellow-100 text-yellow-800 dark:bg-yellow-200 dark:text-yellow-900",
+            "info" => "bg-purple-100 text-purple-800 dark:bg-purple-200 dark:text-purple-900",
+            "light" => "bg-gray-100 text-gray-800 dark:bg-gray-200 dark:text-gray-800",
+            "dark" => "bg-gray-800 text-gray-100 dark:bg-gray-300 dark:text-gray-700",
+            "link" => "bg-indigo-100 text-indigo-800 dark:bg-indigo-200 dark:text-indigo-900",
             _ => null,
         };
     }
