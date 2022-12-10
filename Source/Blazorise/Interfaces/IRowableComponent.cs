ce90a54309220db41131232c673027ab6b44de7f
Mladen Macanovic <mladen.macanovic@gmail.com>
2022-12-10 15:49:03 +0100

    Comment ResetUsedSpace

--- Source/Blazorise/Interfaces/IRowableComponent.cs
+++ Source/Blazorise/Interfaces/IRowableComponent.cs
@@ -24,6 +24,12 @@ public interface IRowableComponent
     /// Forces the row to start calculating used space from start.
     /// </summary>
     /// <param name="column">Column that is requested the calculation.</param>
+    /// <remarks>
+    /// The used space will reset only if the column is the first columnn in a row(has a zero index). This should work
+    /// most of the time. But, in some rare cases when columns are (re)created dynamicaly(for example if it's
+    /// placed behind an if statement) then the column would have a highest index once it is recreated. For those cases
+    /// it is advised to developers to use the <see cref="BaseComponent.Display"/> parameter to show or hide the column.
+    /// </remarks>
     void ResetUsedSpace( IColumnableComponent column );
 
     /// <summary>
