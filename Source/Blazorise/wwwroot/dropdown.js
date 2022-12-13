29801d6bd3c8e14e11ae1e89fef5e23fd01160f5
Mladen Macanovic <mladen.macanovic@gmail.com>
2022-12-13 13:37:15 +0100

    Version 1.1.5

--- Source/Blazorise/wwwroot/dropdown.js
+++ Source/Blazorise/wwwroot/dropdown.js
@@ -1,6 +1,6 @@
-import { getRequiredElement } from "./utilities.js?v=1.1.4.1";
-import { createPopper } from "./popper.js?v=1.1.4.1";
-import { createAttributesObserver, observeClassChanged, destroyObserver } from "./observer.js?v=1.1.4.1";
+import { getRequiredElement } from "./utilities.js?v=1.1.5.0";
+import { createPopper } from "./popper.js?v=1.1.5.0";
+import { createAttributesObserver, observeClassChanged, destroyObserver } from "./observer.js?v=1.1.5.0";
 
 const _instances = [];
 const DIRECTION_DEFAULT = 'Default'
