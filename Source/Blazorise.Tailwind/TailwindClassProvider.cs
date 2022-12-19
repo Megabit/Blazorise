a8c3f5c2afad238d286e4b053b21698eb446c37b
Mladen Macanovic <mladen.macanovic@gmail.com>
2022-12-19 11:45:49 +0100

    Validation colors

--- Source/Blazorise.Tailwind/TailwindClassProvider.cs
+++ Source/Blazorise.Tailwind/TailwindClassProvider.cs
@@ -1832,6 +1832,16 @@ public class TailwindClassProvider : ClassProvider
         };
     }
 
+    public override string ToValidationStatus( ValidationStatus validationStatus )
+    {
+        return validationStatus switch
+        {
+            Blazorise.ValidationStatus.Success => "bg-green-50 border border-green-500 text-green-900 dark:text-green-400 placeholder-green-700 dark:placeholder-green-500 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-green-500",
+            Blazorise.ValidationStatus.Error => "bg-red-50 border border-red-500 text-red-900 placeholder-red-700 text-sm rounded-lg focus:ring-red-500 dark:bg-gray-700 focus:border-red-500 block w-full p-2.5 dark:text-red-500 dark:placeholder-red-500 dark:border-red-500",
+            _ => null,
+        };
+    }
+
     #endregion
 
     public override bool UseCustomInputStyles { get; set; } = true;
