cc2dc8adc7afadeec40b1d2a2d0e802c24c814a7
David Nogueira Moreira <david_nogueira_moreira@hotmail.com>
2023-01-02 22:50:14 +0000

    Fix Dropdown Nesting

---
+++ Source/Blazorise.Tailwind/Dropdown.razor.cs
@@ -0,0 +1,25 @@
+﻿#region Using directives
+
+#endregion
+
+namespace Blazorise.Tailwind;
+
+public partial class Dropdown : Blazorise.Dropdown
+{
+    #region Members
+
+    #endregion
+
+    #region Methods
+
+    //inheritdoc
+    protected override string GetShowElementId()
+        => ElementId;
+
+    #endregion
+
+    #region Properties
+
+
+    #endregion
+}
