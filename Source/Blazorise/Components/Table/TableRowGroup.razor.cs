183dc37e0d10633b0d1d5f95796e4632de8b6d1e
Mladen Macanovic <mladen.macanovic@gmail.com>
2023-01-07 20:16:37 +0100

    Use ColumnSpan directly, and use MouseEventArgs type for clicks

--- Source/Blazorise/Components/Table/TableRowGroup.razor.cs
+++ Source/Blazorise/Components/Table/TableRowGroup.razor.cs
@@ -75,17 +75,11 @@ public partial class TableRowGroup : BaseDraggableComponent
                 await ExpandedChanged.InvokeAsync( Expanded );
             }
 
-            await Clicked.InvokeAsync( EventArgsMapper.ToMouseEventArgs( eventArgs ) );
+            await Clicked.InvokeAsync( eventArgs );
         }
         else if ( eventArgs.Detail == 2 )
-            await DoubleClicked.InvokeAsync( EventArgsMapper.ToMouseEventArgs( eventArgs ) );
+            await DoubleClicked.InvokeAsync( eventArgs );
     }
-
-    /// <summary>
-    /// Determines the column span for the table cell.
-    /// </summary>
-    /// <returns>Column span value.</returns>
-    protected int GetColumnSpan() => ColumnSpan ?? 1000;
 
     #endregion
 
@@ -120,30 +114,30 @@ public partial class TableRowGroup : BaseDraggableComponent
     }
 
     /// <summary>
+    /// Defines if the group is expanded.
+    /// </summary>
+    [Parameter] public EventCallback<bool> ExpandedChanged { get; set; }
+
+    /// <summary>
     /// Defines if the group <see cref="Expanded"/> property can be toggled by clicking. It is still possible to toggle it programatically.
     /// </summary>
     [Parameter]
     public bool Toggleable { get; set; } = true;
 
     /// <summary>
-    /// Defines if the group is expanded.
+    /// Determines the column span for the inner table cell.
     /// </summary>
-    [Parameter] public EventCallback<bool> ExpandedChanged { get; set; }
-
-    /// <summary>
-    /// Number of columns a cell should span.
-    /// </summary>
-    [Parameter] public int? ColumnSpan { get; set; }
+    [Parameter] public int ColumnSpan { get; set; } = 1000;
 
     /// <summary>
     /// Occurs when the row is clicked.
     /// </summary>
-    [Parameter] public EventCallback<BLMouseEventArgs> Clicked { get; set; }
+    [Parameter] public EventCallback<MouseEventArgs> Clicked { get; set; }
 
     /// <summary>
     /// Occurs when the row is double clicked.
     /// </summary>
-    [Parameter] public EventCallback<BLMouseEventArgs> DoubleClicked { get; set; }
+    [Parameter] public EventCallback<MouseEventArgs> DoubleClicked { get; set; }
 
     /// <summary>
     /// Specifies the title to be rendered inside this <see cref="TableRowGroup"/>.
