8f7c9ff6e5a12974638502faf6c40a22f3c9dd6e
David Nogueira Moreira <david_nogueira_moreira@hotmail.com>
2023-01-11 00:23:23 +0000

    Setup group expanded state

--- Source/Extensions/Blazorise.DataGrid/Contexts/GroupContext.cs
+++ Source/Extensions/Blazorise.DataGrid/Contexts/GroupContext.cs
@@ -67,7 +67,7 @@ public class GroupContext<TItem>
     /// <summary>
     /// Gets whether the group is expanded.
     /// </summary>
-    public bool Expanded { get; private set; }
+    public bool Expanded { get; private set; } = true;
 
     /// <summary>
     /// Gets the Template for this group.
