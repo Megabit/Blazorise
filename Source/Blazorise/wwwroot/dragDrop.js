f1100452a9ca387e31ffb93222a137443e3ef3eb
David Moreira <david_nogueira_moreira@hotmail.com>
2023-01-15 12:04:22 +0000

    Table Throttled Drag DragOver Events (#4466)
    * ThrottledDragDropEvents
    
    * move preventDefault out of throttle
    
    * Rename to InitializeThrottledDragEvents && fix tests by adding mock
    
    * Fix initializeThrottledDragEvents js function name && make it init only if Draggable && events defined
    
    * ThrottledDragDropEvents
    
    * move preventDefault out of throttle
    
    * Rename to InitializeThrottledDragEvents && fix tests by adding mock
    
    * Fix initializeThrottledDragEvents js function name && make it init only if Draggable && events defined
    
    * Implement Disposable process
    
    * Revert Dashboard back to original
    
    * Format braces
    
    Co-authored-by: Mladen Macanovic <mladen.macanovic@gmail.com>

--- Source/Blazorise/wwwroot/dragDrop.js
+++ Source/Blazorise/wwwroot/dragDrop.js
@@ -1,4 +1,6 @@
 ﻿import { getRequiredElement } from "./utilities.js?v=1.1.5.0";
+let throttledDragHandler;
+let throttledDragOverHandler;
 
 export function initialize(element, elementId) {
     element = getRequiredElement(element, elementId);
@@ -18,6 +20,53 @@ export function destroy(element, elementId) {
 
     element.removeEventListener('dragover', dragOverHandler);
     element.removeEventListener('dragstart', dragStartHandler);
+}
+
+export function initializeThrottledDragEvents(element, elementId, dotnetAdapter) {
+    element = getRequiredElement(element, elementId);
+    if (!element)
+        return;
+
+    let timeOutForDrag = null;
+    let timeOutForDragOver = null;
+
+    throttledDragHandler = function (e) {
+        e.preventDefault();
+        if (!timeOutForDrag) {
+            timeOutForDrag = setTimeout(function () {
+                timeOutForDrag = null;
+                dotnetAdapter.invokeMethodAsync("OnDragHandler", e)
+            }.bind(this), 250);
+        }
+    }
+
+    throttledDragOverHandler = function (e) {
+        e.preventDefault();
+        if (!timeOutForDragOver) {
+            timeOutForDragOver = setTimeout(function () {
+                timeOutForDragOver = null;
+                dotnetAdapter.invokeMethodAsync("OnDragOverHandler", e)
+            }.bind(this), 250);
+        }
+    }
+
+    element.addEventListener('drag', throttledDragHandler);
+    element.addEventListener('dragover', throttledDragOverHandler);
+}
+
+export function destroyThrottledDragEvents(element, elementId) {
+    element = getRequiredElement(element, elementId);
+
+    if (!element)
+        return;
+
+    if (typeof throttledDragHandler === "function") {
+        element.removeEventListener("drag", throttledDragHandler);
+    }
+
+    if (typeof throttledDragOverHandler === "function") {
+        element.removeEventListener("dragover", throttledDragOverHandler);
+    }
 }
 
 function dragOverHandler(e) {
