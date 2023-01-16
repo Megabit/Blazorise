5bd1833fa6ed58d3535844b0b46438c606a5491a
Mladen Macanovic <mladen.macanovic@gmail.com>
2023-01-16 14:10:11 +0100

    Remove license key

--- Demos/Blazorise.Demo/Config.cs
+++ Demos/Blazorise.Demo/Config.cs
@@ -15,7 +15,7 @@ public static class Config
         services
             .AddBlazorise( options =>
             {
-                options.LicenseKey = "CjxRAnB+Nwo9UAJ3fT01BlEAc300CD5RAHF+NQg8bjoPL2hdNiFZLSIlf20XVTJDD3tjDkAgIHwCQQpTMyF2V2oXPUsLLmJcM3QTFGJydil4EGVVaT0pWnE2Ul14BV0IAGJJVBl+IndWbFglCBE9NEp2LVMIPnBAQxFkKChpWXYCezAPbFpJCFt2JC5yaClRcTZTcD4SWg4GR1A/MUYMBWtxfhdaFzlAAW8MYTkiYnBeAUEXKSpOXxsENA1MaXUHX25hPG5pWmoTBkd/PSlHcA83a0hTeAQ8fA1qVwgXOX1cTQdlaiw3TlYEZzQGfFwjSEYINn0JVVRncQBNQVZXXxAIPFBqK2cmH3R2fCRzGRo9dHYAdgIfXXM5BUIUGk1oOCkfcwBkSloAfXh/cn5DBGMxdlVTfxRybgQzbVUXegd9XQ97VAkxFGMXfVdhEDtuUl4AeScjQGlYUEIzP0FwWAFzdgJQczkPfykkX2FFB3gZfH9pNDB2C3Z/fyNWdA5/RAFkG3YMNm1oeSRCIz4ufWAFSXEdY0h9CUd8cw==";
+                options.LicenseKey = "";
                 options.Immediate = true;
             } )
             .AddBlazoriseRichTextEdit( options =>
