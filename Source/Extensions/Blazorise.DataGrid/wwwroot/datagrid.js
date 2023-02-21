c84532b33b116238938bce18952e8c9e60def998
Mladen Macanovic <mladen.macanovic@gmail.com>
2023-02-21 11:25:22 +0100

    Raise version 1.2.1

--- Source/Extensions/Blazorise.DataGrid/wwwroot/datagrid.js
+++ Source/Extensions/Blazorise.DataGrid/wwwroot/datagrid.js
@@ -1,4 +1,4 @@
-﻿import { getRequiredElement } from "../Blazorise/utilities.js?v=1.2.0.0";
+﻿import { getRequiredElement } from "../Blazorise/utilities.js?v=1.2.1.0";
 
 export function initialize(element, elementId) {
     element = getRequiredElement(element, elementId);
