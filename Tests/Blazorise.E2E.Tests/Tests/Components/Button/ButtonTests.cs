6bcaf484e8d3dcb30819aba372a3deb4e68a93a8
David Moreira <david_nogueira_moreira@hotmail.com>
2023-03-16 21:18:46 +0000

    Add a SelectTestComponent Helper

--- Tests/Blazorise.E2E.Tests/Tests/Components/Button/ButtonTests.cs
+++ Tests/Blazorise.E2E.Tests/Tests/Components/Button/ButtonTests.cs
@@ -1,18 +1,17 @@
-﻿using Blazorise.E2E.Tests.Infrastructure;
+﻿using BasicTestApp.Client;
+using Blazorise.E2E.Tests.Infrastructure;
 
 namespace Blazorise.E2E.Tests.Tests.Components.Button
 {
 
     [Parallelizable( ParallelScope.Self )]
     [TestFixture]
-    public class ButtonTests : BlazorPageTest
+    public class ButtonTests : BlazorisePageTest
     {
         [Test]
         public async Task CanRaiseCallback()
         {
-            await Page.GotoAsync( RootUri.AbsoluteUri );
-
-            await Page.GetByRole( AriaRole.Combobox ).SelectOptionAsync( new[] { "BasicTestApp.Client.ButtonComponent" } );
+            await SelectTestComponent<ButtonComponent>();
 
             var buttonResult = Page.Locator( "#basic-button-event-result" );
             await Page.GetByRole( AriaRole.Button, new() { Name = "Count" } ).ClickAsync();
