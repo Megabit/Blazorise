3e6f161eac71bae1e1540c1b0f7d2bcfe3ef3bc6
David Moreira <david_nogueira_moreira@hotmail.com>
2023-03-16 19:27:38 +0000

    Revert "Necessary project changes to setup WebApplicationFactory with WASM | Fix build.cmd"
    This reverts commit b8e010eff09b9606b4ab50c61ed892c7b9d4457e.

--- build.cmd
+++ build.cmd
@@ -1,1 +1,2 @@
-dotnet build Blazorise.sln
+cd /d "%~dp0"
+"%ProgramFiles(x86)%\\Microsoft Visual Studio\2019\Preview\MSBuild\Current\Bin\MSBuild.exe" Blazorise.sln /p:Configuration=Release  /t:Restore;Rebuild /v:m
