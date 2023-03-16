fe931a73b4d466b40bd24c79b4269509e941283e
David Moreira <david_nogueira_moreira@hotmail.com>
2023-03-16 21:07:20 +0000

    AsyncValidateTextEditTests

--- Tests/Blazorise.E2E.Tests/Tests/Components/TextEdit/AsyncValidateTextEditTests.cs
+++ Tests/Blazorise.E2E.Tests/Tests/Components/TextEdit/AsyncValidateTextEditTests.cs
@@ -11,21 +11,91 @@ namespace Blazorise.E2E.Tests.Tests.Components.TextEdit
         public async Task CanValidateText_InitiallyBlank()
         {
             await Page.GotoAsync( RootUri.AbsoluteUri );
-            await Page.PauseAsync();
             await Page.GetByRole( AriaRole.Combobox ).SelectOptionAsync( new[] { "BasicTestApp.Client.AsyncValidateTextEditComponent" } );
 
             var sut = Page.Locator( "#validate-text-initially-blank" );
             var textBox = sut.GetByRole( AriaRole.Textbox );
             var validationFeedback = sut.GetByText( "error" );
 
-            await textBox.ClickAsync();
-            await Expect( validationFeedback ).ToHaveClassAsync( new Regex( "invalid-feedback" ) );
+            await ExpectTo_StartInvalid_ValidUponFill_InvalidUponClear( textBox, validationFeedback );
+        }
 
-            await textBox.FillAsync( "a" );
+
+
+        [Test]
+        public async Task CanValidateText_InitiallyPopulated()
+        {
+            await Page.GotoAsync( RootUri.AbsoluteUri );
+            await Page.GetByRole( AriaRole.Combobox ).SelectOptionAsync( new[] { "BasicTestApp.Client.AsyncValidateTextEditComponent" } );
+
+            var sut = Page.Locator( "#validate-text-initially-populated" );
+            var textBox = sut.GetByRole( AriaRole.Textbox );
+            var validationFeedback = sut.GetByText( "error" );
+
+            await ExpectTo_StartValid_InvalidUponClear_ValidUponFill( textBox, validationFeedback );
+        }
+
+
+
+        [Test]
+        public async Task CanValidateTextWithBind_InitiallyBlank()
+        {
+            await Page.GotoAsync( RootUri.AbsoluteUri );
+            await Page.GetByRole( AriaRole.Combobox ).SelectOptionAsync( new[] { "BasicTestApp.Client.AsyncValidateTextEditComponent" } );
+
+            var sut = Page.Locator( "#validate-text-with-bind-initially-blank" );
+            var textBox = sut.GetByRole( AriaRole.Textbox );
+            var validationFeedback = sut.GetByText( "error" );
+
+            await ExpectTo_StartInvalid_ValidUponFill_InvalidUponClear( textBox, validationFeedback );
+        }
+
+        [Test]
+        public async Task CanValidateTextWithEvent_InitiallyBlank()
+        {
+            await Page.GotoAsync( RootUri.AbsoluteUri );
+            await Page.GetByRole( AriaRole.Combobox ).SelectOptionAsync( new[] { "BasicTestApp.Client.AsyncValidateTextEditComponent" } );
+
+            var sut = Page.Locator( "#validate-text-with-event-initially-blank" );
+            var textBox = sut.GetByRole( AriaRole.Textbox );
+            var validationFeedback = sut.GetByText( "error" );
+
+            await ExpectTo_StartInvalid_ValidUponFill_InvalidUponClear( textBox, validationFeedback );
+        }
+
+        [Test]
+        public async Task CanValidateTextWithEvent_InitiallyPopulated()
+        {
+            await Page.GotoAsync( RootUri.AbsoluteUri );
+            await Page.GetByRole( AriaRole.Combobox ).SelectOptionAsync( new[] { "BasicTestApp.Client.AsyncValidateTextEditComponent" } );
+
+            var sut = Page.Locator( "#validate-text-with-event-initially-populated" );
+            var textBox = sut.GetByRole( AriaRole.Textbox );
+            var validationFeedback = sut.GetByText( "error" );
+
+            await ExpectTo_StartValid_InvalidUponClear_ValidUponFill( textBox, validationFeedback );
+        }
+
+        private async Task ExpectTo_StartInvalid_ValidUponFill_InvalidUponClear( ILocator textBox, ILocator validationFeedback )
+        {
+            await Expect( validationFeedback ).ToHaveClassAsync( "invalid-feedback" );
+
+            await textBox.FillAsync( "some text" );
             await Expect( validationFeedback ).ToBeHiddenAsync();
 
             await textBox.FillAsync( "" );
             await Expect( validationFeedback ).ToHaveClassAsync( "invalid-feedback" );
+        }
+
+        private async Task ExpectTo_StartValid_InvalidUponClear_ValidUponFill( ILocator textBox, ILocator validationFeedback )
+        {
+            await Expect( validationFeedback ).ToBeHiddenAsync();
+
+            await textBox.FillAsync( "" );
+            await Expect( validationFeedback ).ToHaveClassAsync( "invalid-feedback" );
+
+            await textBox.FillAsync( "some text" );
+            await Expect( validationFeedback ).ToBeHiddenAsync();
         }
 
     }
