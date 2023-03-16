33356852eb53bb5baa69fd8ee127112318c032f2
David Moreira <david_nogueira_moreira@hotmail.com>
2023-03-16 18:17:16 +0000

    Attempt WASM test

---
+++ Tests/Blazorise.E2E.Tests/Tests/Components/Button/ButtonTests.cs
@@ -0,0 +1,30 @@
+﻿using Blazorise.E2E.Tests.Infrastructure;
+
+namespace Blazorise.E2E.Tests.Tests.Components.Button
+{
+
+    [Parallelizable( ParallelScope.Self )]
+    [TestFixture]
+    public class ButtonTests : BlazorPageTest
+    {
+        [Test]
+        public async Task CanRaiseCallback()
+        {
+            await Page.GotoAsync( RootUri.AbsoluteUri );
+
+            await Page.GetByRole( AriaRole.Combobox ).SelectOptionAsync( new[] { "BasicTestApp.Client.ButtonComponent" } );
+
+            await Page.GetByRole( AriaRole.Button, new() { Name = "Count" } ).ClickAsync();
+            await Expect( Page.Locator( "#basic-button-event-result" ) ).ToHaveTextAsync( "1" );
+
+
+            await Page.GetByRole( AriaRole.Button, new() { Name = "Count" } ).ClickAsync();
+            await Expect( Page.Locator( "#basic-button-event-result" ) ).ToHaveTextAsync( "2" );
+
+
+            await Page.GetByRole( AriaRole.Button, new() { Name = "Count" } ).ClickAsync();
+            await Expect( Page.Locator( "#basic-button-event-result" ) ).ToHaveTextAsync( "3" );
+        }
+    }
+
+}
