2c8daac924cc932e3ebb3052e16f0a2d370dbb03
David Moreira <david_nogueira_moreira@hotmail.com>
2023-03-16 21:27:51 +0000

    ComponentRenderingTest

--- Tests/Blazorise.E2E.Tests/Tests/Components/_/ComponentRenderingTest.cs
+++ Tests/Blazorise.E2E.Tests/Tests/Components/_/ComponentRenderingTest.cs
@@ -1,4 +1,5 @@
-﻿using Blazorise.E2E.Tests.Infrastructure;
+﻿using BasicTestApp.Client;
+using Blazorise.E2E.Tests.Infrastructure;
 
 namespace Blazorise.E2E.Tests.Tests.Components._
 {
@@ -17,9 +18,33 @@ namespace Blazorise.E2E.Tests.Tests.Components._
         [Test]
         public async Task CanRenderTextOnlyComponent()
         {
-            await Page.GotoAsync( RootUri.AbsoluteUri );
+            await SelectTestComponent<TextOnlyComponent>();
+            await Expect( Page.GetByText( "Hello from TextOnlyComponent" ) ).ToBeVisibleAsync();
+        }
 
-            //await Expect( Page ).ToHaveTitleAsync( "Blazorise test app" );
+        [Test]
+        public async Task CanRenderButtonComponent()
+        {
+            await SelectTestComponent<ButtonOnlyComponent>();
+            var button = Page.GetByRole( AriaRole.Button );
+            await Expect( button ).ToHaveTextAsync( "hello primary" );
+            await Expect( button ).ToHaveClassAsync( "btn btn-primary" );
+        }
+
+        [Test]
+        public async Task CannotChangeElementId()
+        {
+            await SelectTestComponent<ElementIdComponent>();
+            var date = Page.GetByRole( AriaRole.Textbox );
+            var button = Page.GetByRole( AriaRole.Button );
+
+            var idBefore = await date.GetAttributeAsync( "id" );
+
+            Assert.AreNotEqual( string.Empty, idBefore );
+
+            await button.ClickAsync();
+
+            Assert.AreEqual( idBefore, await date.GetAttributeAsync( "id" ) );
         }
     }
 }
