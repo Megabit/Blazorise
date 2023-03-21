60b020b3bcd65e465de041d6522f380ecb3c81b0
David Moreira <david_nogueira_moreira@hotmail.com>
2023-03-21 22:34:00 +0000

    Fix the test by typing seperately

--- Tests/Blazorise.E2E.Tests/Tests/Components/NumericEdit/NumericEditTests.cs
+++ Tests/Blazorise.E2E.Tests/Tests/Components/NumericEdit/NumericEditTests.cs
@@ -136,6 +136,7 @@ public class NumericEditTests : BlazorisePageTest
 
         await Expect( result ).ToHaveTextAsync( "42" );
 
+        await Page.Keyboard.PressAsync( "." );
         await Page.Keyboard.PressAsync( ".3" );
         await Expect( result ).ToHaveTextAsync( "42.3" );
     }
