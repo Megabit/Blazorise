c82f5b59abb1b9efe4122d8191ceb792395ffe80
David Moreira <david_nogueira_moreira@hotmail.com>
2023-03-22 21:44:36 +0000

    Make Bunit Select Tests Async

--- Tests/Blazorise.Tests/Extensions/IElementExtensions.cs
+++ Tests/Blazorise.Tests/Extensions/IElementExtensions.cs
@@ -9,4 +9,7 @@ public static class IElementExtensions
 {
     public static Task InputAsync( this IElement element, string value )
         => element.InputAsync( new Microsoft.AspNetCore.Components.ChangeEventArgs() { Value = value } );
+
+    public static Task ChangeAsync( this IElement element, string value )
+        => element.ChangeAsync( new Microsoft.AspNetCore.Components.ChangeEventArgs() { Value = value } );
 }
