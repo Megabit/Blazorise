a93242b9321f8cf51848a073dd6f289917d50462
David Moreira <david_nogueira_moreira@hotmail.com>
2023-03-30 20:29:18 +0100

    update build.cmd

--- build.cmd
+++ build.cmd
@@ -1,2 +1,2 @@
-cd /d "%~dp0"
-"%ProgramFiles(x86)%\\Microsoft Visual Studio\2019\Preview\MSBuild\Current\Bin\MSBuild.exe" Blazorise.sln /p:Configuration=Release  /t:Restore;Rebuild /v:m
+dotnet build Blazorise.sln
+pause
