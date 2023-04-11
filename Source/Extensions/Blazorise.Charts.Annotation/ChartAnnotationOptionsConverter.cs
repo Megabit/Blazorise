a2dee7f340c4838bc843f2ac091fe1d307a4e95f
Mladen Macanovic <mladen.macanovic@gmail.com>
2023-04-11 14:35:26 +0200

    region

--- Source/Extensions/Blazorise.Charts.Annotation/ChartAnnotationOptionsConverter.cs
+++ Source/Extensions/Blazorise.Charts.Annotation/ChartAnnotationOptionsConverter.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿#region Using directives
+using System;
 using System.Text.Json;
 using System.Text.Json.Serialization;
-using System.Threading.Tasks;
+#endregion
 
 namespace Blazorise.Charts.Annotation;
 
