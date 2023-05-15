2d268906321ab851562cdb0eaecd2b877f271af9
David Moreira <david_nogueira_moreira@hotmail.com>
2023-05-15 16:30:30 +0100

    Add Multiple tests

--- Tests/Blazorise.E2E.Tests/Tests/Extensions/DataGrid/DataGridPerfTests.cs
+++ Tests/Blazorise.E2E.Tests/Tests/Extensions/DataGrid/DataGridPerfTests.cs
@@ -2,12 +2,6 @@
 
 public class DataGridPerfTests : BlazorisePageTest
 {
-    [SetUp]
-    public async Task Init()
-    {
-
-    }
-
 
     [Test]
     public async Task SelectRow_Should_RenderOnce()
@@ -30,6 +24,46 @@ public class DataGridPerfTests : BlazorisePageTest
     public async Task SelectRow_Should_RenderOnce_When_BindSelectedRowIsSet()
     {
         await SelectTestComponent<DataGridPerfBindSelectedComponent>();
+
+        var perfRenderCalls = Page.Locator( "#PerfRenderCalls" );
+        var perfReset = Page.Locator( "#PerfReset" );
+        var row = Page.Locator( "tbody tr.table-row-selectable:first-child" );
+
+        await perfReset.ClickAsync();
+        await row.ClickAsync();
+
+        var callsAfterSelection = await perfRenderCalls.InnerTextAsync();
+
+        Assert.AreEqual( 1, int.Parse( callsAfterSelection ) );
+    }
+
+    [Test]
+    public async Task SelectRow_Multiple_Should_RenderOnce()
+    {
+        await SelectTestComponent<DataGridPerfComponent>();
+
+        var selectionMode = Page.Locator( "#dataGridSelectionMode" );
+        await selectionMode.SelectOptionAsync( new[] { "Multiple" } );
+
+        var perfRenderCalls = Page.Locator( "#PerfRenderCalls" );
+        var perfReset = Page.Locator( "#PerfReset" );
+        var row = Page.Locator( "tbody tr.table-row-selectable:first-child" );
+
+        await perfReset.ClickAsync();
+        await row.ClickAsync();
+
+        var callsAfterSelection = await perfRenderCalls.InnerTextAsync();
+
+        Assert.AreEqual( 1, int.Parse( callsAfterSelection ) );
+    }
+
+    [Test]
+    public async Task SelectRow_Multiple_Should_RenderOnce_When_BindSelectedRowsIsSet()
+    {
+        await SelectTestComponent<DataGridPerfBindSelectedComponent>();
+
+        var selectionMode = Page.Locator( "#dataGridSelectionMode" );
+        await selectionMode.SelectOptionAsync( new[] { "Multiple" } );
 
         var perfRenderCalls = Page.Locator( "#PerfRenderCalls" );
         var perfReset = Page.Locator( "#PerfReset" );
