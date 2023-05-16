d9f159de1f54f91e7dc6c4c137b593a1f2afab0c
David Moreira <david_nogueira_moreira@hotmail.com>
2023-05-16 22:02:05 +0100

    Autocomplete_4772Tests

---
+++ Tests/Blazorise.E2E.Tests/Tests/Extensions/Autocomplete/Autocomplete_4772Tests.cs
@@ -0,0 +1,36 @@
+﻿namespace Blazorise.E2E.Tests.Tests.Extensions.Autocomplete;
+
+[Parallelizable( ParallelScope.Self )]
+[TestFixture]
+public class Autocomplete_4772Tests : BlazorisePageTest
+{
+    [SetUp]
+    public async Task Init()
+    {
+        await SelectTestComponent<Autocomplete_4772Component>();
+    }
+
+    /// <summary>
+    /// Tests whether the autocomplete resets correctly when the value is cleared, showing back the options when MinLength is 0.
+    /// </summary>
+    /// <returns></returns>
+    [Test]
+    public async Task Test()
+    {
+        var sut = Page.Locator( ".b-is-autocomplete input[type=search]" );
+        var dropdownMenu = Page.Locator( ".dropdown-menu.show" );
+
+
+        await sut.FillAsync( "teeeeeeeeeeeest" );
+        await sut.BlurAsync();
+
+        await Expect( sut ).ToHaveValueAsync( "" );
+
+        await sut.FocusAsync();
+
+        await dropdownMenu.WaitForAsync();
+        var optionsCount = await dropdownMenu.Locator( ".dropdown-item" ).CountAsync();
+        Assert.AreEqual( 2, optionsCount );
+    }
+}
+
