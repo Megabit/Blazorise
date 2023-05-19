f8e6400f53e56bf469649729c3150f7af83e129c
David Moreira <david_nogueira_moreira@hotmail.com>
2023-05-19 19:10:38 +0100

    FieldLabelComponentTest

---
+++ Tests/Blazorise.Tests/Components/FieldLabelComponentTest.cs
@@ -0,0 +1,37 @@
+﻿#region Using directives
+using Blazorise.Tests.Helpers;
+using Bunit;
+using FluentAssertions;
+using Xunit;
+#endregion
+
+namespace Blazorise.Tests.Components;
+
+public class FieldLabelComponentTest : TestContext
+{
+    public FieldLabelComponentTest()
+    {
+        BlazoriseConfig.AddBootstrapProviders( Services );
+        BlazoriseConfig.JSInterop.AddUtilities( JSInterop );
+    }
+
+    [Fact]
+    public void ValidationIndicator_Should_RenderIndicator()
+    {
+        // setup
+
+        // test
+        var cut = RenderComponent<FieldLabel>( parameters =>
+        {
+            parameters.Add( x => x.ValidationIndicator, true );
+            parameters.AddChildContent( "Required Field" );
+        } );
+
+        // validate
+        var requiredLabel = cut.WaitForElement( ".b-required" );
+        requiredLabel.Should().NotBeNull();
+    }
+
+
+
+}
