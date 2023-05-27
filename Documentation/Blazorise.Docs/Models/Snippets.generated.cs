53acc8eaefae879135d810c7df72184dafb2ebf3
Russell Stather <russell.stather@mac.com>
2023-05-27 14:00:49 +0100

    DataGrid: Support for row overlay (#4325)
    * Support for row overlay
    
    * Missing file added
    
    * Formating
    
    * merge fix
    
    * Add a RowOverlay Demo Example
    
    * Add position example
    
    * Simplify & Rework to use _DataGridFullColumnSpanRow
    
    * Optimize handler binding so users that do not use these features do not have uneeded performacne impact
    
    * RowOverlayPosition default to start.
    
    * Prettify example | Add background customization | Make sure zindex is higher by default
    
    * Docs : RowOverlay | RowOverlay Improvements
    
    * format
    
    * fix merge
    
    * RowOverlayTemplate => Add RowOverlayContext to Renderfragment
    
    * Rename RowLeave to RowMouseLeave and RowOver to RowMouseOver
    
    * Add stopPropagation to mouseleave & mouseover
    
    * Release notes
    
    * format
    
    * Style instead of style
    
    * Prefer a Div instead of _BaseDataGridFullColumnSpanRow since we are placing it inside a tr
    
    * Revert _DataGridFullColumnSpanRow back to original
    
    * Adjust the docs example
    
    * Provide default b-table-row-overlay class
    
    ---------
    
    Co-authored-by: Mladen Macanovic <mladen.macanovic@gmail.com>
    Co-authored-by: David Moreira <david_nogueira_moreira@hotmail.com>

--- Documentation/Blazorise.Docs/Models/Snippets.generated.cs
+++ Documentation/Blazorise.Docs/Models/Snippets.generated.cs
@@ -6332,6 +6332,25 @@ List<ChartDataLabelsDataset> lineDataLabelsDatasets = new()
     }
 }";
 
+        public const string DataGridRowOverlayExample = @"<DataGrid TItem=""Employee""
+          Data=""@employeeList""
+          RowSelectable=@((x)=> x.Item.FirstName != ""John"")
+          Responsive>
+    <DataGridColumns>
+        <DataGridColumn Field=""@nameof( Employee.FirstName )"" Caption=""Name"" Editable=""false""></DataGridColumn>
+    </DataGridColumns>
+    <RowOverlayTemplate>
+        <Div>
+            <Button Color=""Color.Primary"" Size=""Size.ExtraSmall""> <Icon Name=""IconName.User"" /> User Details > </Button>
+            <Button Color=""Color.Secondary"" Size=""Size.ExtraSmall""> <Icon Name=""IconName.Building"" /> Company Details > </Button>
+        </Div>
+    </RowOverlayTemplate>
+</DataGrid>
+
+@code {
+    private List<Employee> employeeList = new() { new() { FirstName = ""David"" }, new() { FirstName = ""MLaden"" }, new() { FirstName = ""John"" }, new() { FirstName = ""Ana"" }, new() { FirstName = ""Jessica"" } };
+}";
+
         public const string DataGridRowSelectableMultipleSelectionExample = @"<DataGrid TItem=""Employee""
           Data=""@employeeList""
           @bind-SelectedRow=""@selectedEmployee""
