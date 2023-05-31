ceee67ddcc900a25a32709ab6ecbed2167f98804
Mladen Macanovic <mladen.macanovic@gmail.com>
2023-05-31 21:45:20 +0200

    Fix readonly comment

--- Source/Extensions/Blazorise.SignaturePad/SignaturePad.razor.cs
+++ Source/Extensions/Blazorise.SignaturePad/SignaturePad.razor.cs
@@ -275,7 +275,7 @@ public partial class SignaturePad : BaseComponent, IAsyncDisposable
     [Parameter] public bool IncludeImageBackgroundColor { get; set; }
 
     /// <summary>
-    /// If true, prevents the user interactions and make it appear normal.
+    /// If true, prevents the user interactions.
     /// </summary>
     [Parameter] public bool ReadOnly { get; set; }
 
