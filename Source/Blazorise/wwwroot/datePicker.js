f3f3910eba6c21126c98c6c938ab367cf88c02e1
Mladen Macanović <mladen.macanovic@blazorise.com>
2023-05-31 20:48:53 +0200

    TimePicker: fix blur event does not fire TimeChanged event (#4795)
    * add createEvent to utilities
    
    * trigger onchange event if user click outside of input
    
    * comment
    
    * Fix datePicker blur event

--- Source/Blazorise/wwwroot/datePicker.js
+++ Source/Blazorise/wwwroot/datePicker.js
@@ -75,6 +75,16 @@ export function initialize(dotnetAdapter, element, elementId, options) {
         picker.altInput.disabled = options.disabled || false;
         picker.altInput.readOnly = options.readOnly || false;
         picker.altInput.placeholder = options.placeholder;
+
+        picker.altInput.addEventListener("blur", (e) => {
+            const isInput = e.target === picker._input;
+
+            // Workaround for: onchange does not fire when user writes the time and then click outside of the input area.
+            if (isInput && picker.isOpen === false) {
+                picker.input.dispatchEvent(utilities.createEvent("change"));
+                picker.input.dispatchEvent(utilities.createEvent("input"));
+            }
+        });
     }
 
     picker.customOptions = {
