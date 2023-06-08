2e53cc15de490452142a1fe40f1e5a9ff9d6a501
David Moreira <david_nogueira_moreira@hotmail.com>
2023-06-08 21:59:57 +0100

    Initial Approach for DataGridSelectColumn

---
+++ Source/Extensions/Blazorise.DataGrid/_DataGridCellSelectEdit.razor.cs
@@ -0,0 +1,59 @@
+﻿#region Using directives
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+#endregion
+
+namespace Blazorise.DataGrid;
+
+/// <summary>
+/// Internal component for editing the row item cell value.
+/// </summary>
+/// <typeparam name="TItem"></typeparam>
+public partial class _DataGridCellSelectEdit<TItem> : ComponentBase
+{
+    /// <summary>
+    /// Updated the internal cell values.
+    /// </summary>
+    /// <param name="value">Value that is updating.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
+    protected Task OnEditValueChanged( object value )
+    {
+        CellEditContext.CellValue = value;
+
+        if ( ValidationItem != null )
+            Column.SetValue( ValidationItem, value );
+
+        return CellValueChanged.InvokeAsync( value );
+    }
+
+    /// <summary>
+    /// Column that this cell belongs to.
+    /// </summary>
+    [Parameter] public DataGridSelectColumn<TItem> Column { get; set; }
+
+    /// <summary>
+    /// Instance of the currently editing row item.
+    /// </summary>
+    [Parameter] public TItem Item { get; set; }
+
+    /// <summary>
+    /// Instance of the currently validating row item.
+    /// </summary>
+    [Parameter] public TItem ValidationItem { get; set; }
+
+    /// <summary>
+    /// Value data type.
+    /// </summary>
+    [Parameter] public Type ValueType { get; set; }
+
+    /// <summary>
+    /// Currently editing cell value.
+    /// </summary>
+    [Parameter] public CellEditContext<TItem> CellEditContext { get; set; }
+
+    /// <summary>
+    /// Raises when cell value changes.
+    /// </summary>
+    [Parameter] public EventCallback<object> CellValueChanged { get; set; }
+}
