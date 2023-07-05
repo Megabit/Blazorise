d2ea033a6dad9133ba2e48fdc3c969d89da30709
David Moreira <david_nogueira_moreira@hotmail.com>
2023-07-05 15:42:24 +0100

    Revert "HeaderGroupCaption implementation"
    This reverts commit edfd92c90ad6bb042d626ddcd9abf55d3cc98051.

--- Source/Extensions/Blazorise.DataGrid/DataGridColumn.cs
+++ Source/Extensions/Blazorise.DataGrid/DataGridColumn.cs
@@ -730,12 +730,5 @@ public partial class DataGridColumn<TItem> : BaseDataGridColumn<TItem>
     /// </summary>
     [Parameter] public DataGridFilterMethod? FilterMethod { get; set; }
 
-
-    /// <summary>
-    /// Defines the caption to be displayed for a group header.
-    /// <para>If set, all the column headers that are part of the group will be grouped under this caption.</para>
-    /// </summary>
-    [Parameter] public string HeaderGroupCaption { get; set; }
-
     #endregion
 }
