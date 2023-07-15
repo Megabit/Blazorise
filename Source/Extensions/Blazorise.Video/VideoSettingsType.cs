e062b983e5f07e8aa9a99a8e62ee07a85aa6e6c4
Mladen Macanovic <mladen.macanovic@gmail.com>
2023-07-15 20:48:23 +0200

    Add DefaultQuality option to Video

---
+++ Source/Extensions/Blazorise.Video/VideoSettingsType.cs
@@ -0,0 +1,27 @@
+﻿namespace Blazorise.Video;
+
+/// <summary>
+/// Defines the video settings types.
+/// </summary>
+public static class VideoSettingsType
+{
+    /// <summary>
+    /// If defined, the captions settings will be available.
+    /// </summary>
+    public const string Captions = "captions";
+
+    /// <summary>
+    /// If defined, the quality settings will be available.
+    /// </summary>
+    public const string Quality = "quality";
+
+    /// <summary>
+    /// If defined, the speed settings will be available.
+    /// </summary>
+    public const string Speed = "speed";
+
+    /// <summary>
+    /// If defined, the loop settings will be available.
+    /// </summary>
+    public const string Loop = "loop";
+}
