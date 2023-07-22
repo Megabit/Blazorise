5c0f695669dd0913c8bbb499dc64213ebf141db5
David Moreira <david_nogueira_moreira@hotmail.com>
2023-07-22 11:56:44 +0100

    Move ColumnFilter to its own component | Provide translations for the FilterMethods

---
+++ Source/Extensions/Blazorise.DataGrid/_DataGridColumnFilter.razor.cs
@@ -0,0 +1,47 @@
+﻿#region Using directives
+using System;
+using Blazorise.Localization;
+using Microsoft.AspNetCore.Components;
+#endregion
+
+namespace Blazorise.DataGrid;
+
+public partial class _DataGridColumnFilter<TItem> : ComponentBase, IDisposable
+{
+    #region Methods
+
+    /// <inheritdoc/>
+    protected override void OnInitialized()
+    {
+        LocalizerService.LocalizationChanged += OnLocalizationChanged;
+
+        base.OnInitialized();
+    }
+
+    /// <inheritdoc/>
+    public void Dispose()
+    {
+        LocalizerService.LocalizationChanged -= OnLocalizationChanged;
+    }
+
+    private async void OnLocalizationChanged( object sender, EventArgs e )
+    {
+        await InvokeAsync( StateHasChanged );
+    }
+    #endregion
+
+    #region Properties
+
+    /// <summary>
+    /// Gets or sets the DI registered <see cref="ITextLocalizer"/> for <see cref="DataGrid{TItem}"/> />.
+    /// </summary>
+    [Inject] protected ITextLocalizer<DataGrid<TItem>> Localizer { get; set; }
+
+    [Inject] protected ITextLocalizerService LocalizerService { get; set; }
+
+    [CascadingParameter] public DataGrid<TItem> ParentDataGrid { get; set; }
+
+    [Parameter] public DataGridColumn<TItem> Column { get; set; }
+
+    #endregion
+}
