a37e53d053cc1df093036a6942f05e3a5f431cf3
steal4life22 <dinovrtlar0607@skole.hr>
2023-07-25 12:14:33 +0200

    Made Offcanvas for Material

--- Source/Blazorise.Material/Config.cs
+++ Source/Blazorise.Material/Config.cs
@@ -26,6 +26,7 @@ public static class Config
         }
 
         serviceCollection.AddScoped<IJSModalModule, Modules.MaterialJSModalModule>();
+        serviceCollection.AddScoped<IJSOffcanvasModule, Modules.MaterialJSOffcanvasModule>();
         serviceCollection.AddScoped<IJSTooltipModule, Modules.MaterialJSTooltipModule>();
 
         return serviceCollection;
