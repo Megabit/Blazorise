6f61d46c92bf6c3318141e718a152da759dbd8ad
David Moreira <david_nogueira_moreira@hotmail.com>
2023-08-10 11:04:01 +0100

    Datagrid: Filter support for specialized numeric column (#4920)
    * Refactor NumericEdit out of CellEdit | Reusable
    
    * DataGrid : NumericEdit support in Filtering  with specialized numeric column
    
    * Release notes
    
    * Format lines manually
    
    * Formating
    
    * Auto format : Codemaid
    
    * Revert "Auto format : Codemaid"
    
    This reverts commit 47f4bc84ec62be81c22c739fe6f9f668e94dd577.
    
    ---------
    
    Co-authored-by: Mladen Macanovic <mladen.macanovic@gmail.com>

---
+++ Source/Extensions/Blazorise.DataGrid/_DataGridCellNumericEdit.razor.cs
@@ -0,0 +1,44 @@
+﻿#region Using directives
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+#endregion
+
+namespace Blazorise.DataGrid;
+
+/// <summary>
+/// Internal component for editing the row item cell value.
+/// </summary>
+/// <typeparam name="TItem"></typeparam>
+public partial class _DataGridCellNumericEdit<TItem> : ComponentBase
+{
+    /// <summary>
+    /// Value data type.
+    /// </summary>
+    private Type valueType;
+
+    /// <inheritdoc/>
+    protected override void OnInitialized()
+    {
+        valueType = Column.GetValueType();
+        base.OnInitialized();
+    }
+
+    public Task OnCellValueChanged<TValue>( TValue value )
+        => CellValueChanged.InvokeAsync( value );
+
+    /// <summary>
+    /// Column that this cell belongs to.
+    /// </summary>
+    [Parameter] public DataGridNumericColumn<TItem> Column { get; set; }
+
+    /// <summary>
+    /// Currently editing cell value.
+    /// </summary>
+    [Parameter] public object CellValue { get; set; }
+
+    /// <summary>
+    /// Raises when cell value changes.
+    /// </summary>
+    [Parameter] public EventCallback<object> CellValueChanged { get; set; }
+}
