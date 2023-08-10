c1f3be31ce7f9924d1034eb423ed5376cdb2aa58
steal4life <dino.vrtlar@blazorise.com>
2023-08-10 13:55:28 +0200

    Removed unnecessary check inside of Autocomplete

--- Source/Extensions/Blazorise.Components/Autocomplete.razor.cs
+++ Source/Extensions/Blazorise.Components/Autocomplete.razor.cs
@@ -1091,9 +1091,6 @@ public partial class Autocomplete<TItem, TValue> : BaseAfterRenderComponent, IAs
             if ( Style != null )
                 sb.Append( Style );
 
-            if ( Disabled != false )
-                sb.Append( "background-color: #e9ecef;" );
-
             return sb.ToString();
         }
     }
