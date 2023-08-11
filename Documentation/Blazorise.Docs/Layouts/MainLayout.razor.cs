bd738e69cb23018e271df1c952d44fbb5631fa11
Mladen Macanovic <mladen.macanovic@gmail.com>
2023-08-11 15:01:13 +0200

    Main buttons selected state

--- Documentation/Blazorise.Docs/Layouts/MainLayout.razor.cs
+++ Documentation/Blazorise.Docs/Layouts/MainLayout.razor.cs
@@ -1,5 +1,6 @@
 ﻿#region Using directives
 using Blazorise;
+using Microsoft.AspNetCore.Components;
 #endregion
 
 
@@ -10,6 +11,24 @@ public partial class MainLayout
     #region Members
 
     private bool topbarVisible = false;
+
+    #endregion
+
+    #region Properties
+
+    private static IFluentBorder SelectedBorders = Border.Rounded.Is1.Primary.OnBottom.Is1.Primary.OnTop;
+
+    private IFluentBorder CommunityBorders => !IsComercialPage ? SelectedBorders : null;
+    private TextWeight CommunityTextWeight => !IsComercialPage ? TextWeight.Normal : TextWeight.Default;
+    private Shadow CommunityShadow => !IsComercialPage ? Shadow.Small : Shadow.None;
+
+    private IFluentBorder CommercialBorders => IsComercialPage ? SelectedBorders : null;
+    private TextWeight CommercialTextWeight => IsComercialPage ? TextWeight.Normal : TextWeight.Default;
+    private Shadow CommercialShadow => IsComercialPage ? Shadow.Small : Shadow.None;
+
+    private bool IsComercialPage => NavigationManager.Uri != null && NavigationManager.ToBaseRelativePath( NavigationManager.Uri ) == "commercial";
+
+    [Inject] public NavigationManager NavigationManager { get; set; }
 
     #endregion
 }
