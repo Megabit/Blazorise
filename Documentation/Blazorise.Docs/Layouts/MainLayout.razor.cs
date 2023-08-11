824bcb7e927fab691def4a4ebfcbea4a86bfc24d
Mladen Macanovic <mladen.macanovic@gmail.com>
2023-08-11 21:23:02 +0200

    Reorganize top bar

--- Documentation/Blazorise.Docs/Layouts/MainLayout.razor.cs
+++ Documentation/Blazorise.Docs/Layouts/MainLayout.razor.cs
@@ -3,7 +3,6 @@ using Blazorise;
 using Microsoft.AspNetCore.Components;
 #endregion
 
-
 namespace Blazorise.Docs.Layouts;
 
 public partial class MainLayout
@@ -11,24 +10,6 @@ public partial class MainLayout
     #region Members
 
     private bool topbarVisible = false;
-
-    #endregion
-
-    #region Properties
-
-    private static IFluentBorder SelectedBorders = Border.Rounded.Is1.Primary.OnBottom.Is1.Primary.OnTop;
-
-    private IFluentBorder CommunityBorders => !IsComercialPage ? SelectedBorders : null;
-    private TextWeight CommunityTextWeight => !IsComercialPage ? TextWeight.Normal : TextWeight.Default;
-    private Shadow CommunityShadow => !IsComercialPage ? Shadow.Small : Shadow.None;
-
-    private IFluentBorder CommercialBorders => IsComercialPage ? SelectedBorders : null;
-    private TextWeight CommercialTextWeight => IsComercialPage ? TextWeight.Normal : TextWeight.Default;
-    private Shadow CommercialShadow => IsComercialPage ? Shadow.Small : Shadow.None;
-
-    private bool IsComercialPage => NavigationManager.Uri != null && NavigationManager.ToBaseRelativePath( NavigationManager.Uri ) == "commercial";
-
-    [Inject] public NavigationManager NavigationManager { get; set; }
 
     #endregion
 }
