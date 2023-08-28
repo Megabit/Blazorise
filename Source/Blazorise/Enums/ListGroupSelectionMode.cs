887b477edcaa574d939e1b0a523dd9bbabf4d42f
steal4life <dino.vrtlar@blazorise.com>
2023-08-28 15:11:55 +0200

    Created enums for selection mode

---
+++ Source/Blazorise/Enums/ListGroupSelectionMode.cs
@@ -0,0 +1,17 @@
+﻿namespace Blazorise;
+
+/// <summary>
+/// Specifies the selection mode for the list group.
+/// </summary>
+public enum ListGroupSelectionMode
+{
+    /// <summary>
+    /// Allows multiple items to be selected.
+    /// </summary>
+    Multiple,
+
+    /// <summary>
+    /// Allows only a single item to be selected.
+    /// </summary>
+    Single
+}
