bd3c6428bd6d61e6adcae8d08be78544cb66671d
David Moreira <david_nogueira_moreira@hotmail.com>
2023-10-05 16:26:55 +0100

    Autocomplete | Fix FreeTypingNotFoundTemplate not showing

---
+++ Tests/Blazorise.E2E.Tests/Tests/Extensions/Autocomplete/Autocomplete_5038_Tests.cs
@@ -0,0 +1,35 @@
+﻿namespace Blazorise.E2E.Tests.Tests.Extensions.Autocomplete;
+
+[Parallelizable( ParallelScope.Self )]
+[TestFixture]
+public class Autocomplete_5038_Tests : BlazorisePageTest
+{
+
+    [SetUp]
+    public async Task Init()
+    {
+        await SelectTestComponent<Autocomplete_5038Component>();
+    }
+
+    /// <summary>
+    /// When a value is already set, setting a different value programmatically and then revisiting should have updated the Current Search Value and options shown.
+    /// </summary>
+    /// <returns></returns>
+    [Test]
+    public async Task Test()
+    {
+        var sut = Page.Locator( ".b-is-autocomplete input[type=search]" );
+        var dropdownMenu = Page.Locator( ".dropdown-menu.show" );
+
+        await sut.ClickAsync();
+        await sut.FillAsync( "My Color" );
+        await sut.PressAsync( "Enter" );
+        await Expect( sut ).ToHaveValueAsync( "My Color" );
+
+        await dropdownMenu.WaitForAsync();
+        var option = ( await dropdownMenu.Locator( ".dropdown-item" ).AllAsync() ).Single();
+
+        await Expect( option ).ToHaveTextAsync( @"Add ""My Color""" );
+    }
+}
+
