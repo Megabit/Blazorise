c2131cbed65414617b2f4926e1f130eaad2f6703
David Moreira <david_nogueira_moreira@hotmail.com>
2023-10-10 17:31:03 +0100

    Fix GroupHeaderCaptions Columns algorithm grouping of remaining columns

--- Source/Extensions/Blazorise.DataGrid/DataGrid.razor.cs
+++ Source/Extensions/Blazorise.DataGrid/DataGrid.razor.cs
@@ -1930,7 +1930,7 @@ public partial class DataGrid<TItem> : BaseDataGridComponent
                 {
                     var toRemove = new List<DataGridColumn<TItem>>();
 
-                    foreach ( var remainingDisplayColumn in orderedDisplayColumns.Skip( i + 1 ) )
+                    foreach ( var remainingDisplayColumn in orderedDisplayColumnsAsList.Skip( i + 1 ) )
                     {
                         if ( remainingDisplayColumn.HeaderGroupCaption == displayColumn.HeaderGroupCaption )
                         {
