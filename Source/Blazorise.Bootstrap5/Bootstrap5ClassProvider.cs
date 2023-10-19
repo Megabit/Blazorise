184f314439bfbb9291f9136039238119fee39693
steal4life <dino.vrtlar@blazorise.com>
2023-10-19 15:12:05 +0200

    made CardLinkUnstyled for bootstrap5

--- Source/Blazorise.Bootstrap5/Bootstrap5ClassProvider.cs
+++ Source/Blazorise.Bootstrap5/Bootstrap5ClassProvider.cs
@@ -620,6 +620,8 @@ public class Bootstrap5ClassProvider : ClassProvider
 
     public override string CardLink() => "card-link";
 
+    public override string CardLinkUnstyled( bool unstyled ) => unstyled ? "link-unstyled" : null;
+
     public override string CardLinkActive( bool active ) => LinkActive( active );
 
     #endregion
