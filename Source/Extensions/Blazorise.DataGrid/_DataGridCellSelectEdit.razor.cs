e4ee3542681243f567d92b710cfa578c62813235
David Moreira <david_nogueira_moreira@hotmail.com>
2023-10-24 12:20:46 +0100

    DataGridCellSelectEdit | Workaround to figure out proper TValue internally

--- Source/Extensions/Blazorise.DataGrid/_DataGridCellSelectEdit.razor.cs
+++ Source/Extensions/Blazorise.DataGrid/_DataGridCellSelectEdit.razor.cs
@@ -1,4 +1,5 @@
 ﻿#region Using directives
+using System.Linq;
 using Microsoft.AspNetCore.Components;
 #endregion
 
@@ -26,4 +27,39 @@ public partial class _DataGridCellSelectEdit<TItem> : ComponentBase
     [Parameter] public EventCallback<object> CellValueChanged { get; set; }
 
     [Parameter] public bool ShowValidationFeedback { get; set; }
+
+    private System.Collections.Generic.List<(string text, object value, bool disabled)> selectItems;
+    protected override void OnInitialized()
+    {
+        if ( Column.Data is not null )
+        {
+            selectItems = new();
+            foreach ( var item in Column.Data )
+            {
+                var text = Column.TextField?.Invoke( item );
+                var value = Column.ValueField != null ? Column.ValueField.Invoke( item ) : default;
+                var disabled = Column.ItemDisabled != null && Column.ItemDisabled.Invoke( item );
+                selectItems.Add( (text, value, disabled) );
+            }
+        }
+
+        base.OnInitialized();
+    }
+    private void OnSelectedValueChanged( object value )
+    {
+        if ( selectItems is not null )
+        {
+            //Using a Select<object> makes it so the internal value representation is always a string instead of the actual Value type from ValueField.
+            //This is a problem, because when the value is passed back to the DataGrid internal item setter, it is passed as a string instead of the actual Value type.
+            //This workaround, resolves the string value back to the actual Value type by matching the string representation back to the pre-generated list of select items which contain the actual Value type.
+            var selectItem = selectItems
+                .FirstOrDefault( x => ( x.value?.ToString() ?? string.Empty ).Equals( value?.ToString() ?? string.Empty, System.StringComparison.Ordinal ) );
+
+            CellValueChanged.InvokeAsync( selectItem.value );
+        }
+        else
+        {
+            CellValueChanged.InvokeAsync( value );
+        }
+    }
 }
