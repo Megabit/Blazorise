97e153087d5ec172aa74c31e94330e41c5a0c9f8
steal4life <dino.vrtlar@blazorise.com>
2023-10-24 15:08:46 +0200

    made unstyled parameter for CardLink

--- Source/Blazorise.AntDesign/AntDesignClassProvider.cs
+++ Source/Blazorise.AntDesign/AntDesignClassProvider.cs
@@ -626,6 +626,8 @@ public class AntDesignClassProvider : ClassProvider
 
     public override string CardLink() => "ant-card-extra";
 
+    public override string CardLinkUnstyled( bool unstyled ) => unstyled ? "ant-link-unstyled" : null;
+
     public override string CardLinkActive( bool active ) => LinkActive( active );
 
     #endregion
