47eea02868e3aaecab75073cb7cd8928f0e7d1ae
Mladen Macanovic <mladen.macanovic@gmail.com>
2023-11-06 18:12:12 +0100

    Format

--- Source/Blazorise/wwwroot/datePicker.js
+++ Source/Blazorise/wwwroot/datePicker.js
@@ -358,42 +358,43 @@ export function select(element, elementId, focus) {
 }
 
 class ClassWatcher {
-
     constructor(targetNode, classToWatch, classAddedCallback, classRemovedCallback) {
-        this.targetNode = targetNode
-        this.classToWatch = classToWatch
-        this.classAddedCallback = classAddedCallback
-        this.classRemovedCallback = classRemovedCallback
-        this.observer = null
-        this.lastClassState = targetNode.classList.contains(this.classToWatch)
+        this.targetNode = targetNode;
+        this.classToWatch = classToWatch;
+        this.classAddedCallback = classAddedCallback;
+        this.classRemovedCallback = classRemovedCallback;
+        this.observer = null;
+        this.lastClassState = targetNode.classList.contains(this.classToWatch);
 
-        this.init()
+        this.init();
     }
 
     init() {
-        this.observer = new MutationObserver(this.mutationCallback)
-        this.observe()
+        this.observer = new MutationObserver(this.mutationCallback);
+        this.observe();
     }
 
     observe() {
-        this.observer.observe(this.targetNode, { attributes: true })
+        this.observer.observe(this.targetNode, { attributes: true });
     }
 
     disconnect() {
-        this.observer.disconnect()
+        this.observer.disconnect();
     }
 
     mutationCallback = mutationsList => {
         for (let mutation of mutationsList) {
             if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
-                let currentClassState = mutation.target.classList.contains(this.classToWatch)
+                let currentClassState = mutation.target.classList.contains(this.classToWatch);
+
                 if (this.lastClassState !== currentClassState) {
-                    this.lastClassState = currentClassState
+                    this.lastClassState = currentClassState;
+
                     if (currentClassState) {
-                        this.classAddedCallback()
+                        this.classAddedCallback();
                     }
                     else {
-                        this.classRemovedCallback()
+                        this.classRemovedCallback();
                     }
                 }
             }
