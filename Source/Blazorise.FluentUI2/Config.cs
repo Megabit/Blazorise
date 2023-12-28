fb7bf8d783f8588057d0ce74a9b00801f82a935f
Mladen Macanovic <mladen.macanovic@gmail.com>
2023-12-28 20:56:19 +0100

    Radio

--- Source/Blazorise.FluentUI2/Config.cs
+++ Source/Blazorise.FluentUI2/Config.cs
@@ -41,6 +41,7 @@ public static class Config
         { typeof( Blazorise.NumericEdit<> ), typeof( FluentUI2.NumericEdit<> ) },
         { typeof( Blazorise.TextEdit ), typeof( FluentUI2.TextEdit ) },
         { typeof( Blazorise.TimeEdit<> ), typeof( FluentUI2.TimeEdit<> ) },
+        { typeof( Blazorise.Radio<> ), typeof( FluentUI2.Radio<> ) },
         { typeof( Blazorise.Select<> ), typeof( FluentUI2.Select<> ) },
         { typeof( Blazorise.ValidationError ), typeof( FluentUI2.ValidationError ) },
         { typeof( Blazorise.ValidationSuccess ), typeof( FluentUI2.ValidationSuccess ) },
