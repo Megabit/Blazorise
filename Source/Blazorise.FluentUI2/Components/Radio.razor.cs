10689a0fde2cad613a706fd7951c1563e8b78356
Mladen Macanovic <mladen.macanovic@blazorise.com>
2024-01-15 11:36:23 +0100

    Cache input classnames and styles

---
+++ Source/Blazorise.FluentUI2/Components/Radio.razor.cs
@@ -0,0 +1,57 @@
+﻿#region Using directives
+using Blazorise.Utilities;
+#endregion
+
+namespace Blazorise.FluentUI2.Components;
+
+public partial class Radio<TValue>
+{
+    #region Constructors
+
+    public Radio()
+    {
+        InputClassBuilder = new ClassBuilder( BuildInputClasses );
+    }
+
+    #endregion
+
+    #region Methods
+
+    protected internal override void DirtyClasses()
+    {
+        InputClassBuilder.Dirty();
+
+        base.DirtyClasses();
+    }
+
+    private void BuildInputClasses( ClassBuilder builder )
+    {
+        builder.Append( "fui-Radio" );
+
+        if ( ParentValidation?.Status == ValidationStatus.Error )
+        {
+            builder.Append( "fui-Radio__error" );
+        }
+        else if ( ParentValidation?.Status == ValidationStatus.Success )
+        {
+            builder.Append( "fui-Radio__success" );
+        }
+
+        if ( Disabled )
+        {
+            builder.Append( "disabled" );
+        }
+    }
+
+    #endregion
+
+    #region Properties
+
+    protected ClassBuilder InputClassBuilder { get; private set; }
+
+    protected string InputClassNames => InputClassBuilder.Class;
+
+    protected string AddonClassNames => "fui-Input__content";
+
+    #endregion
+}
