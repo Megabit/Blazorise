63ef4e02fce5f2040b61387a11f59441f1d4c7a8
Mladen Macanovic <mladen.macanovic@gmail.com>
2024-01-16 21:20:22 +0100

    Tooltip component

--- Source/Blazorise.FluentUI2/wwwroot/tooltip.js
+++ Source/Blazorise.FluentUI2/wwwroot/tooltip.js
@@ -1,10 +1,33 @@
 import { initialize as baseInitialize, destroy, updateContent } from "../Blazorise/tooltip.js?v=1.4.0.0";
 
 export function initialize(element, elementId, options) {
-    baseInitialize(element, elementId, options);
+    options = Object.assign({}, options, { theme: "fluentui" });
 
-    if (options.autodetectInline && element && element.querySelector(".custom-control-input,button")) {
-        element.classList.add("inline-block");
+    const tippyInstance = baseInitialize(element, elementId, options);
+
+    if (options.autodetectInline && element && element.querySelector(".fui-Input,.fui-Input__input,button")) {
+        element.classList.add("fui-Display-inline-block");
+    }
+
+    if (tippyInstance && tippyInstance.popper) {
+        tippyInstance.popper.className = "fui-Tooltip";
+
+        const tippyBox = tippyInstance.popper.querySelector(".tippy-box");
+
+        if (tippyBox) {
+            tippyBox.className = "fui-Tooltip__box";
+
+            const tippyContent = tippyBox.querySelector(".tippy-content");
+
+            if (tippyContent) {
+                tippyContent.className = "fui-Tooltip__content";
+            }
+            const tippyArrow = tippyBox.querySelector(".tippy-arrow");
+
+            if (tippyArrow) {
+                tippyArrow.classList.add("fui-Tooltip__arrow");
+            }
+        }
     }
 }
 
