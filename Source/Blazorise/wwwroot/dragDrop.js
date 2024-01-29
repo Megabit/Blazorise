a943dcf121828be19b2ff17334c7adf0c1cadc1e
Mladen Macanovic <mladen.macanovic@blazorise.com>
2024-01-29 11:42:14 +0100

    Update JS and CSS query version to 1.4.2

--- Source/Blazorise/wwwroot/dragDrop.js
+++ Source/Blazorise/wwwroot/dragDrop.js
@@ -1,4 +1,4 @@
-﻿import { getRequiredElement } from "./utilities.js?v=1.4.1.0";
+﻿import { getRequiredElement } from "./utilities.js?v=1.4.2.0";
 
 export function initialize(element, elementId) {
     element = getRequiredElement(element, elementId);
--- Source/Extensions/Blazorise.SignaturePad/wwwroot/signaturepad.js
+++ Source/Extensions/Blazorise.SignaturePad/wwwroot/signaturepad.js
@@ -1,5 +1,5 @@
-import { getRequiredElement } from "../Blazorise/utilities.js?v=1.4.1.0";
-import "./vendors/sigpad.js?v=1.4.1.0";
+import { getRequiredElement } from "../Blazorise/utilities.js?v=1.4.2.0";
+import "./vendors/sigpad.js?v=1.4.2.0";
 
 const _instances = [];
 
