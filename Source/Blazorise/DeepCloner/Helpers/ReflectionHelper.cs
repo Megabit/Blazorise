cf1b3cd8214261a5b2127b3cdce8a86aa38a49b8
David Moreira <david_nogueira_moreira@hotmail.com>
2024-02-09 11:33:13 +0000

    Copy DeepCloner implementation | Remove non dotnet core code | Make sure it compiles

---
+++ Source/Blazorise/DeepCloner/Helpers/ReflectionHelper.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Blazorise.DeepCloner.Helpers;
+
+internal static class ReflectionHelper
+{
+    public static bool IsEnum( this Type t )
+    {
+        return t.GetTypeInfo().IsEnum;
+
+    }
+
+    public static bool IsValueType( this Type t )
+    {
+        return t.GetTypeInfo().IsValueType;
+
+    }
+
+    public static bool IsClass( this Type t )
+    {
+        return t.GetTypeInfo().IsClass;
+
+    }
+
+    public static Type BaseType( this Type t )
+    {
+        return t.GetTypeInfo().BaseType;
+
+    }
+
+    public static FieldInfo[] GetAllFields( this Type t )
+    {
+        return t.GetTypeInfo().DeclaredFields.Where( x => !x.IsStatic ).ToArray();
+
+    }
+
+    public static PropertyInfo[] GetPublicProperties( this Type t )
+    {
+        return t.GetTypeInfo().DeclaredProperties.ToArray();
+
+    }
+
+    public static FieldInfo[] GetDeclaredFields( this Type t )
+    {
+        return t.GetTypeInfo().DeclaredFields.Where( x => !x.IsStatic ).ToArray();
+
+    }
+
+    public static ConstructorInfo[] GetPrivateConstructors( this Type t )
+    {
+        return t.GetTypeInfo().DeclaredConstructors.ToArray();
+
+    }
+
+    public static ConstructorInfo[] GetPublicConstructors( this Type t )
+    {
+        return t.GetTypeInfo().DeclaredConstructors.ToArray();
+
+    }
+
+    public static MethodInfo GetPrivateMethod( this Type t, string methodName )
+    {
+        return t.GetTypeInfo().GetDeclaredMethod( methodName );
+
+    }
+
+    public static MethodInfo GetMethod( this Type t, string methodName )
+    {
+        return t.GetTypeInfo().GetDeclaredMethod( methodName );
+
+    }
+
+    public static MethodInfo GetPrivateStaticMethod( this Type t, string methodName )
+    {
+        return t.GetTypeInfo().GetDeclaredMethod( methodName );
+
+    }
+
+    public static FieldInfo GetPrivateField( this Type t, string fieldName )
+    {
+        return t.GetTypeInfo().GetDeclaredField( fieldName );
+
+    }
+
+    public static FieldInfo GetPrivateStaticField( this Type t, string fieldName )
+    {
+        return t.GetTypeInfo().GetDeclaredField( fieldName );
+
+    }
+
+    public static bool IsSubclassOfTypeByName( this Type t, string typeName )
+    {
+        while ( t != null )
+        {
+            if ( t.Name == typeName )
+                return true;
+            t = t.BaseType();
+        }
+
+        return false;
+    }
+
+    public static bool IsAssignableFrom( this Type from, Type to )
+    {
+        return from.GetTypeInfo().IsAssignableFrom( to.GetTypeInfo() );
+    }
+
+    public static bool IsInstanceOfType( this Type from, object to )
+    {
+        return from.IsAssignableFrom( to.GetType() );
+    }
+
+    public static Type[] GenericArguments( this Type t )
+    {
+        return t.GetTypeInfo().GenericTypeArguments;
+
+    }
+}
---
+++ Source/Blazorise/DeepCloner/Helpers/ShallowObjectCloner.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Blazorise.DeepCloner.Helpers;
+
+/// <summary>
+/// Internal class but due implementation restriction should be public
+/// </summary>
+public abstract class ShallowObjectCloner
+{
+    /// <summary>
+    /// Abstract method for real object cloning
+    /// </summary>
+    protected abstract object DoCloneObject( object obj );
+
+    private static readonly ShallowObjectCloner _unsafeInstance;
+
+    private static ShallowObjectCloner _instance;
+
+    /// <summary>
+    /// Performs real shallow object clone
+    /// </summary>
+    public static object CloneObject( object obj )
+    {
+        return _instance.DoCloneObject( obj );
+    }
+
+    internal static bool IsSafeVariant()
+    {
+        return _instance is ShallowSafeObjectCloner;
+    }
+
+    static ShallowObjectCloner()
+    {
+
+        _instance = new ShallowSafeObjectCloner();
+        // no unsafe variant for core
+        _unsafeInstance = _instance;
+    }
+
+    /// <summary>
+    /// Purpose of this method is testing variants
+    /// </summary>
+    internal static void SwitchTo( bool isSafe )
+    {
+        DeepClonerCache.ClearCache();
+        if ( isSafe )
+            _instance = new ShallowSafeObjectCloner();
+        else
+            _instance = _unsafeInstance;
+    }
+
+
+    private class ShallowSafeObjectCloner : ShallowObjectCloner
+    {
+        private static readonly Func<object, object> _cloneFunc;
+
+        static ShallowSafeObjectCloner()
+        {
+            var methodInfo = typeof( object ).GetPrivateMethod( "MemberwiseClone" );
+            var p = Expression.Parameter( typeof( object ) );
+            var mce = Expression.Call( p, methodInfo );
+            _cloneFunc = Expression.Lambda<Func<object, object>>( mce, p ).Compile();
+        }
+
+        protected override object DoCloneObject( object obj )
+        {
+            return _cloneFunc( obj );
+        }
+    }
+}
