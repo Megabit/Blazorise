509dbf48afea9be501be1e44f6b49b16f0c20ab4
Mladen Macanovic <mladen.macanovic@gmail.com>
2024-02-10 13:37:03 +0100

    Rename Button state classes

--- Source/Blazorise.FluentUI2/Providers/FluentUI2ClassProvider.cs
+++ Source/Blazorise.FluentUI2/Providers/FluentUI2ClassProvider.cs
@@ -402,13 +402,13 @@ public class FluentUI2ClassProvider : ClassProvider
 
     public override string ButtonBlock( bool outline ) => $"{Button( outline )}-block";
 
-    public override string ButtonActive( bool outline ) => "active";
+    public override string ButtonActive( bool outline ) => "fui-Button-active";
 
-    public override string ButtonDisabled( bool outline ) => "disabled";
+    public override string ButtonDisabled( bool outline ) => "fui-Button-disabled";
 
-    public override string ButtonLoading( bool outline ) => "loading";
+    public override string ButtonLoading( bool outline ) => "fui-Button-loading";
 
-    public override string ButtonStretchedLink( bool stretched ) => stretched ? "link-stretched" : null;
+    public override string ButtonStretchedLink( bool stretched ) => stretched ? "fui-Button-link-stretched" : null;
 
     #endregion
 
