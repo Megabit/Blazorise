3bce7ad2a36ccabd2a0aa99150ac352dfa412332
Geert van Horrik <GeertvanHorrik@users.noreply.github.com>
2024-02-10 21:51:01 +0100

    Update BootstrapGuideSourceFilesExample.snippet (#5302)
    * Update BootstrapGuideSourceFilesExample.snippet
    
    * Compile docs files
    
    ---------
    
    Co-authored-by: David Moreira <david_nogueira_moreira@hotmail.com>

--- Documentation/Blazorise.Docs/Models/Snippets.generated.cs
+++ Documentation/Blazorise.Docs/Models/Snippets.generated.cs
@@ -9732,7 +9732,7 @@ builder.Services
 	<div id=""app""></div>
 
 	<!-- inside of body section and after the div/app tag  -->
-	<!-- These are the standard js dependencies this provider tipically dependes upon, but Blazorise deems these as optional as Blazorise Components should work correctly without these  -->
+	<!-- These are the standard js dependencies this provider typically dependes upon, but Blazorise deems these as optional as Blazorise Components should work correctly without these  -->
 	<script src=""https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"" integrity=""sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"" crossorigin=""anonymous""></script>
 	<script src=""https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"" integrity=""sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"" crossorigin=""anonymous""></script>
 	<script src=""https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js"" integrity=""sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2"" crossorigin=""anonymous""></script>
