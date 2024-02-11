ffb6f9a3cf9ff5fc6576719887b34f6d1521bb27
Mladen Macanovic <mladen.macanovic@gmail.com>
2024-02-11 14:11:50 +0100

    CaptchaState comments

--- Source/Extensions/Blazorise.Captcha/CaptchaState.cs
+++ Source/Extensions/Blazorise.Captcha/CaptchaState.cs
@@ -1,8 +1,17 @@
 ﻿namespace Blazorise.Captcha;
 
+/// <summary>
+/// Provides contextual information about the Captcha state.
+/// </summary>
 public class CaptchaState
 {
+    /// <summary>
+    /// Gets or sets the Captcha validation result.
+    /// </summary>
     public bool Valid { get; set; }
-    public string Response { get; set; }
 
+    /// <summary>
+    /// Gets or sets the Captcha response from the API.
+    /// </summary>
+    public string Response { get; set; }
 }
