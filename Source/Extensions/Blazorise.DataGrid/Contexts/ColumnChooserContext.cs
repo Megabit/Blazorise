28062db2bd403587d22d917ba0ca4f84ff316736
David Moreira <david_nogueira_moreira@hotmail.com>
2024-02-22 13:22:37 +0000

    ColumnChooserContext docs

--- Source/Extensions/Blazorise.DataGrid/Contexts/ColumnChooserContext.cs
+++ Source/Extensions/Blazorise.DataGrid/Contexts/ColumnChooserContext.cs
@@ -1,17 +1,28 @@
 ﻿#region Using directives
-#endregion
 
 using System.Collections.Generic;
 using Blazorise.DataGrid.EventArguments;
 using Microsoft.AspNetCore.Components;
 
+#endregion
+
 namespace Blazorise.DataGrid;
 
+/// <summary>
+/// Context for the column chooser.
+/// </summary>
+/// <typeparam name="TItem"></typeparam>
 public class ColumnChooserContext<TItem>
 {
+    /// <summary>
+    /// The list of columns that can be displayed.
+    /// </summary>
     public IEnumerable<DataGridColumn<TItem>> Columns { get; private set; }
-    public EventCallback<ColumnDisplayChangedEventArgs<TItem>> ColumnDisplayChanged { get; private set; }
 
+    /// <summary>
+    /// The event that is raised when the column display changes.
+    /// </summary>
+    public EventCallback<ColumnDisplayChangedEventArgs<TItem>> ColumnDisplayChanged { get; private set; }
 
     public ColumnChooserContext( IEnumerable<DataGridColumn<TItem>> columns, EventCallback<ColumnDisplayChangedEventArgs<TItem>> columnDisplayChanged )
     {
