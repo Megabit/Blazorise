17a4109ee2ee7b9781ce8d94f650a49a36150870
David Moreira <david_nogueira_moreira@hotmail.com>
2024-02-28 08:05:54 +0000

    Autocomplete: Only ResyncText if not Multiple (#5335)

--- Source/Extensions/Blazorise.Components/Autocomplete.razor.cs
+++ Source/Extensions/Blazorise.Components/Autocomplete.razor.cs
@@ -507,8 +507,10 @@ public partial class Autocomplete<TItem, TValue> : BaseAfterRenderComponent, IAs
 
                 await Revalidate();
             }
-
-            await ResyncText();
+            else
+            {
+                await ResyncText();
+            }
 
             return;
         }
