d3b98562cc85cbf20d8c1da1d110b4375f800cfb
Mladen Macanovic <mladen.macanovic@gmail.com>
2024-02-29 21:28:57 +0100

    Override BS5 SetVisibleState to give it more time on closing

---
+++ Source/Blazorise.Bootstrap5/Toast.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+
+namespace Blazorise.Bootstrap5;
+
+public class Toast : Blazorise.Toast
+{
+    protected override async Task SetVisibleState( bool visible )
+    {
+        if ( visible )
+            State = State with { Visible = visible };
+
+        await HandleVisibilityStyles( visible );
+        await RaiseEvents( visible );
+
+        if ( !visible )
+            State = State with { Visible = visible };
+    }
+}
