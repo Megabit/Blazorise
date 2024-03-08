36315bdee3c0c43b86d5278e1c9ed3ff5cb3aa93
Mladen Macanovic <mladen.macanovic@blazorise.com>
2024-03-08 14:25:35 +0100

    Add ImageType and ImageQuality to Cropper export options

--- Source/Extensions/Blazorise.Cropper/CropperCropOptions.cs
+++ Source/Extensions/Blazorise.Cropper/CropperCropOptions.cs
@@ -14,4 +14,17 @@ public class CropperCropOptions
     /// The destination height  of the output canvas.
     /// </summary>
     public int Height { get; set; }
+
+    /// <summary>
+    /// A string indicating the image format. The default type is image/png; this image format will be also used if the specified type is not supported.
+    /// </summary>
+    public string ImageType { get; set; } = "image/png";
+
+    /// <summary>
+    /// A Number between 0 and 1 indicating the image quality to be used when creating images using file formats that support lossy compression (such as image/jpeg or image/webp).
+    /// </summary>
+    /// <remarks>
+    /// A user agent will use its default quality value if this option is not specified, or if the number is outside the allowed range.
+    /// </remarks>
+    public double ImageQuality { get; set; }
 }
