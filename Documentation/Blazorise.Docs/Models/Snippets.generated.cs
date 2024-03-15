a2e52e58277dcb6b459a9b2198ca9ca4b61da96b
Mladen Macanovic <mladen.macanovic@gmail.com>
2024-03-15 21:28:10 +0100

    Remove BS4 and BS5 script mentions

--- Documentation/Blazorise.Docs/Models/Snippets.generated.cs
+++ Documentation/Blazorise.Docs/Models/Snippets.generated.cs
@@ -10036,15 +10036,6 @@ builder.Services
 	<link href=""_content/Blazorise/blazorise.css"" rel=""stylesheet"" />
 	<link href=""_content/Blazorise.Bootstrap/blazorise.bootstrap.css"" rel=""stylesheet"" />
 </head>
-<body>
-	<div id=""app""></div>
-
-	<!-- inside of body section and after the div/app tag  -->
-	<!-- These are the standard js dependencies this provider typically dependes upon, but Blazorise deems these as optional as Blazorise Components should work correctly without these  -->
-	<script src=""https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"" integrity=""sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"" crossorigin=""anonymous""></script>
-	<script src=""https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"" integrity=""sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"" crossorigin=""anonymous""></script>
-	<script src=""https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js"" integrity=""sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2"" crossorigin=""anonymous""></script>
-</body>
 </html>";
 
         public const string BootstrapGuideUsingExample = @"@using Blazorise";
@@ -10074,13 +10065,6 @@ builder.Services
   <link href=""_content/Blazorise/blazorise.css"" rel=""stylesheet"" />
   <link href=""_content/Blazorise.Bootstrap5/blazorise.bootstrap5.css"" rel=""stylesheet"" />
 </head>
-<body>
-  <div id=""app""></div>
-
-  <!-- inside of body section and after the div/app tag  -->
-  <!-- These are the standard js dependencies this provider typically dependes upon, but Blazorise deems these as optional as Blazorise Components should work correctly without these  -->
-  <script src=""https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"" integrity=""sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ"" crossorigin=""anonymous""></script>
-</body>
 </html>";
 
         public const string Bootstrap5GuideUsingExample = @"@using Blazorise";
