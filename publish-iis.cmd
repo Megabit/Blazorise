c44fa27363cb2e107745ee2650d4e4017261242b
Mladen Macanovic <mladen.macanovic@blazorise.com>
2024-03-19 09:51:09 +0100

    Remove publish cmd files

--- publish-iis.cmd
+++
@@ -1,4 +0,0 @@
-cd /d "%~dp0"
-set username=%1
-set password=%2
-"%ProgramFiles(x86)%\\Microsoft Visual Studio\2019\Preview\MSBuild\Current\Bin\MSBuild.exe" Blazorise.sln /p:Configuration=Release /t:Restore;Rebuild /v:m /p:DeployOnBuild=true /p:PublishProfile=IISProfile /p:UserName=%username% /p:Password=%password%
--- publish.cmd
+++
@@ -1,2 +0,0 @@
-cd /d "%~dp0"
-"%ProgramFiles(x86)%\\Microsoft Visual Studio\2019\Preview\MSBuild\Current\Bin\MSBuild.exe" Blazorise.sln /p:Configuration=Release /t:Restore;Rebuild /v:m /p:DeployOnBuild=true /p:PublishProfile=FolderProfile
