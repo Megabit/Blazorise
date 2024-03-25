f967e67f3e2afe91e58e4e080d6eb5c241d2c38f
Mladen Macanovic <mladen.macanovic@blazorise.com>
2024-03-25 11:59:20 +0100

    Remove unused namespace

--- Source/Extensions/Blazorise.DataGrid/DataGrid.razor.cs
+++ Source/Extensions/Blazorise.DataGrid/DataGrid.razor.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
