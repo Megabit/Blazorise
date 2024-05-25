58b9d2bd54560b0a5dc9c67e9a5f79841a87e77e
David Moreira <david_nogueira_moreira@hotmail.com>
2024-05-25 21:16:10 +0100

    ToastProvider | ToastInstanceOptions | Add more options

--- Source/Blazorise/Models/ToastInstanceOptions.cs
+++ Source/Blazorise/Models/ToastInstanceOptions.cs
@@ -18,6 +18,26 @@ public class ToastInstanceOptions
     public EventCallback? Closed { get; set; }
 
     /// <summary>
+    /// Specifies whether the Toast should have an animated transition.
+    /// </summary>
+    public bool? Animated { get; set; }
+
+    /// <summary>
+    /// The duration of the animation in milliseconds.
+    /// </summary>
+    public int? AnimationDuration { get; set; }
+
+    /// <summary>
+    /// Automatically hide the toast after the delay.
+    /// </summary>
+    public bool? Autohide { get; set; }
+
+    /// <summary>
+    /// Delay in milliseconds before hiding the toast.
+    /// </summary>
+    public double? AutohideDelay { get; set; }
+
+    /// <summary>
     /// Creates the default toast options.
     /// </summary>
     /// <returns>Default toast options.</returns>
