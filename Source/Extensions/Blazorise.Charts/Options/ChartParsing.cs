e11e912ef8293f4cab6a1f82de8e697836b425a8
Mladen Macanović <mladen.macanovic@blazorise.com>
2024-06-18 12:36:31 +0200

    Docs: minor improvements (#5574)
    * mention that ProtectionHttpRequestHeaders are optionsl
    
    * Expand chart options API

--- Source/Extensions/Blazorise.Charts/Options/ChartParsing.cs
+++ Source/Extensions/Blazorise.Charts/Options/ChartParsing.cs
@@ -4,11 +4,20 @@ using System.Text.Json.Serialization;
 
 namespace Blazorise.Charts;
 
+/// <summary>
+/// Configuration for parsing chart data.
+/// </summary>
 public class ChartParsing
 {
+    /// <summary>
+    /// Key for the x-axis values in the data.
+    /// </summary>
     [JsonIgnore( Condition = JsonIgnoreCondition.WhenWritingNull )]
     public string XAxisKey { get; set; }
 
+    /// <summary>
+    /// Key for the y-axis values in the data.
+    /// </summary>
     [JsonIgnore( Condition = JsonIgnoreCondition.WhenWritingNull )]
     public string YAxisKey { get; set; }
 }
