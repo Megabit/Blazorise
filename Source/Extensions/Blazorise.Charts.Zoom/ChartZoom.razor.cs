23c0d6c91215914670b07eeaf77571edb58ce6cd
David Moreira <david_nogueira_moreira@hotmail.com>
2024-07-12 17:13:36 +0100

    Chart: Zoom plugin component (#5602)
    * Chart Zoom plugin support implementation
    
    * ChartZoom on own folder and files separate | implement animation
    
    * Example animation
    
    * Load zoom script in every demo index
    
    * Implement Animation correctly through chart transitions
    
    * Docs : Chart Zoom
    
    * Docs Adjust example zoom options
    
    * Docs : Chart Zoom Api Attibutes
    
    * Release notes | Chart Zoom
    
    * Add bar example to the demo page
    
    * ChartZoom plugin refactor into its own project
    
    * Remove ChartTransition class
    
    * Rename options and formating
    
    * Remove Folder settings
    
    * Typo fix
    
    * Fix docs
    
    ---------
    
    Co-authored-by: Mladen Macanovic <mladen.macanovic@blazorise.com>
    Co-authored-by: Mladen Macanovic <mladen.macanovic@gmail.com>

---
+++ Source/Extensions/Blazorise.Charts.Zoom/ChartZoom.razor.cs
@@ -0,0 +1,111 @@
+﻿#region Using directives
+using System;
+using System.Threading.Tasks;
+using Blazorise.Extensions;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+#endregion
+
+namespace Blazorise.Charts.Zoom;
+
+/// <summary>
+/// Provides the annotation capabilities to the supported chart types.
+/// </summary>
+/// <typeparam name="TItem">Data point type.</typeparam>
+public partial class ChartZoom<TItem> : BaseComponent, IAsyncDisposable
+{
+    #region Members
+
+    private const string PluginName = "Zoom";
+
+    #endregion
+
+    #region Methods
+
+    /// <inheritdoc/>
+    public override Task SetParametersAsync( ParameterView parameters )
+    {
+        if ( Rendered && JSModule is not null )
+        {
+            var optionsChanged = parameters.TryGetValue<ChartZoomPluginOptions>( nameof( Options ), out var paramOptions ) && !Options.IsEqual( paramOptions );
+
+            if ( optionsChanged )
+            {
+                ExecuteAfterRender( async () =>
+                {
+                    await JSModule.AddZoom( ParentChart.ElementId, Options );
+                } );
+            }
+        }
+
+        return base.SetParametersAsync( parameters );
+    }
+
+    /// <inheritdoc/>
+    protected override Task OnInitializedAsync()
+    {
+        if ( ParentChart is not null )
+        {
+            ParentChart.Initialized += OnParentChartInitialized;
+
+            ParentChart.NotifyPluginInitialized( PluginName );
+        }
+
+        return base.OnInitializedAsync();
+    }
+
+    private async void OnParentChartInitialized( object sender, EventArgs e )
+    {
+        if ( JSModule == null )
+        {
+            JSModule = new JSChartZoomModule( JSRuntime, VersionProvider );
+
+            ExecuteAfterRender( async () =>
+            {
+                await JSModule.AddZoom( ParentChart.ElementId, Options );
+            } );
+
+            await InvokeAsync( StateHasChanged );
+        }
+    }
+
+    /// <inheritdoc/>
+    protected override async ValueTask DisposeAsync( bool disposing )
+    {
+        if ( disposing && Rendered )
+        {
+            await JSModule.SafeDisposeAsync();
+
+            if ( ParentChart is not null )
+            {
+                ParentChart.Initialized -= OnParentChartInitialized;
+
+                ParentChart.NotifyPluginRemoved( PluginName );
+            }
+        }
+
+        await base.DisposeAsync( disposing );
+    }
+
+    #endregion
+
+    #region Properties
+
+    /// <inheritdoc/>
+    protected override bool ShouldAutoGenerateId => true;
+
+    protected JSChartZoomModule JSModule { get; private set; }
+
+    [Inject] private IJSRuntime JSRuntime { get; set; }
+
+    [Inject] private IVersionProvider VersionProvider { get; set; }
+
+    [CascadingParameter] protected BaseChart<TItem> ParentChart { get; set; }
+
+    /// <summary>
+    /// Defines the options for an annotation.
+    /// </summary>
+    [Parameter] public ChartZoomPluginOptions Options { get; set; }
+
+    #endregion
+}
