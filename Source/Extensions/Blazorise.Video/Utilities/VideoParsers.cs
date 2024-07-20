98b7269a8d95b934926fbdb4d240a57ce5341b55
Mladen Macanovic <mladen.macanovic@gmail.com>
2024-07-20 18:26:26 +0200

    Remove exceptions from ParseAspectRatio

--- Source/Extensions/Blazorise.Video/Utilities/VideoParsers.cs
+++ Source/Extensions/Blazorise.Video/Utilities/VideoParsers.cs
@@ -13,21 +13,13 @@ internal static class VideoParsers
 
         var parts = aspectRatio.Split( ':' );
 
-        if ( parts.Length != 2 )
+        if ( parts.Length == 2
+            && int.TryParse( parts[0], out int width )
+            && int.TryParse( parts[1], out int height ) )
         {
-            throw new ArgumentException( "Invalid aspect ratio format. Expected format: 'width:height'." );
+            return (double)width / height;
         }
 
-        if ( !int.TryParse( parts[0], out int width ) || !int.TryParse( parts[1], out int height ) )
-        {
-            throw new ArgumentException( "Invalid aspect ratio format. Parts should be integers." );
-        }
-
-        if ( height == 0 )
-        {
-            throw new ArgumentException( "Invalid aspect ratio format. Height should not be zero." );
-        }
-
-        return (double)width / height;
+        return null;
     }
 }
