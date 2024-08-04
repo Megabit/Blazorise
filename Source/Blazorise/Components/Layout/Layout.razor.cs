97de28509eeefcc771ac4e87a632d0d801599850
Mladen Macanovic <mladen.macanovic@gmail.com>
2024-08-04 12:23:52 +0200

    Layout

--- Source/Blazorise/Components/Layout/Layout.razor.cs
+++ Source/Blazorise/Components/Layout/Layout.razor.cs
@@ -50,7 +50,9 @@ public partial class Layout : BaseComponent
     /// <param name="builder">Class builder used to append the classnames.</param>
     protected void BuildLoadingClasses( ClassBuilder builder )
     {
-        builder.Append( ClassProvider.LayoutLoading(), string.IsNullOrEmpty( LoadingClass ) );
+        if ( string.IsNullOrEmpty( LoadingClass ) )
+            builder.Append( ClassProvider.LayoutLoading() );
+
         builder.Append( LoadingClass );
     }
 
