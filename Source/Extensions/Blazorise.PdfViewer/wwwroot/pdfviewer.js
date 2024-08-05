4d64ad8eb2cdda5120f2cf4da8c96a9fa2f97414
Mladen Macanovic <mladen.macanovic@blazorise.com>
2024-08-05 13:56:22 +0200

    Working example

--- Source/Extensions/Blazorise.PdfViewer/wwwroot/pdfviewer.js
+++ Source/Extensions/Blazorise.PdfViewer/wwwroot/pdfviewer.js
@@ -16,6 +16,45 @@ export async function initialize(dotNetAdapter, element, elementId, options) {
         options: options,
     };
 
+    var url = 'https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/examples/learning/helloworld.pdf';
+
+    const { pdfjsLib } = globalThis;
+
+    pdfjsLib.GlobalWorkerOptions.workerSrc = "./pdf.worker.min.mjs?v=1.6.0.0";
+
+    // Asynchronous download of PDF
+    var loadingTask = pdfjsLib.getDocument(url);
+    loadingTask.promise.then(function (pdf) {
+        console.log('PDF loaded');
+
+        // Fetch the first page
+        var pageNumber = 1;
+        pdf.getPage(pageNumber).then(function (page) {
+            console.log('Page loaded');
+
+            var scale = 1.5;
+            var viewport = page.getViewport({ scale: scale });
+
+            // Prepare canvas using PDF page dimensions
+            var canvas = element;
+            var context = canvas.getContext('2d');
+            canvas.height = viewport.height;
+            canvas.width = viewport.width;
+
+            // Render PDF page into canvas context
+            var renderContext = {
+                canvasContext: context,
+                viewport: viewport
+            };
+            var renderTask = page.render(renderContext);
+            renderTask.promise.then(function () {
+                console.log('Page rendered');
+            });
+        });
+    }, function (reason) {
+        // PDF loading error
+        console.error(reason);
+    });
 
     _instances[elementId] = instance;
 }
