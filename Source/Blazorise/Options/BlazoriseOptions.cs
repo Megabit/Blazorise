47957d2fa5ec8c52ffd20fa73ac9812f0f46ffca
Mladen Macanovic <mladen.macanovic@blazorise.com>
2024-08-23 18:09:37 +0200

    Move options to subfolder and separate files

--- Source/Blazorise/Options/BlazoriseOptions.cs
+++ Source/Blazorise/Options/BlazoriseOptions.cs
@@ -131,19 +131,3 @@ public class BlazoriseOptions
 
     #endregion
 }
-
-/// <summary>
-/// Represents the options available for configuring the behavior of a Tooltip in Blazorise.
-/// </summary>
-public class BlazoriseTooltipOptions
-{
-    /// <summary>
-    /// Specifies the delay in milliseconds (ms) after a trigger event before a Tooltip is shown. Default is <c>null</c> for no delay.
-    /// </summary>
-    public int? ShowDelay { get; set; }
-
-    /// <summary>
-    /// Specifies the delay in milliseconds (ms) after a trigger event before a Tooltip is hidden. Default is <c>null</c> for no delay.
-    /// </summary>
-    public int? HideDelay { get; set; }
-}
