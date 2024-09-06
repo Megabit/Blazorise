f7b22ed7878738a016736a5e1d9503c52c1e4b0e
Mladen Macanović <mladen.macanovic@blazorise.com>
2024-09-06 10:18:54 +0200

    PdfViewer component (#5676)
    * Add PdfViewer project
    
    * Working example
    
    * Sort extensions in the demo
    
    * Loading and pages navigation
    
    * Optimize scripts loading
    
    * Use buttons
    
    * loadDocument method
    
    * Zoom
    
    * Update sln guid
    
    * Split viewer into separate components
    
    * nuget pack
    
    * Docs and release notes
    
    * readonly note
    
    * Update PageNumber and Scale options
    
    * Update Rotation
    
    * Delete extra NotifyPdfInitialized
    
    * Scale two way binding
    
    * Mention mthods in docs
    
    * Fix initial pageNumber
    
    * Add printing
    
    * Fix zoom icon names
    
    * Non-generic subscribable callbacks
    
    * Use internal state
    
    * URL wording
    
    * mark Subscribable methods as internal
    
    * Rename method insertCSSIntoDocumentHead
    
    * Rename canvas to element
    
    * Null checks
    
    * null checks
    
    * Use delegate in example
    
    * Grammar
    
    * Rename to SubscribeOrReplace
    
    * Base64 example
    
    * Comment
    
    * Change lead sentence
    
    * Separate API page

--- Source/Blazorise/Infrastructure/EventCallbackSubscribable.cs
+++ Source/Blazorise/Infrastructure/EventCallbackSubscribable.cs
@@ -8,15 +8,14 @@ namespace Blazorise.Infrastructure;
 
 /// <summary>
 /// Represents an event that you may subscribe to. This differs from normal C# events in that the handlers
-/// are EventCallback<typeparamref name="T"/>, and so may have async behaviors and cause component re-rendering
+/// are EventCallback, and so may have async behaviors and cause component re-rendering
 /// while retaining error flow.
 /// </summary>
-/// <typeparam name="T">A type for the eventargs.</typeparam>
-public class EventCallbackSubscribable<T>
+public class EventCallbackSubscribable
 {
     #region Members
 
-    private readonly Dictionary<EventCallbackSubscriber<T>, EventCallback<T>> callbacks = new();
+    private readonly Dictionary<EventCallbackSubscriber, EventCallback> callbacks = new();
 
     #endregion
 
@@ -25,28 +24,29 @@ public class EventCallbackSubscribable<T>
     /// <summary>
     /// Invokes all the registered callbacks sequentially, in an undefined order.
     /// </summary>
-    public async Task InvokeCallbackAsync( T eventArgs )
+    public async Task InvokeCallbackAsync()
     {
         foreach ( var callback in callbacks.Values )
         {
-            await callback.InvokeAsync( eventArgs );
+            await callback.InvokeAsync();
         }
     }
 
     /// <summary>
-    /// Don't call this directly - it gets called by EventCallbackSubscription.
+    /// Subscribes an event callback for the specified owner.
     /// </summary>
-    /// <param name="owner"></param>
-    /// <param name="callback"></param>
-    public void Subscribe( EventCallbackSubscriber<T> owner, EventCallback<T> callback )
+    /// <param name="owner">The subscriber that owns the callback.</param>
+    /// <param name="callback">The callback method to be subscribed.</param>
+    internal void Subscribe( EventCallbackSubscriber owner, EventCallback callback )
         => callbacks.Add( owner, callback );
 
     /// <summary>
-    /// Don't call this directly - it gets called by EventCallbackSubscription.
+    /// Unsubscribes the event callback for the specified owner.
     /// </summary>
-    /// <param name="owner"></param>
-    public void Unsubscribe( EventCallbackSubscriber<T> owner )
+    /// <param name="owner">The subscriber whose callback is to be unsubscribed.</param>
+    internal void Unsubscribe( EventCallbackSubscriber owner )
         => callbacks.Remove( owner );
+
 
     #endregion
 }
