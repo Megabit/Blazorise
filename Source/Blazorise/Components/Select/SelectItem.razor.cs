2f6bc5894774edef9d718683fd054a4f2f173a1e
Mladen Macanovic <mladen.macanovic@blazorise.com>
2024-09-19 10:50:10 +0200

    Move ISelect to separate file

--- Source/Blazorise/Components/Select/SelectItem.razor.cs
+++ Source/Blazorise/Components/Select/SelectItem.razor.cs
@@ -8,36 +8,6 @@ using Microsoft.AspNetCore.Components;
 namespace Blazorise;
 
 /// <summary>
-/// Interface for all <see cref="SelectItem{TValue}"/> components.
-/// </summary>
-public interface ISelectItem
-{
-    /// <summary>
-    /// Checks if the value is equal to the item value.
-    /// </summary>
-    /// <param name="value">The value to compare.</param>
-    /// <returns>A <see cref="bool"/> value indicating whether the value is equal to the item value.</returns>
-    bool CompareTo( object value );
-
-    /// <summary>
-    /// Gets or sets the item render fragment.
-    /// </summary>
-    RenderFragment ChildContent { get; set; }
-}
-
-/// <summary>
-/// Basic type for all <see cref="SelectItem{TValue}"/> components.
-/// </summary>
-/// <typeparam name="TValue"></typeparam>
-public interface ISelectItem<TValue> : ISelectItem
-{
-    /// <summary>
-    /// Gets or sets the item value.
-    /// </summary>
-    TValue Value { get; set; }
-}
-
-/// <summary>
 /// Option item in the <see cref="Select{TValue}"/> component.
 /// </summary>
 /// <typeparam name="TValue">The type of the <see cref="Value"/>.</typeparam>
