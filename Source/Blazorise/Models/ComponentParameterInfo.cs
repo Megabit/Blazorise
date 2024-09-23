597cdb60d0ef1b7e22e39bc95cfb0842f063e5bc
Mladen Macanovic <mladen.macanovic@blazorise.com>
2024-09-23 11:40:53 +0200

    Comments

--- Source/Blazorise/Models/ComponentParameterInfo.cs
+++ Source/Blazorise/Models/ComponentParameterInfo.cs
@@ -1,9 +1,9 @@
 ﻿namespace Blazorise;
 
 /// <summary>
-/// Represents a component parameter that can containe the last received value.
+/// Represents a component parameter that can contain the last received value, as well as information about whether it was defined or changed.
 /// </summary>
-/// <typeparam name="T"></typeparam>
+/// <typeparam name="T">The type of the component parameter's value.</typeparam>
 public record struct ComponentParameterInfo<T>
 {
     #region Members
@@ -17,27 +17,25 @@ public record struct ComponentParameterInfo<T>
     #region Constructors
 
     /// <summary>
-    /// Initializes a new instance of the <see cref="ComponentParameterInfo{T}"/> structure to the specified value.
+    /// Initializes a new instance of the <see cref="ComponentParameterInfo{T}"/> structure with the specified value.
     /// </summary>
-    /// <param name="value">A value type.</param>
+    /// <param name="value">The value of the component parameter.</param>
     public ComponentParameterInfo( T value )
     {
         Value = value;
-
         defined = false;
         changed = false;
     }
 
     /// <summary>
-    /// Initializes a new instance of the <see cref="ComponentParameterInfo{T}"/> structure to the specified value and received information.
+    /// Initializes a new instance of the <see cref="ComponentParameterInfo{T}"/> structure with the specified value and received information.
     /// </summary>
-    /// <param name="value">A value type.</param>
-    /// <param name="defined">True if the parameter has being received through the parameters.</param>
-    /// <param name="changed">True if the parameter has being changes since the last time.</param>
+    /// <param name="value">The value of the component parameter.</param>
+    /// <param name="defined">A value indicating whether the parameter was received through the component parameters or attributes.</param>
+    /// <param name="changed">A value indicating whether the parameter has changed since it was last rendered.</param>
     public ComponentParameterInfo( T value, bool defined, bool changed )
     {
         Value = value;
-
         this.defined = defined;
         this.changed = changed;
     }
@@ -47,13 +45,10 @@ public record struct ComponentParameterInfo<T>
     #region Methods
 
     /// <summary>
-    /// Retrieves the value of the current <see cref="ComponentParameterInfo{T}"/> object, or a default value.
+    /// Retrieves the value of the current <see cref="ComponentParameterInfo{T}"/> object, or a default value if the parameter was not defined.
     /// </summary>
     /// <param name="defaultValue">A value to return if the <see cref="Defined"/> property is false.</param>
-    /// <returns>The value of the <see cref="Value"/> property if the <see cref="Defined"/> property is true; otherwise, the <paramref name="defaultValue"/> parameter.</returns>
-    /// <remarks>
-    /// The <see cref="GetValueOrDefault"/> method returns a value even if the <see cref="Defined"/> property is false.
-    /// </remarks>
+    /// <returns>The value of the <see cref="Value"/> property if <see cref="Defined"/> is true; otherwise, the <paramref name="defaultValue"/> parameter.</returns>
     public T GetValueOrDefault( T defaultValue )
     {
         if ( !defined )
@@ -67,17 +62,17 @@ public record struct ComponentParameterInfo<T>
     #region Properties
 
     /// <summary>
-    /// Gets the actual value that was received through the supplied parameters.
+    /// Gets the actual value that was received through the component parameters or attributes.
     /// </summary>
     public T Value { get; }
 
     /// <summary>
-    /// Indicates if the parameter was being defined and received through the component parameters or attributes.
+    /// Indicates whether the parameter was defined and received through the component parameters or attributes.
     /// </summary>
     public bool Defined => defined;
 
     /// <summary>
-    /// Indicates if the received parameter has being changed since the last rendering. 
+    /// Indicates whether the received parameter has changed since the last rendering.
     /// </summary>
     public bool Changed => changed;
 
