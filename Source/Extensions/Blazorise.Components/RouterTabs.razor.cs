01a4637f70e5494bdccf2c74ce0c81e10cf7e9fc
David Moreira <david_nogueira_moreira@hotmail.com>
2024-10-08 23:03:39 +0100

    RouterTabs ongoing

---
+++ Source/Extensions/Blazorise.Components/RouterTabs.razor.cs
@@ -0,0 +1,61 @@
+﻿#region Using directives
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
+#endregion
+
+namespace Blazorise.Components;
+
+public partial class RouterTabs : ComponentBase, IDisposable
+{
+    private bool disposedValue;
+
+    [CascadingParameter] public RouteData RouteData { get; set; }
+
+    [Inject] public NavigationManager NavigationManager { get; set; }
+    [Inject] public RouterTabsService RouterTabsService { get; set; }
+
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+
+        NavigationManager.LocationChanged += OnLocationChanged;
+        RouterTabsService.OnStateHasChanged += OnStateHasChanged;
+        if ( RouteData  is not null)
+            RouterTabsService.TrySetRouteData( RouteData );
+    }
+
+    private void OnLocationChanged( object o, LocationChangedEventArgs _ )
+    {
+        RouterTabsService.TrySetRouteData( RouteData );
+    }
+
+    private void OnStateHasChanged()
+    {
+        StateHasChanged();
+    }
+
+    protected virtual void Dispose( bool disposing )
+    {
+        if ( !disposedValue )
+        {
+            if ( disposing )
+            {
+                RouterTabsService.OnStateHasChanged -= OnStateHasChanged;
+                NavigationManager.LocationChanged -= OnLocationChanged;
+            }
+
+            disposedValue = true;
+        }
+    }
+
+    public void Dispose()
+    {
+        // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        Dispose( disposing: true );
+        GC.SuppressFinalize( this );
+    }
+}
---
+++ Source/Extensions/Blazorise.Components/RouterTabsService.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace Blazorise.Components;
+
+public class RouterTabsItem
+{
+    public string TypeName { get; set; }
+    public string Url { get; set; }
+    public RenderFragment Body { get; set; }
+
+}
+
+public class RouterTabsService
+{
+
+    internal event Action OnStateHasChanged;
+
+    private List<RouterTabsItem> _tabs = new List<RouterTabsItem>();
+    private readonly NavigationManager navigationManager;
+    public IReadOnlyCollection<RouterTabsItem> Tabs => _tabs.AsReadOnly();
+
+    public RouterTabsService( NavigationManager navigationManager )
+    {
+        this.navigationManager = navigationManager;
+    }
+
+    internal string CurrentUrl
+    {
+        get => "/" + navigationManager.ToBaseRelativePath( navigationManager.Uri );
+        set
+        {
+            try
+            {
+                navigationManager.NavigateTo( value.StartsWith( '/' ) ? value[1..] : value );
+            }
+            catch ( NavigationException )
+            {
+                
+            }
+        }
+    }
+
+    private void AddRouterTabsItem( RouterTabsItem routerTabsItem )
+    {
+        _tabs.Add( routerTabsItem );
+    }
+
+    internal void TrySetRouteData( RouteData routeData )
+    {
+        var routerTabsItem = _tabs?.FirstOrDefault( w => w.Url == CurrentUrl );
+
+        if ( routerTabsItem is null )
+        {
+            routerTabsItem = new RouterTabsItem
+            {
+                Url = CurrentUrl,
+            };
+
+            AddRouterTabsItem( routerTabsItem );
+        }
+
+        if ( routeData is null )
+        {
+
+        }
+        else
+        {
+            routerTabsItem.Body ??= CreateRouterTabsItemBody( routeData, routerTabsItem );
+            routerTabsItem.TypeName = routeData.PageType.FullName;
+        }
+
+        OnStateHasChanged?.Invoke();
+    }
+
+    private RenderFragment CreateRouterTabsItemBody( RouteData routeData, RouterTabsItem item )
+    {
+        return builder =>
+        {
+            builder.OpenComponent( 0, routeData.PageType );
+            foreach ( var routeValue in routeData.RouteValues )
+            {
+                builder.AddAttribute( 1, routeValue.Key, routeValue.Value );
+            }
+
+            builder.AddComponentReferenceCapture( 2, @ref =>
+            {
+                SetRouterTabsItemFromPage( item, routeData.PageType, item.Url);
+            } );
+
+            builder.CloseComponent();
+        };
+    }
+
+    private void SetRouterTabsItemFromPage( RouterTabsItem pageItem, Type pageType, string url)
+    {
+        //Get metadata from specialized page attributes
+    }
+}
