6ff5ab1e5593973cae644ad59c9d92888b2f1ac5
Mladen Macanovic <mladen.macanovic@blazorise.com>
2024-10-15 12:09:08 +0200

    Version 1.6.2

--- Source/Blazorise/wwwroot/colorPicker.js
+++ Source/Blazorise/wwwroot/colorPicker.js
@@ -1,5 +1,5 @@
-import "./vendors/Pickr.js?v=1.6.1.0";
-import * as utilities from "./utilities.js?v=1.6.1.0";
+import "./vendors/Pickr.js?v=1.6.2.0";
+import * as utilities from "./utilities.js?v=1.6.2.0";
 
 const _instancesInfos = [];
 
