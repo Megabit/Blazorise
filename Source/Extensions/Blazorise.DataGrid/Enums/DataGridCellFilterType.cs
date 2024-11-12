5f1f21dfcf6a220f841b14cf3a28b219f41b3da9
David Moreira <david_nogueira_moreira@hotmail.com>
2024-11-12 00:09:43 +0000

    Workaround for DatePicker being set to null no matter what when filter is set through Parameter FilterSearchValue

---
+++ Source/Extensions/Blazorise.DataGrid/Enums/DataGridCellFilterType.cs
@@ -0,0 +1,11 @@
+﻿namespace Blazorise.DataGrid;
+
+/// <summary>
+/// Defines the DataGrid cell filter type.
+/// </summary>
+public enum DataGridCellFilterType
+{
+    Single,
+    Range1,
+    Range2
+}
