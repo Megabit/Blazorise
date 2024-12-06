aba720126d22869d21d3889acecf02239be7a19e
tesar-tech <jan@tesar.tech>
2024-12-06 19:00:51 +0100

    Fix for generic in dictionary.

--- Documentation/Blazorise.Docs.Compiler/ApiDocsGenerator/Extensions/TypeSymbolExtensions.cs
+++ Documentation/Blazorise.Docs.Compiler/ApiDocsGenerator/Extensions/TypeSymbolExtensions.cs
@@ -15,7 +15,26 @@ public static class TypeSymbolExtensions
             throw new ArgumentNullException( nameof( componentType ) );
         string nonGenericTypeName = componentType.ToDisplayString( SymbolDisplayFormat.FullyQualifiedFormat.WithGenericsOptions( SymbolDisplayGenericsOptions.None ) );
         if ( nonGenericTypeName.Split( "." ).Last() == "Dictionary" )//for dictionaries, otherwise it leaves with Dictionary<> 
-            return componentType.ToDisplayString( SymbolDisplayFormat.FullyQualifiedFormat.WithGenericsOptions( SymbolDisplayGenericsOptions.IncludeTypeParameters ) );
+        {
+            if (componentType is INamedTypeSymbol nt && nt.IsGenericType)
+            {
+                var typeArguments = nt.TypeArguments.Select(
+                x =>
+                {
+                    if ( x is INamedTypeSymbol t && t.IsGenericType )
+                    {//this is for the case where generic param (in dictionary) has generic param of TItem (which is not valid, thus replacing with <object>)
+                     //spotted in DataGrid._BaseDataGridRowEdit<>.CellValues (of type Dictionary<string, CellEditContext<TItem>> => Dictionary<string, CellEditContext<object>>)
+                     //maybe this can be also used for other generics of generic where TItem lives, but currently there is no such case, thus the limit for Dictionary.
+                        return $"{t.ToDisplayString( SymbolDisplayFormat.FullyQualifiedFormat.WithGenericsOptions( SymbolDisplayGenericsOptions.None ) )}<object>";
+                    }
+                    //just a normal type like string (inside a Dictionary<string)
+                    return x.ToDisplayString( SymbolDisplayFormat.FullyQualifiedFormat.WithGenericsOptions( SymbolDisplayGenericsOptions.IncludeTypeParameters ) );
+                }
+                ); 
+                return $"{nonGenericTypeName}<{string.Join(", ", typeArguments)}>";
+            }
+        }
+        
 
         return
             nonGenericTypeName +
