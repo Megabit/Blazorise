5fc84c472acd1a78ec1f2ebc9f409ad0d933217d
tesar-tech <jan@tesar.tech>
2025-02-05 13:32:13 +0100

    xml comments on the colors

--- Source/Blazorise/Themes/Colors/ThemeColorIndigo.cs
+++ Source/Blazorise/Themes/Colors/ThemeColorIndigo.cs
@@ -8,22 +8,75 @@ namespace Blazorise;
 /// </summary>
 public record ThemeColorIndigo : ThemeColor
 {
-#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+    /// <summary>
+    /// The lightest shade of the indigo theme color.
+    /// </summary>
     public ThemeColorShade _50 { get; } = new( "50", "_50", "#e8eaf6" );
+
+    /// <summary>
+    /// A very light shade of the indigo theme color.
+    /// </summary>
     public ThemeColorShade _100 { get; } = new( "100", "_100", "#c5cae9" );
+
+    /// <summary>
+    /// A light shade of the indigo theme color.
+    /// </summary>
     public ThemeColorShade _200 { get; } = new( "200", "_200", "#9fa8da" );
+
+    /// <summary>
+    /// A medium-light shade of the indigo theme color.
+    /// </summary>
     public ThemeColorShade _300 { get; } = new( "300", "_300", "#7986cb" );
+
+    /// <summary>
+    /// A medium shade of the indigo theme color.
+    /// </summary>
     public ThemeColorShade _400 { get; } = new( "400", "_400", "#5c6bc0" );
+
+    /// <summary>
+    /// The base indigo theme color shade.
+    /// </summary>
     public ThemeColorShade _500 { get; } = new( "500", "_500", "#3f51b5" );
+
+    /// <summary>
+    /// A slightly darker shade of the indigo theme color.
+    /// </summary>
     public ThemeColorShade _600 { get; } = new( "600", "_600", "#3949ab" );
+
+    /// <summary>
+    /// A dark shade of the indigo theme color.
+    /// </summary>
     public ThemeColorShade _700 { get; } = new( "700", "_700", "#303f9f" );
+
+    /// <summary>
+    /// A very dark shade of the indigo theme color.
+    /// </summary>
     public ThemeColorShade _800 { get; } = new( "800", "_800", "#283593" );
+
+    /// <summary>
+    /// The darkest shade of the indigo theme color.
+    /// </summary>
     public ThemeColorShade _900 { get; } = new( "900", "_900", "#1a237e" );
+
+    /// <summary>
+    /// A bright variant of the indigo theme color.
+    /// </summary>
     public ThemeColorShade A100 { get; } = new( "A100", "A100", "#8c9eff" );
+
+    /// <summary>
+    /// A lighter accent shade of the indigo theme color.
+    /// </summary>
     public ThemeColorShade A200 { get; } = new( "A200", "A200", "#536dfe" );
+
+    /// <summary>
+    /// A strong accent shade of the indigo theme color.
+    /// </summary>
     public ThemeColorShade A400 { get; } = new( "A400", "A400", "#3d5afe" );
+
+    /// <summary>
+    /// The darkest accent shade of the indigo theme color.
+    /// </summary>
     public ThemeColorShade A700 { get; } = new( "A700", "A700", "#304ffe" );
-#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
 
     /// <summary>
     /// A default <see cref="ThemeColorIndigo"/> constructor
