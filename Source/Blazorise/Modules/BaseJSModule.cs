7a0a5959921655a4146732a6abd3ac9e472266fb
tesar-tech <jan@tesar.tech>
2025-02-19 19:09:06 +0100

    Adds Debug.WriteLine to see the js exception.

--- Source/Blazorise/Modules/BaseJSModule.cs
+++ Source/Blazorise/Modules/BaseJSModule.cs
@@ -1,5 +1,6 @@
 ﻿#region Using directives
 using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using Blazorise.Extensions;
 using Microsoft.JSInterop;
@@ -136,8 +137,9 @@ public abstract class BaseJSModule : IBaseJSModule, IAsyncDisposable
 
                 await module.InvokeVoidAsync( identifier, args );
             }
-            catch ( Exception )
+            catch ( Exception ex )
             {
+                Debug.WriteLine( $"Exception form InvokeSafeVoidAsync: {ex.Message}" );
             }
         }
         else
@@ -181,8 +183,9 @@ public abstract class BaseJSModule : IBaseJSModule, IAsyncDisposable
 
                 return await module.InvokeAsync<TValue>( identifier, args );
             }
-            catch ( Exception )
+            catch ( Exception ex)
             {
+                Debug.WriteLine( $"Exception form InvokeSafeVoidAsync: {ex.Message}" );
                 return default;
             }
         }
