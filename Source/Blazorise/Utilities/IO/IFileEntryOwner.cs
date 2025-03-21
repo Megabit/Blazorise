d6459236268a20d3bab01fa8a6c51f3c4f7450fe
Mladen Macanovic <mladen.macanovic@gmail.com>
2025-03-21 16:26:47 +0100

    Revert IFileEntryOwner

--- Source/Blazorise/Utilities/IO/IFileEntryOwner.cs
+++ Source/Blazorise/Utilities/IO/IFileEntryOwner.cs
@@ -1,10 +1,7 @@
 ﻿#region Using directives
 using System.IO;
-using System.Text.Json.Serialization;
 using System.Threading;
 using System.Threading.Tasks;
-using Blazorise.Modules;
-using Microsoft.AspNetCore.Components;
 #endregion
 
 namespace Blazorise;
@@ -30,26 +27,4 @@ public interface IFileEntryOwner
     /// <param name="cancellationToken">A cancellation token to signal the cancellation of streaming file data.</param>
     /// <returns>Returns the stream for the uploaded file entry.</returns>
     Stream OpenReadStream( FileEntry fileEntry, CancellationToken cancellationToken = default );
-
-    /// <summary>
-    /// Removes the file entry from js dictionary.
-    /// </summary>
-    /// <param name="fileEntry">Currently processed file entry.</param>
-    /// <param name="cancellationToken">A cancellation token to signal the cancellation of streaming file data.</param>
-    /// <returns>A task that represents the asynchronous operation.</returns>
-    async Task RemoveFileEntry( IFileEntry fileEntry, CancellationToken cancellationToken = default )
-    {
-        await JSFileModule.RemoveFileEntry( ElementRef, fileEntry.Id );
-    }
-
-    /// <summary>
-    /// Element reference.
-    /// </summary>
-    ElementReference ElementRef { get; set; }
-
-    /// <summary>
-    /// JS file module.
-    /// </summary>
-    [JsonIgnore]
-    IJSFileModule JSFileModule { get; set; }
 }
