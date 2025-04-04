66c33b6b0ae89c2905f6b8045b867d1b1bb646f9
Mladen Macanovic <mladen.macanovic@blazorise.com>
2025-04-04 10:06:32 +0200

    Add missing page redirects

--- Documentation/Blazorise.Docs.Server/Infrastructure/PermanentRedirects.cs
+++ Documentation/Blazorise.Docs.Server/Infrastructure/PermanentRedirects.cs
@@ -6,7 +6,11 @@ static class PermanentRedirects
 {
     public static readonly Dictionary<string, string> Map = new()
     {
+        ["/docs/components/date"] = "/docs/components/dateedit",
         ["/docs/components/file"] = "/docs/components/fileedit",
+        ["/docs/components/memo"] = "/docs/components/memoedit",
+        ["/docs/components/numeric"] = "/docs/components/numericedit",
         ["/docs/components/text"] = "/docs/components/textedit",
+        ["/docs/components/time"] = "/docs/components/timeedit",
     };
 }
