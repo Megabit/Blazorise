f89711bfb4dd41d06c4ba70f0152b9b6f7613205
Mladen Macanovic <mladen.macanovic@blazorise.com>
2025-04-15 10:42:23 +0200

    Formating

--- Source/Extensions/Blazorise.DataGrid/Constants.cs
+++ Source/Extensions/Blazorise.DataGrid/Constants.cs
@@ -1,5 +1,8 @@
 namespace Blazorise.DataGrid;
 
+/// <summary>
+/// Contains static readonly properties for various display, margin, flex, padding, and gap settings used in UI layouts.
+/// </summary>
 static class Constants
 {
     // Display
@@ -21,15 +24,15 @@ static class Constants
     internal static readonly IFluentSpacing MarginIs0FromBottomIs3FromStart = Margin.Is0.FromBottom.Is3.FromStart;
     internal static readonly IFluentSpacing MarginIs2FromStart = Margin.Is2.FromStart;
     internal static readonly IFluentSpacing MarginIs1FromEnd = Margin.Is1.FromEnd;
-    
+
     // Flex
     internal static readonly IFluentFlex FlexRow = Flex.Row;
     internal static readonly IFluentFlex FlexJustifyContentEndAlignItemsCenter = Flex.JustifyContent.End.AlignItems.Center;
-    internal static readonly IFluentFlex FlexRowAlignItemsCenterJustifyContentEnd =Flex.Row.AlignItems.Center.JustifyContent.End;
+    internal static readonly IFluentFlex FlexRowAlignItemsCenterJustifyContentEnd = Flex.Row.AlignItems.Center.JustifyContent.End;
     internal static readonly IFluentFlex FlexInlineFlexOnTabletJustifyContentCenterOnTablet = Flex.InlineFlex.OnTablet.JustifyContent.Center.OnTablet;
     internal static readonly IFluentFlex FlexGrowIs1 = Flex.Grow.Is1;
-    internal static readonly IFluentFlex FlexInlineFlex= Flex.InlineFlex;
-    
+    internal static readonly IFluentFlex FlexInlineFlex = Flex.InlineFlex;
+
     // Padding
     internal static readonly IFluentSpacing PaddingIs2OnXIs2OnY = Padding.Is2.OnX.Is2.OnY;
     internal static readonly IFluentSpacing PaddingIs3 = Padding.Is3;
@@ -37,5 +40,4 @@ static class Constants
 
     // Gap
     internal static readonly IFluentGap GapIs2 = Gap.Is2;
-
 }
