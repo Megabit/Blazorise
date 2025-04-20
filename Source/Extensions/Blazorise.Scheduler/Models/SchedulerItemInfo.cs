d0651c6fedcdbec81e8a76c5b080eb16adb50afe
Mladen Macanovic <mladen.macanovic@gmail.com>
2025-04-20 21:07:55 +0200

    Simlify SchedulerItemInfo constructors

--- Source/Extensions/Blazorise.Scheduler/Models/SchedulerItemInfo.cs
+++ Source/Extensions/Blazorise.Scheduler/Models/SchedulerItemInfo.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿#region Using Directives
+using System;
 using Blazorise.Scheduler.Utilities;
+#endregion
 
 namespace Blazorise.Scheduler;
 
@@ -10,31 +12,13 @@ namespace Blazorise.Scheduler;
 public class SchedulerItemInfo<TItem>
 {
     /// <summary>
-    /// Represents information about a scheduled item, including its time frame and overflow status.
+    /// Initializes a scheduler item with specified details including time and recurrence settings.
     /// </summary>
-    /// <param name="item">Defines the specific item being scheduled.</param>
+    /// <param name="item">Represents the specific item to be scheduled.</param>
     /// <param name="start">Indicates the starting time of the scheduled item.</param>
     /// <param name="end">Indicates the ending time of the scheduled item.</param>
-    /// <param name="allDay"></param>
-    /// <param name="recurrenceRule">Defines the recurrence rule for the scheduled item.</param>
-    /// <param name="isRecurring">Indicates whether the item is recurring.</param>
-    public SchedulerItemInfo( TItem item, DateTime start, DateTime end, bool allDay, string recurrenceRule, bool isRecurring )
-    {
-        Item = item;
-        Start = start;
-        End = end;
-        Duration = end - start;
-        AllDay = allDay;
-        AllDayByDuration = Duration.Days >= 1;
-
-        if ( !string.IsNullOrEmpty( recurrenceRule ) )
-        {
-            RecurrenceRule = RecurringRuleParser.Parse( recurrenceRule );
-        }
-
-        IsRecurring = isRecurring;
-    }
-
+    /// <param name="allDay">Specifies whether the scheduled item lasts all day.</param>
+    /// <param name="isRecurring">Indicates if the scheduled item repeats at regular intervals.</param>
     public SchedulerItemInfo( TItem item, DateTime start, DateTime end, bool allDay, bool isRecurring )
     {
         Item = item;
@@ -43,20 +27,40 @@ public class SchedulerItemInfo<TItem>
         Duration = end - start;
         AllDay = allDay;
         AllDayByDuration = Duration.Days >= 1;
-        RecurrenceRule = null;
         IsRecurring = isRecurring;
     }
 
-    public SchedulerItemInfo( TItem item, DateTime start, DateTime end, bool allDay, SchedulerRecurrenceRule recurrenceRule, bool isRecurring )
+    /// <summary>
+    /// Initializes a new instance of the SchedulerItemInfo class with specified parameters for scheduling.
+    /// </summary>
+    /// <param name="item">Represents the item to be scheduled, containing relevant details for the event.</param>
+    /// <param name="start">Indicates the starting date and time of the scheduled event.</param>
+    /// <param name="end">Specifies the ending date and time of the scheduled event.</param>
+    /// <param name="allDay">Indicates whether the event lasts all day without specific start and end times.</param>
+    /// <param name="recurrenceRule">Defines the pattern for recurring events, allowing for complex scheduling.</param>
+    /// <param name="isRecurring">Indicates if the event is part of a recurring series, affecting its scheduling behavior.</param>
+    public SchedulerItemInfo( TItem item, DateTime start, DateTime end, bool allDay, string recurrenceRule, bool isRecurring )
+        : this( item, start, end, allDay, isRecurring )
     {
-        Item = item;
-        Start = start;
-        End = end;
-        Duration = end - start;
-        AllDay = allDay;
-        AllDayByDuration = Duration.Days >= 1;
+        if ( !string.IsNullOrEmpty( recurrenceRule ) )
+        {
+            RecurrenceRule = RecurringRuleParser.Parse( recurrenceRule );
+        }
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the SchedulerItemInfo class with specified parameters for scheduling.
+    /// </summary>
+    /// <param name="item">Represents the item to be scheduled.</param>
+    /// <param name="start">Indicates the starting date and time of the scheduled item.</param>
+    /// <param name="end">Indicates the ending date and time of the scheduled item.</param>
+    /// <param name="allDay">Specifies whether the scheduled item lasts all day.</param>
+    /// <param name="recurrenceRule">Defines the rules for recurring events in the schedule.</param>
+    /// <param name="isRecurring">Indicates if the scheduled item is part of a recurring series.</param>
+    public SchedulerItemInfo( TItem item, DateTime start, DateTime end, bool allDay, SchedulerRecurrenceRule recurrenceRule, bool isRecurring )
+        : this( item, start, end, allDay, isRecurring )
+    {
         RecurrenceRule = recurrenceRule;
-        IsRecurring = isRecurring;
     }
 
     /// <summary>
