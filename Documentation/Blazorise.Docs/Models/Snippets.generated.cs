5e2b37f0a4aa7fbd14108d7a12d635bf8c754282
Mladen Macanovic <mladen.macanovic@gmail.com>
2025-07-15 10:04:28 +0200

    NotificationOptions: add missing ShowOkButton property

--- Documentation/Blazorise.Docs/Models/Snippets.generated.cs
+++ Documentation/Blazorise.Docs/Models/Snippets.generated.cs
@@ -13103,12 +13103,13 @@ builder.Services
 
     Task ShowWarningNotification()
     {
-        return NotificationService.Warning( ""This is a simple notification message!"", ""Hello"", options =>
+        return NotificationService.Warning( ""Are you sure you want to remove the selected item!"", ""Removal"", options =>
         {
             options.IntervalBeforeClose = 500;
             options.Multiline = true;
-            options.OkButtonIcon = true;
-            options.OkButtonText = ""OK"";
+            options.ShowOkButton = true;
+            options.OkButtonIcon = IconName.Delete;
+            options.OkButtonText = ""Remove"";
         } );
     }
 }";
