bd0f86aceb45d683d23fb8da4639e4c6a81116dc
Mladen Macanović <mladen.macanovic@blazorise.com>
2025-09-30 11:23:48 +0200

    Web: add Paddle Billing integration checkout (#6230)
    * Paddle2 setup
    
    * Change localhost ports
    
    * Update price ids
    
    * production client id

--- Documentation/Blazorise.Docs/wwwroot/blazorise.docs.js
+++ Documentation/Blazorise.Docs/wwwroot/blazorise.docs.js
@@ -104,26 +104,14 @@ window.myComponent = {
 window.blazorisePRO = {
     paddle: {
         openCheckout: (product, quantity, upsell) => {
-            const referral = window.Rewardful && window.Rewardful.referral;
-
-            if (upsell) {
-                Paddle.Checkout.open({
-                    product: product,
-                    quantity: quantity,
-                    upsell: upsell.id,
-                    upsellTitle: upsell.title,
-                    upsellText: upsell.text,
-                    upsellAction: upsell.action,
-                    passthrough: JSON.stringify({ rewardful: { referral: referral } })
-                });
-            }
-            else {
-                Paddle.Checkout.open({
-                    product: product,
-                    quantity: quantity,
-                    passthrough: JSON.stringify({ rewardful: { referral: referral } })
-                });
-            }
+            Paddle.Checkout.open({
+                items: [
+                    {
+                        priceId: product,
+                        quantity: quantity
+                    }
+                ]
+            });
         }
     }
 };
