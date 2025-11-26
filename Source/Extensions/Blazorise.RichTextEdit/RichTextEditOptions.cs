81e730c79d23bc7e09f3f9a4e691c8e5a26e08f6
Mladen Macanovic <mladen.macanovic@gmail.com>
2025-11-26 14:07:18 +0100

    Remove obsolete parameters from RichTextEdit

--- Source/Extensions/Blazorise.RichTextEdit/RichTextEditOptions.cs
+++ Source/Extensions/Blazorise.RichTextEdit/RichTextEditOptions.cs
@@ -29,22 +29,4 @@ public sealed class RichTextEditOptions
     /// If true enables the QuillJs image resize module. Please be aware that this module is not part of the core QuillJs library, and it is still experimental.
     /// </summary>
     public bool UseResize { get; set; }
-
-    /// <summary>
-    /// The QuillJs version to load
-    /// </summary>
-    [Obsolete( "Dynamic loading no longer used." )]
-    public string QuillJsVersion { get; set; } = "2.0.0";
-
-    /// <summary>
-    /// Load the RichTextEdit scripts and stylesheets on demand.
-    /// </summary>
-    [Obsolete( "Dynamic loading no longer used." )]
-    public bool DynamicallyLoadReferences { get; set; } = false;
-
-    /// <summary>
-    /// Dynamic references to be loaded when initializing the RichTextEdit component.
-    /// </summary>
-    [Obsolete( "Dynamic loading no longer used." )]
-    public List<DynamicReference> DynamicReferences { get; set; } = new();
 }
