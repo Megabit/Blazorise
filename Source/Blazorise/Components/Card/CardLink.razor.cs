72ac549a96c5c5f6ea5bfb955f8968183001af90
Mladen Macanovic <mladen.macanovic@blazorise.com>
2025-11-26 12:15:02 +0100

    Remove obsolete parameters from CardLink

--- Source/Blazorise/Components/Card/CardLink.razor.cs
+++ Source/Blazorise/Components/Card/CardLink.razor.cs
@@ -14,22 +14,6 @@ public partial class CardLink : BaseLinkComponent
     #region Methods
 
     /// <inheritdoc/>
-    protected override void OnInitialized()
-    {
-#pragma warning disable CS0618 // Type or member is obsolete
-        if ( Source is not null && To is null )
-            To = Source;
-#pragma warning restore CS0618 // Type or member is obsolete
-
-#pragma warning disable CS0618 // Type or member is obsolete
-        if ( Alt is not null && Title is null )
-            Title = Alt;
-#pragma warning restore CS0618 // Type or member is obsolete
-
-        base.OnInitialized();
-    }
-
-    /// <inheritdoc/>
     protected override void BuildClasses( ClassBuilder builder )
     {
         builder.Append( ClassProvider.CardLink() );
