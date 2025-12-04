f5119e361b100109fe19141a4a9c99bbe3eb956b
Mladen Macanović <mladen.macanovic@blazorise.com>
2025-12-04 17:48:25 +0100

    Docs: implement theme by storing and reading from cookies (#6300)

--- Documentation/Blazorise.Docs/Routes.razor.cs
+++ Documentation/Blazorise.Docs/Routes.razor.cs
@@ -1,20 +1,26 @@
 ﻿using System.Threading.Tasks;
-using Blazored.LocalStorage;
 using Blazorise.Docs.Services;
-using Blazorise.Modules;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Http;
+using Microsoft.JSInterop;
 
 namespace Blazorise.Docs;
 
 public partial class Routes
 {
+    private const string ThemeCookieName = "bdocs-theme";
+    private const string SystemThemeCookieName = "bdocs-system";
+    private bool jsReady;
+
     [Inject] private ThemeService ThemeService { get; set; }
-    [Inject] private IJSUtilitiesModule JSUtilitiesModule { get; set; }
-    [Inject] ILocalStorageService LocalStorage { get; set; }
+    [Inject] private IHttpContextAccessor HttpContextAccessor { get; set; }
+    [Inject] private IJSRuntime JSRuntime { get; set; }
 
     protected override void OnInitialized()
     {
         ThemeService.ThemeChanged += OnThemeChanged;
+
+        TryLoadThemeFromCookie();
 
         base.OnInitialized();
     }
@@ -23,8 +29,10 @@ public partial class Routes
     {
         if ( firstRender )
         {
-            var theme = await LocalStorage.GetItemAsync<string>( "theme" );
-            var systemIsDarkMode = await JSUtilitiesModule.IsSystemDarkMode();
+            var theme = await JSRuntime.InvokeAsync<string>( "blazoriseDocs.theme.getStoredTheme" );
+            var systemIsDarkMode = await JSRuntime.InvokeAsync<bool>( "blazoriseDocs.theme.isSystemDark" );
+
+            jsReady = true;
 
             ThemeService.SetTheme( theme, systemIsDarkMode );
         }
@@ -39,17 +47,28 @@ public partial class Routes
 
     async void OnThemeChanged( object sender, string theme )
     {
-        if ( ThemeService.ShouldDark )
-        {
-            await JSUtilitiesModule.AddAttributeToBody( "data-bs-theme", "dark" );
-        }
-        else
-        {
-            await JSUtilitiesModule.RemoveAttributeFromBody( "data-bs-theme" );
-        }
+        if ( !jsReady )
+            return;
 
-        await LocalStorage.SetItemAsync( "theme", theme );
+        await JSRuntime.InvokeVoidAsync( "blazoriseDocs.theme.save", theme, ThemeService.SystemIsDarkMode );
 
         await InvokeAsync( StateHasChanged );
+    }
+
+    private void TryLoadThemeFromCookie()
+    {
+        var httpContext = HttpContextAccessor.HttpContext;
+
+        if ( httpContext is null )
+            return;
+
+        if ( httpContext.Request.Cookies.TryGetValue( ThemeCookieName, out var themeFromCookie ) )
+        {
+            var systemIsDarkMode = httpContext.Request.Cookies.TryGetValue( SystemThemeCookieName, out var systemCookie )
+                && bool.TryParse( systemCookie, out var parsedSystemDark )
+                && parsedSystemDark;
+
+            ThemeService.SetTheme( themeFromCookie, systemIsDarkMode );
+        }
     }
 }
