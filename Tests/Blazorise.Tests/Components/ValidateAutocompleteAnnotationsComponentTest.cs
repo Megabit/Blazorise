5af7e0e9685bbb78bc40e6a38c558f724dc9ef14
Mladen Macanovic <mladen.macanovic@gmail.com>
2025-12-26 08:22:04 +0100

    Unit tests

---
+++ Tests/Blazorise.Tests/Components/ValidateAutocompleteAnnotationsComponentTest.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Blazorise.Tests.Extensions;
+using Bunit;
+using Microsoft.AspNetCore.Components;
+using Xunit;
+
+namespace Blazorise.Tests.Components;
+
+public class ValidateAutocompleteAnnotationsComponentTest : TestContext
+{
+    public ValidateAutocompleteAnnotationsComponentTest()
+    {
+        Services.AddBlazoriseTests().AddBootstrapProviders().AddEmptyIconProvider().AddTestData();
+        JSInterop
+            .AddBlazoriseTextInput()
+            .AddBlazoriseUtilities()
+            .AddBlazoriseClosable()
+            .AddBlazoriseDropdown();
+    }
+
+    [Fact]
+    public async Task CanAutoValidateAutocompleteSingle_InitiallyBlank()
+    {
+        var comp = RenderComponent<ValidateAutocompleteAnnotationsComponent>();
+        var wrapperSelector = "#auto-validate-autocomplete-single .b-is-autocomplete";
+
+        Assert.Contains( "is-invalid", comp.Find( wrapperSelector ).ClassList );
+
+        await SelectAutocompleteValue( comp, "#auto-validate-autocomplete-single", "Portugal" );
+
+        comp.WaitForAssertion( () =>
+            Assert.Contains( "is-valid", comp.Find( wrapperSelector ).ClassList ),
+            TestExtensions.WaitTime );
+    }
+
+    [Fact]
+    public async Task CanAutoValidateAutocompleteMultiple_InitiallyBlank()
+    {
+        var comp = RenderComponent<ValidateAutocompleteAnnotationsComponent>();
+        var wrapperSelector = "#auto-validate-autocomplete-multiple .b-is-autocomplete";
+
+        Assert.Contains( "is-invalid", comp.Find( wrapperSelector ).ClassList );
+
+        await SelectAutocompleteValue( comp, "#auto-validate-autocomplete-multiple", "Portugal" );
+
+        comp.WaitForAssertion( () =>
+            Assert.Contains( "is-valid", comp.Find( wrapperSelector ).ClassList ),
+            TestExtensions.WaitTime );
+
+        var badge = comp.Find( "#auto-validate-autocomplete-multiple .badge" );
+        var removeButton = badge.GetElementsByTagName( "span" )[0];
+        await removeButton.ClickAsync( new() );
+
+        comp.WaitForAssertion( () =>
+            Assert.Contains( "is-invalid", comp.Find( wrapperSelector ).ClassList ),
+            TestExtensions.WaitTime );
+    }
+
+    private static async Task SelectAutocompleteValue<TComponent>( IRenderedComponent<TComponent> comp, string scopeSelector, string expectedText ) where TComponent : IComponent
+    {
+        var input = comp.Find( $"{scopeSelector} input" );
+        await input.FocusAsync( new() );
+        await input.InputAsync( expectedText );
+
+        WaitAndClickFirstOption( comp, scopeSelector, expectedText );
+    }
+
+    private static void WaitAndClickFirstOption<TComponent>( IRenderedComponent<TComponent> comp, string scopeSelector, string expectedText ) where TComponent : IComponent
+    {
+        var iterations = 0;
+        while ( true )
+        {
+            var firstSuggestion = comp.WaitForElement( $"{scopeSelector} .b-is-autocomplete-suggestion", TestExtensions.WaitTime );
+            if ( firstSuggestion.TextContent.Contains( expectedText ) )
+            {
+                firstSuggestion.Click();
+                break;
+            }
+
+            if ( iterations++ > 10 )
+                throw new InvalidOperationException( $"Could not find a valid suggestion for {expectedText}" );
+
+            Thread.Sleep( 100 );
+        }
+    }
+}
