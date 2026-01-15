8d1a5f6a05ee9c4a6c79315346ba02c1c815c02b
Mladen Macanovic <mladen.macanovic@blazorise.com>
2026-01-15 09:53:13 +0100

    unit test

---
+++ Tests/Blazorise.Tests/Components/SelectErrorBoundaryComponentTest.cs
@@ -0,0 +1,30 @@
+using System.ComponentModel;
+using System.Threading.Tasks;
+using Bunit;
+using Xunit;
+
+namespace Blazorise.Tests.Components;
+
+public class SelectErrorBoundaryComponentTest : TestContext
+{
+    public SelectErrorBoundaryComponentTest()
+    {
+        Services.AddBlazoriseTests().AddBootstrapProviders().AddEmptyIconProvider().AddTestData();
+        JSInterop.AddBlazoriseButton();
+    }
+
+    [Fact]
+    public async Task SelectedValueChanged_Exception_TriggersErrorBoundary()
+    {
+        var comp = RenderComponent<SelectErrorBoundaryComponent>();
+        var select = comp.Find( "select" );
+
+        await select.ChangeAsync( "2" );
+
+        comp.WaitForAssertion( () =>
+        {
+            var error = comp.Find( "#select-error-boundary-error" );
+            Assert.Equal( nameof( InvalidEnumArgumentException ), error.TextContent );
+        } );
+    }
+}
