b77ff57fe69e88296c755dbe0a45907a363d191b
Mladen Macanović <mladen.macanovic@blazorise.com>
2026-02-17 11:01:52 +0100

    DataGrid: Self-Reference Hierarchy mode (#6400)
    * toggle datagrid mvp
    
    * DataGridExpandTrigger
    
    * Indent size in rems
    
    * docs example
    
    * Editing example
    
    * ExpandRowTemplate
    
    * ExpandTemplate on column
    
    * regenerate json
    
    * New badge
    
    * formating
    
    * Reorganize hierarchy pages
    
    * Reorganize demo hierarchy pages
    
    * rename examples
    
    * rename models
    
    * rename hierarchy to self reference
    
    * further renaming

---
+++ Source/Extensions/Blazorise.DataGrid/EventArguments/DataGridExpandRowTriggerEventArgs.cs
@@ -0,0 +1,31 @@
+﻿#region Using directives
+using System;
+#endregion
+
+namespace Blazorise.DataGrid;
+
+/// <summary>
+/// Holds self-reference row trigger context configuration.
+/// </summary>
+/// <typeparam name="TItem">Type of the data model.</typeparam>
+public class DataGridExpandRowTriggerEventArgs<TItem> : EventArgs
+{
+    /// <summary>
+    /// Initializes a new instance of self-reference-row-trigger event argument.
+    /// </summary>
+    /// <param name="item">Row item.</param>
+    public DataGridExpandRowTriggerEventArgs( TItem item )
+    {
+        Item = item;
+    }
+
+    /// <summary>
+    /// Gets the row item.
+    /// </summary>
+    public TItem Item { get; private set; }
+
+    /// <summary>
+    /// Gets or sets whether the row is expandable. Defaults to <c>true</c>.
+    /// </summary>
+    public bool Expandable { get; set; } = true;
+}
