e4d522e723a3f1163cbf433a5dfe7bd1e2b9f7a5
Mladen Macanovic <mladen.macanovic@blazorise.com>
2026-02-27 09:51:59 +0100

    remove inline styles

--- Source/Extensions/Blazorise.Gantt/Utilities/FluentConstants.cs
+++ Source/Extensions/Blazorise.Gantt/Utilities/FluentConstants.cs
@@ -8,10 +8,19 @@ internal static class FluentConstants
     // Flex
     internal static readonly IFluentFlex FlexAlignItemsCenter = Flex.AlignItems.Center;
     internal static readonly IFluentFlex FlexAlignItemsCenterNoWrap = Flex.AlignItems.Center.NoWrap;
+    internal static readonly IFluentFlex FlexAlignItemsCenterWrap = Flex.AlignItems.Center.Wrap;
     internal static readonly IFluentFlex FlexColumn = Flex.Column;
     internal static readonly IFluentFlex FlexDefault = Flex.Default;
     internal static readonly IFluentFlex FlexGrowIs1 = Flex.Grow.Is1;
     internal static readonly IFluentFlex FlexJustifyContentBetweenAlignItemsCenter = Flex.JustifyContent.Between.AlignItems.Center;
+    internal static readonly IFluentFlex FlexJustifyContentBetweenAlignItemsCenterWrap = Flex.JustifyContent.Between.AlignItems.Center.Wrap;
+
+    // Sizing
+    internal static readonly IFluentSizing HeightIs100 = Height.Is100;
+    internal static readonly IFluentSizing HeightPx44 = Height.Px( 44 );
+    internal static readonly IFluentSizing WidthIs100 = Width.Is100;
+    internal static readonly IFluentSizing WidthPx220 = Width.Px( 220 );
+    internal static readonly IFluentSizing WidthPx28 = Width.Px( 28 );
 
     // Column
     internal static readonly IFluentColumn ColumnSizeIs12OnMobileIs6OnTablet = ColumnSize.Is12.OnMobile.Is6.OnTablet;
@@ -27,11 +36,23 @@ internal static class FluentConstants
     internal static readonly IFluentSpacing PaddingIs2OnX = Padding.Is2.OnX;
     internal static readonly IFluentSpacing PaddingIs3 = Padding.Is3;
 
+    // Gap
+    internal static readonly IFluentGap GapIs1 = Gap.Is1;
+    internal static readonly IFluentGap GapIs2 = Gap.Is2;
+
+    // Display
+    internal static readonly IFluentDisplay DisplayInlineBlock = Display.InlineBlock;
+
     // Typography
     internal static readonly IFluentTextSize TextSizeSmall = TextSize.Small;
 
     // Border
+    internal static readonly IFluentBorder BorderIs0 = Border.Is0;
     internal static readonly IFluentBorder BorderIs1 = Border.Is1;
+    internal static readonly IFluentBorder BorderIs1OnBottom = Border.Is1.OnBottom;
     internal static readonly IFluentBorder BorderIs1OnEnd = Border.Is1.OnEnd;
     internal static readonly IFluentBorder BorderRounded = Border.Rounded;
+
+    // Position
+    internal static readonly IFluentPosition PositionStickyTopIs0 = Position.Sticky.Top.Is0;
 }
