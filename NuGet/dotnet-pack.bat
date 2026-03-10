6867f834c6e3d05847d6c3f7e8fd071f40943956
Mladen Macanovic <mladen.macanovic@blazorise.com>
2026-03-10 11:51:28 +0100

    dotnet-pack

--- NuGet/dotnet-pack.bat
+++ NuGet/dotnet-pack.bat
@@ -38,6 +38,7 @@ dotnet pack ../Source/Extensions/Blazorise.TreeView/Blazorise.TreeView.csproj -o
 dotnet pack ../Source/Extensions/Blazorise.Video/Blazorise.Video.csproj -o . -c Release -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
 dotnet pack ../Source/Extensions/Blazorise.PdfViewer/Blazorise.PdfViewer.csproj -o . -c Release -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
 dotnet pack ../Source/Extensions/Blazorise.Scheduler/Blazorise.Scheduler.csproj -o . -c Release -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
+dotnet pack ../Source/Extensions/Blazorise.Gantt/Blazorise.Gantt.csproj -o . -c Release -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
 
 dotnet pack ../Source/Extensions/Blazorise.Icons.Bootstrap/Blazorise.Icons.Bootstrap.csproj -o . -c Release -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
 dotnet pack ../Source/Extensions/Blazorise.Icons.FontAwesome/Blazorise.Icons.FontAwesome.csproj -o . -c Release -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
