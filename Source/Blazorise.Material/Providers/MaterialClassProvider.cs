25d5e6ff8c9e7919d59bc1e9006263c8c67ac775
Niek Jannink <info@jannink-it.eu>
2026-04-11 07:05:26 -0500

    Label: Fix Cursor.Pointer (#6521)
    * Label: Fix Cursor.Pointer
    
    * formating and material naming changes
    
    ---------
    
    Co-authored-by: Mladen Macanovic <mladen.macanovic@gmail.com>

--- Source/Blazorise.Material/Providers/MaterialClassProvider.cs
+++ Source/Blazorise.Material/Providers/MaterialClassProvider.cs
@@ -296,7 +296,7 @@ public class MaterialClassProvider : ClassProvider
         };
     }
 
-    public override string LabelCursor( Cursor cursor ) => cursor != Cursor.Default ? $"form-check-label-{ToCursor( cursor )}" : null;
+    public override string LabelCursor( Cursor cursor ) => cursor != Cursor.Default ? $"mui-cursor-{ToCursor( cursor )}" : null;
 
     #endregion
 
