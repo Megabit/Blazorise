6ec5bce76b1a0ef30ae0438196f0f1d4d5495205
Mladen Macanović <mladen.macanovic@blazorise.com>
2026-04-29 17:08:36 +0200

    Accesibility: improve FocusTrap and MessageProvider (#6576)
    * CloseOnEscape for message provider
    
    * refactor FocusTrap
    
    * improve focusable elements

--- Tests/Blazorise.Tests/Components/MessageProviderComponentTest.cs
+++ Tests/Blazorise.Tests/Components/MessageProviderComponentTest.cs
@@ -42,10 +42,38 @@ public class MessageProviderComponentTest : BunitContext
         component.WaitForAssertion( () => Assert.DoesNotContain( "modal-lg", component.Find( ".modal-dialog" ).ClassName ) );
     }
 
+    [Fact]
+    public async Task ConfirmCloseOnEscapeCompletesAsCanceled()
+    {
+        var component = Render<TestMessageProvider>();
+        var messageService = Services.GetRequiredService<IMessageService>();
+        Task<bool> confirmTask = null;
+
+        await component.InvokeAsync( () =>
+        {
+            confirmTask = messageService.Confirm( "Message", "Title", options => options.CloseOnEscape = true );
+
+            return Task.CompletedTask;
+        } );
+
+        Assert.NotNull( confirmTask );
+        component.WaitForAssertion( () => Assert.Contains( "Message", component.Markup ) );
+
+        await component.InvokeAsync( () => component.Instance.InvokeModalClosing( new( false, CloseReason.EscapeClosing ) ) );
+
+        Assert.False( await confirmTask );
+    }
+
     private Task ShowInfoMessage( IRenderedComponent<MessageProvider> component, Action<MessageOptions> options = null )
     {
         var messageService = Services.GetRequiredService<IMessageService>();
 
         return component.InvokeAsync( () => messageService.Info( "Message", "Title", options ) );
+    }
+
+    private class TestMessageProvider : MessageProvider
+    {
+        public Task InvokeModalClosing( ModalClosingEventArgs eventArgs )
+            => OnModalClosing( eventArgs );
     }
 }
