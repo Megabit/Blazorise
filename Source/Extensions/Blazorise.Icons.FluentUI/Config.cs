8221784e19e9f7e560fa18c40012384f5cbccf95
Mladen Macanović <mladen.macanovic@blazorise.com>
2026-07-16 19:18:55 +0200

    FluentUI Icons: add opt-in SVG sprite rendering (#6687)
    * svg icons
    
    * default icon size
    
    * default regular icons
    
    * fix icon size
    
    * optimize svg icons
    
    * docs

--- Source/Extensions/Blazorise.Icons.FluentUI/Config.cs
+++ Source/Extensions/Blazorise.Icons.FluentUI/Config.cs
@@ -1,4 +1,5 @@
 #region Using directives
+using System;
 using Microsoft.Extensions.DependencyInjection;
 #endregion
 
@@ -8,7 +9,17 @@ public static class Config
 {
     public static IServiceCollection AddFluentUIIcons( this IServiceCollection serviceCollection )
     {
-        serviceCollection.AddSingleton<IIconProvider, FluentUIIconProvider>();
+        return AddFluentUIIcons( serviceCollection, null );
+    }
+
+    public static IServiceCollection AddFluentUIIcons( this IServiceCollection serviceCollection, Action<FluentUIIconOptions> configureOptions )
+    {
+        FluentUIIconOptions options = new();
+        configureOptions?.Invoke( options );
+
+        serviceCollection.AddSingleton( options );
+        serviceCollection.AddSingleton<FluentUIIconProvider>();
+        serviceCollection.AddSingleton<IIconProvider>( serviceProvider => serviceProvider.GetRequiredService<FluentUIIconProvider>() );
 
         serviceCollection.AddTransient<Blazorise.Icon, FluentUI.Icon>();
 
