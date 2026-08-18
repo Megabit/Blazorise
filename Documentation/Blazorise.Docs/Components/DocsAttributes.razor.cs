a94b497f884d42122ede721cd5c6b066be15c496
Mladen Macanović <mladen.macanovic@blazorise.com>
2026-08-18 13:10:46 +0200

    Docs: add dedicated API pages and deep links (#6738)
    * refactor docs headings
    
    * short sections
    
    * dedicated api pages
    
    * api pages for services
    
    * scroll to

--- Documentation/Blazorise.Docs/Components/DocsAttributes.razor.cs
+++ Documentation/Blazorise.Docs/Components/DocsAttributes.razor.cs
@@ -57,6 +57,8 @@ public partial class DocsAttributes : IDisposable
 
     [Parameter] public string Title { get; set; }
 
+    [Parameter] public string ElementId { get; set; }
+
     [Parameter] public bool Ordered { get; set; } = true;
 
     [Parameter] public bool ShowDefaults { get; set; } = true;
