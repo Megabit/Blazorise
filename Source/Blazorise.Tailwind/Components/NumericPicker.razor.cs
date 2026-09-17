87a6f9fc61ce610158931df529722707622a0eb7
Mladen Macanovic <mladen.macanovic@blazorise.com>
2026-09-17 11:17:34 +0200

    add missing numericpicker buttons

---
+++ Source/Blazorise.Tailwind/Components/NumericPicker.razor.cs
@@ -0,0 +1,125 @@
+#region Using directives
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Blazorise.Utilities;
+using Microsoft.AspNetCore.Components;
+#endregion
+
+namespace Blazorise.Tailwind.Components;
+
+public partial class NumericPicker<TValue> : Blazorise.NumericPicker<TValue>
+{
+    #region Members
+
+    private IFluentSizing numericWrapperWidth;
+
+    #endregion
+
+    #region Constructors
+
+    public NumericPicker()
+    {
+        NumericWrapperClassBuilder = new( BuildNumericWrapperClasses, builder => builder.Append( Classes?.Wrapper ) );
+        NumericWrapperStyleBuilder = new( BuildNumericWrapperStyles, builder => builder.Append( Styles?.Wrapper ) );
+        ButtonsClassBuilder = new( BuildButtonsClasses, builder => builder.Append( Classes?.Buttons ) );
+        ButtonUpClassBuilder = new( BuildButtonUpClasses, builder => builder.Append( Classes?.ButtonUp ) );
+        ButtonDownClassBuilder = new( BuildButtonDownClasses, builder => builder.Append( Classes?.ButtonDown ) );
+    }
+
+    #endregion
+
+    #region Methods
+
+    public override Task SetParametersAsync( ParameterView parameters )
+    {
+        // Apply width to the wrapper while the input fills the available space.
+        if ( parameters.TryGetValue<IFluentSizing>( nameof( Width ), out IFluentSizing paramWidth ) )
+        {
+            numericWrapperWidth = paramWidth;
+            NumericWrapperClassBuilder.Dirty();
+            NumericWrapperStyleBuilder.Dirty();
+
+            Dictionary<string, object> remainingParameters = parameters.ToDictionary()
+                .Where( x => x.Key != nameof( Width ) )
+                .ToDictionary( x => x.Key, x => x.Value );
+
+            return base.SetParametersAsync( ParameterView.FromDictionary( remainingParameters ) );
+        }
+
+        return base.SetParametersAsync( parameters );
+    }
+
+    protected internal override void DirtyClasses()
+    {
+        NumericWrapperClassBuilder.Dirty();
+        ButtonsClassBuilder.Dirty();
+        ButtonUpClassBuilder.Dirty();
+        ButtonDownClassBuilder.Dirty();
+
+        base.DirtyClasses();
+    }
+
+    protected internal override void DirtyStyles()
+    {
+        NumericWrapperStyleBuilder.Dirty();
+
+        base.DirtyStyles();
+    }
+
+    private void BuildNumericWrapperClasses( ClassBuilder builder )
+    {
+        builder.Append( "tw-numeric" );
+        AppendWrapperUtilities( builder );
+        builder.Append( numericWrapperWidth?.Class( ClassProvider ) );
+    }
+
+    private void BuildNumericWrapperStyles( StyleBuilder builder )
+    {
+        AppendWrapperUtilities( builder );
+        builder.Append( numericWrapperWidth?.Style( StyleProvider ) );
+    }
+
+    private static void BuildButtonsClasses( ClassBuilder builder )
+    {
+        builder.Append( "tw-numeric-buttons" );
+    }
+
+    private static void BuildButtonUpClasses( ClassBuilder builder )
+    {
+        builder.Append( "tw-numeric-button" );
+        builder.Append( "tw-numeric-button-up" );
+    }
+
+    private static void BuildButtonDownClasses( ClassBuilder builder )
+    {
+        builder.Append( "tw-numeric-button" );
+        builder.Append( "tw-numeric-button-down" );
+    }
+
+    #endregion
+
+    #region Properties
+
+    protected ClassBuilder NumericWrapperClassBuilder { get; private set; }
+
+    protected StyleBuilder NumericWrapperStyleBuilder { get; private set; }
+
+    protected ClassBuilder ButtonsClassBuilder { get; private set; }
+
+    protected ClassBuilder ButtonUpClassBuilder { get; private set; }
+
+    protected ClassBuilder ButtonDownClassBuilder { get; private set; }
+
+    protected string NumericWrapperClassNames => NumericWrapperClassBuilder.Class;
+
+    protected string NumericWrapperStyleNames => NumericWrapperStyleBuilder.Styles;
+
+    protected string ButtonsClassNames => ButtonsClassBuilder.Class;
+
+    protected string ButtonUpClassNames => ButtonUpClassBuilder.Class;
+
+    protected string ButtonDownClassNames => ButtonDownClassBuilder.Class;
+
+    #endregion
+}
